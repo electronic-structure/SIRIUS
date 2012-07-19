41d97e34e286afa84a163f36e2f4a33389eb4789
toxa81 <toxa81@gmail.com>
2012-07-19 16:16:45 +0200

    modified:   Makefile 	modified:   hydrogen.cpp 	new file:   read_atom.cpp

--- src/apps/tests/hydrogen.cpp
+++ src/apps/tests/hydrogen.cpp
@@ -2,18 +2,17 @@
 
 int main(int argn, char **argv)
 {
-    sirius::radial_grid r(sirius::linear_exponential_grid, 30000, 1e-8, 100.0);
-    r.print_info();
+    sirius::RadialGrid radial_grid(sirius::linear_exponential_grid, 30000, 1e-8, 100.0);
+    radial_grid.print_info();
 
-    std::vector<double> v(r.size());
-    std::vector<double> p(r.size());
-    std::vector<double> hp(r.size());
+    std::vector<double> v(radial_grid.size());
+    std::vector<double> p(radial_grid.size());
 
-    sirius::radial_solver solver(false, -1.0, r);
+    sirius::RadialSolver solver(false, -1.0, radial_grid);
 
-    for (int i = 0; i < r.size(); i++)
+    for (int i = 0; i < radial_grid.size(); i++)
     {
-        v[i] = -1.0 / r[i];
+        v[i] = -1.0 / radial_grid[i];
     }
     
     double enu = -0.1;
@@ -21,8 +20,9 @@ int main(int argn, char **argv)
     {
         for (int l = 0; l <= n - 1; l++)
         {
-            solver.bound_state(n, l, enu, v, p);
+            solver.bound_state(n, l, v, enu, p);
             std::cout << "energy - exact " << enu + 1.0 / pow(n, 2) / 2 << std::endl;
         }
     }
+    
 }
---
+++ src/apps/tests/read_atom.cpp
@@ -0,0 +1,17 @@
+#include "../../lib/sirius.h"
+
+int main(int argn, char **argv)
+{
+    JsonTree parser(std::string("data_d=1.0.json"));
+    
+    //double mass = parser["mass1"].get<double>();
+    //int l = parser["valence"][0]["l1"] >> l;
+    //parser["mass"] >> mass;
+    
+    //double mass = parser["mass"](default_mass);
+    
+    
+    //std::cout << "mass = " << mass << std::endl;
+    
+    //double f1 = parser["mass"](10.0);
+}
