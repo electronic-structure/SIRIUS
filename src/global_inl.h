926772e8cb1d33ee6e0ae04132a8a52ac62b6c8a
toxa81 <toxa81@gmail.com>
2012-12-11 22:19:24 +0100

    modify write_matrix

--- src/global_inl.h
+++ src/global_inl.h
@@ -34,9 +34,16 @@ double gaussian_smearing(double e)
     return 0.5 * (1 - gsl_sf_erf(e / delta));
 }
 
-void write_matrix(const std::string& fname, mdarray<complex16, 2>& matrix)
+void write_matrix(const std::string& fname, bool write_all, mdarray<complex16, 2>& matrix)
 {
-    FILE* fout = fopen(fname.c_str(), "w");
+    static int icount = 0;
+
+    icount++;
+    std::stringstream s;
+    s << icount;
+    std::string full_name = s.str() + "_" + fname;
+
+    FILE* fout = fopen(full_name.c_str(), "w");
 
     for (int icol = 0; icol < matrix.size(1); icol++)
     {
@@ -46,14 +53,22 @@ void write_matrix(const std::string& fname, mdarray<complex16, 2>& matrix)
         fprintf(fout, " row                real               imag                abs \n");
         for (int i = 0; i < 80; i++) fprintf(fout, "-");
         fprintf(fout, "\n");
-        for (int j = 0; j <= icol; j++)
+        
+        int max_row = (write_all) ? (matrix.size(0) - 1) : std::min(icol, matrix.size(0) - 1);
+        for (int j = 0; j <= max_row; j++)
         {
             fprintf(fout, "%4i  %18.12f %18.12f %18.12f\n", j, real(matrix(j, icol)), imag(matrix(j, icol)), 
                                                             abs(matrix(j, icol)));
         }
         fprintf(fout,"\n");
-
     }
 
     fclose(fout);
+}
+
+bool file_exists(const std::string& file_name)
+{
+    std::ifstream ifs(file_name.c_str());
+    if (ifs.is_open()) return true;
+    return false;
 }
