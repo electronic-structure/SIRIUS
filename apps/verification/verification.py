2195bbb1cf53c1ce49d05a7c6b68ab453aeb6333
toxa81 <toxa81@gmail.com>
2013-04-30 13:35:23 +0200

    update verification tests

--- apps/verification/verification.py
+++ apps/verification/verification.py
@@ -6,35 +6,37 @@ import shutil
 import glob
 import subprocess
 
-def create_input_file(inp, conf):
+def create_input_file(common, private):
     
     k = 0
-    for atom_type in inp["atoms"]:
+    for atom_type in common["atoms"]:
         for atom in atom_type[1]:
-            if "vector_field" in conf: atom.extend(conf["vector_field"][k])
+            if "vector_field" in private: atom.extend(private["vector_field"][k])
             else: atom.extend([0, 0, 0])
             k = k + 1
     
     elk_in = "tasks\n2000\n\n"
     
     elk_in += "avec\n" + \
-              "%16.10f %16.10f %16.10f\n"%(inp["avec"][0][0], inp["avec"][0][1], inp["avec"][0][2]) + \
-              "%16.10f %16.10f %16.10f\n"%(inp["avec"][1][0], inp["avec"][1][1], inp["avec"][1][2]) +  \
-              "%16.10f %16.10f %16.10f\n"%(inp["avec"][2][0], inp["avec"][2][1], inp["avec"][2][2]) + "\n" 
-
-    if "scale" in inp:
-        elk_in += "scale\n" + "%16.10f\n"%inp["scale"] + "\n"
-
-    if "nempty" in inp:
-        elk_in += "nempty\n" + "%i\n"%inp["nempty"] + "\n"
+              "%16.10f %16.10f %16.10f\n"%(common["avec"][0][0], common["avec"][0][1], common["avec"][0][2]) + \
+              "%16.10f %16.10f %16.10f\n"%(common["avec"][1][0], common["avec"][1][1], common["avec"][1][2]) +  \
+              "%16.10f %16.10f %16.10f\n"%(common["avec"][2][0], common["avec"][2][1], common["avec"][2][2]) + "\n" 
     
-    if "spinpol" in inp:
-        if inp["spinpol"] == 0: elk_in += "spinpol\n.false.\n\n"
+    if "scale" in private:
+        elk_in += "scale\n" + "%16.10f\n"%private["scale"] + "\n"
+    elif "scale" in common:
+        elk_in += "scale\n" + "%16.10f\n"%common["scale"] + "\n"
+
+    if "nempty" in common:
+        elk_in += "nempty\n" + "%i\n"%common["nempty"] + "\n"
+    
+    if "spinpol" in common:
+        if common["spinpol"] == 0: elk_in += "spinpol\n.false.\n\n"
         else: elk_in += "spinpol\n.true.\n\n"
 
-    elk_in += "atoms\n" + "%i\n"%len(inp["atoms"])
+    elk_in += "atoms\n" + "%i\n"%len(common["atoms"])
     
-    for atom_type in inp["atoms"]:
+    for atom_type in common["atoms"]:
         elk_in += atom_type[0] + "\n"
         elk_in += "%i\n"%len(atom_type[1])
         for atom in atom_type[1]:
@@ -43,56 +45,81 @@ def create_input_file(inp, conf):
     elk_in += "\n"
     
     ngridk = [2, 2, 2]
-    if "ngridk" in inp: ngridk = inp["ngridk"]
-    if "ngridk" in conf: ngridk = conf["ngridk"]
+    if "ngridk" in private: 
+        ngridk = private["ngridk"]
+    elif "ngridk" in common: ngridk = common["ngridk"]
 
     elk_in += "ngridk\n" + "%i %i %i\n"%(ngridk[0], ngridk[1], ngridk[2]) + "\n"
 
-    if "ldapu" in inp:
+    if "ldapu" in common:
         elk_in += "lda+u\n1 1\n"
-        for i in range(len(inp["ldapu"])):
-            elk_in += "%i %i %f %f\n"%(inp["ldapu"][i][0], inp["ldapu"][i][1], inp["ldapu"][i][2], inp["ldapu"][i][3])
+        for i in range(len(common["ldapu"])):
+            elk_in += "%i %i %f %f\n"%(common["ldapu"][i][0], common["ldapu"][i][1], common["ldapu"][i][2], common["ldapu"][i][3])
         elk_in += "\n"
 
-    if "aw_cutoff" in conf:
-        elk_in += "rgkmax\n"
-        elk_in += "%f\n"%conf["aw_cutoff"]
-        elk_in += "\n"
+    
+    if "aw_cutoff" in private:
+        elk_in += "rgkmax\n" + "%f\n"%private["aw_cutoff"] + "\n"
+    elif "aw_cutoff" in common:
+        elk_in += "rgkmax\n" + "%f\n"%common["aw_cutoff"] + "\n"
 
-    if "lmaxapw" in conf:
-        elk_in += "lmaxapw\n"
-        elk_in += "%i\n"%conf["lmaxapw"]
-        elk_in += "\n"
+    if "lmaxapw" in private:
+        elk_in += "lmaxapw\n" + "%i\n"%private["lmaxapw"] + "\n"
+    elif "lmaxapw" in common:
+        elk_in += "lmaxapw\n" + "%i\n"%common["lmaxapw"] + "\n"
+    
+    if "lmaxvr" in private:
+        elk_in += "lmaxvr\n" + "%i\n"%private["lmaxvr"] + "\n"
+    elif "lmaxvr" in common:
+        elk_in += "lmaxvr\n" + "%i\n"%common["lmaxvr"] + "\n"
+
+    elk_in += "autormt\n.true.\n"
     
     return elk_in
 
-def launch_task(irun, inp, conf):
+def launch_task(irun, common, private):
     path = "./" + str(irun)
     shutil.rmtree(path, True)
     os.mkdir(path)
 
     fout = open(path + "/elk.in", "w")
-    fout.write(create_input_file(inp, conf))
+    fout.write(create_input_file(common, private))
     fout.close()
 
     for i in glob.glob("[A-Z]*"):
         shutil.copy(i, path + "/" + i)
 
+    if "mpi_grid" in private: 
+        mpi_grid = private["mpi_grid"]
+    else: 
+        mpi_grid = common["mpi_grid"]
+    
+    cyclic_block_size = 32
+    if "cyclic_block_size" in private: 
+        cyclic_block_size = private["cyclic_block_size"]
+    elif "cyclic_block_size" in common: 
+        cyclic_block_size = common["cyclic_block_size"]
+
+    if "num_threads" in private: 
+        num_threads = private["num_threads"]
+    else: 
+        num_threads = common["num_threads"]
+
     fout = open(path + "/sirius.json", "w")
     fout.write("{\n")
-    fout.write("    \"mpi_grid_dims\": " + str(conf["mpi_grid"]) + ",\n")
-    fout.write("    \"cyclic_block_size\" : " + str(conf["cyclic_block_size"]) + "\n")
+    fout.write("    \"mpi_grid_dims\": " + str(mpi_grid) + ",\n")
+    fout.write("    \"cyclic_block_size\" : " + str(cyclic_block_size) + "\n")
     fout.write("}\n")
     fout.close()
 
-    np = 1
-    for i in range(len(conf["mpi_grid"])): np = np * conf["mpi_grid"][i]
+    num_proc = 1
+    for i in range(len(mpi_grid)): num_proc *= mpi_grid[i]
 
     # simple execution
     new_env = copy.deepcopy(os.environ)
-    new_env["OMP_NUM_THREADS"] = str(conf["num_threads"])
+    new_env["OMP_NUM_THREADS"] = str(num_threads)
     fstdout = open(path + "/output.txt", "w");
-    p = subprocess.Popen(["mpirun", "-np", str(np), "../elk"], cwd=path, env=new_env, stdout=fstdout)
+    p = subprocess.Popen(["mpirun", "-np", str(num_proc), "../elk"], cwd=path, env=new_env, stdout=fstdout)
     p.wait()
     fstdout.close()
 
@@ -102,16 +129,16 @@ def launch_task(irun, inp, conf):
     #fout.write("#SBATCH --time=00:05:00\n")
     #fout.write("#SBATCH --ntasks=" + str(np) + "\n")
     #fout.write("#SBATCH --ntasks-per-node=1\n")
-    #fout.write("#SBATCH --cpus-per-task=" + str(conf["num_threads"]) + "\n")
+    #fout.write("#SBATCH --cpus-per-task=" + str(private["num_threads"]) + "\n")
     #fout.write("#SBATCH --job-name=test-sirius\n")
     #fout.write("#SBATCH --output=output-%j.txt\n")
     #fout.write("#SBATCH --error=output-%j.txt\n")
     #fout.write("#SBATCH --account=s299\n")
-    #fout.write("export NCPUS=" + str(conf["num_threads"]) + "\n")
-    #fout.write("export GOTO_NUM_THREADS=" + str(conf["num_threads"]) + "\n")
-    #fout.write("export OMP_NUM_THREADS=" + str(conf["num_threads"]) + "\n")
+    #fout.write("export NCPUS=" + str(private["num_threads"]) + "\n")
+    #fout.write("export GOTO_NUM_THREADS=" + str(private["num_threads"]) + "\n")
+    #fout.write("export OMP_NUM_THREADS=" + str(private["num_threads"]) + "\n")
     #fout.write("date\n")
-    #fout.write("aprun -n " + str(np) + " -N 1 -d " + str(conf["num_threads"]) + " ../elk\n")
+    #fout.write("aprun -n " + str(np) + " -N 1 -d " + str(private["num_threads"]) + " ../elk\n")
     #fout.write("date\n")
     #fout.close()
     #p = subprocess.Popen(["sbatch", "batch_job.slrm"], cwd=path)
@@ -124,21 +151,12 @@ def main():
     jin = json.load(fin)
     fin.close()
 
-    for irun in range(len(jin["run"])):
+    for irun in range(len(jin["private"])):
         
-        inp = copy.deepcopy(jin["input"])
+        common = copy.deepcopy(jin["common"])
 
-        launch_task(irun, inp, jin["run"][irun])
+        launch_task(irun, common, jin["private"][irun])
 
-
-
-### run tests one by one on one or multiple nodes;
-### test is described by: elk input + mpi_grid conf + num_theads
-### at this point we want to test: 
-###   LSDA collinear vs LSDA at arbitrary direction
-###   LSDA+U collinear vs LSDA+U at arbitrary direction
-###   LSDA+SO for [100] [010] and [001] directions
-###  test is a series of runs
 
 if __name__ == "__main__":
     main()
