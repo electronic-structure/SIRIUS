d4f2b3714b80912a502abf577c555b038874b5e1
toxa81 <toxa81@gmail.com>
2013-05-15 13:20:39 +0200

    new gpu api

--- src/gpu_interface.h
+++ src/gpu_interface.h
@@ -40,5 +40,26 @@ extern "C" void magma_zhegvdx_2stage_wrapper(int32_t matrix_size, int32_t nv, vo
                                              void* b, int32_t ldb, double* eval);
 #endif
 
+extern "C" void cuda_create_streams(int num_streams);
+
+extern "C" void cuda_destroy_streams(int num_streams);
+
+extern "C" void cuda_stream_synchronize(int stream_id);
+
+extern "C" void cuda_async_copy_to_device(void *target, void *source, size_t size, int stream_id);
+
+extern "C" void cuda_async_copy_to_host(void *target, void *source, size_t size, int stream_id);
+
+template <typename T>
+void spline_inner_product_gpu(int size, double* r_dr, T* s1_coefs, T* s2_coefs);
+
+void bessel_lo_inner_product_gpu(int num_gkvec, int num_rowcol, int max_num_mt_points, int lmax_pw, int num_atom_types, 
+                                 double* bessel, double* lo, int* l_by_ilo, int* iat_by_ilo, int* nmtp_by_ilo, 
+                                 double* r_dr, double* result);
+
+void bessel_vlm_inner_product_gpu(int max_num_mt_points, int lmax_pot, int lmmax_pot, int num_atoms, int num_atom_types, 
+                                  int* iat_by_ia, int* nmtp_by_ia, int* l_by_lm, double* r_dr, double* jl_coefs, 
+                                  double* vlm_coefs, double* vjlm, int stream_id);
+
 #endif // _GPU_INTERFACE_H_
 
