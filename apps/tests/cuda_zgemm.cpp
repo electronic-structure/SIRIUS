34e60609b4d08955cb887c66b827c18a1c5ea766
toxa81 <toxa81@gmail.com>
2013-11-13 10:39:49 +0100

    update of zgemm test

--- apps/tests/cuda_zgemm.cpp
+++ apps/tests/cuda_zgemm.cpp
@@ -1,25 +1,36 @@
 #include <sirius.h>
 
-void test_gemm()
+void test_gemm(int N, mdarray<complex16, 2>& c)
 {
     sirius::Timer t("test_gemm"); 
-
-    mdarray<complex16, 2> a(2000, 2000);
-    mdarray<complex16, 2> b(2000, 2000);
-    mdarray<complex16, 2> c(2000, 2000);
     
-    memset(&a(0, 0), 1, 2000*2000*16);
-    memset(&b(0, 0), 1, 2000*2000*16);
+    mdarray<complex16, 2> a(N, N);
+    mdarray<complex16, 2> b(N, N);
 
+    //#pragma omp parallel for
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            a(j, i) = complex16(double(rand()) / RAND_MAX, double(rand()) / RAND_MAX);
+            b(j, i) = complex16(double(rand()) / RAND_MAX, double(rand()) / RAND_MAX);
+        }
+    }
+
+    sirius::Timer t1("gemm_only"); 
+    std::cout << "start of gemm loop" << std::endl;
     for (int i = 0; i < 10; i++)
     {
-        blas<cpu>::gemm(0, 0, 2000, 2000, 2000, a.get_ptr(), a.ld(), b.get_ptr(), b.ld(), c.get_ptr(), c.ld());
+        std::cout << "i = " << i << std::endl;
+        blas<cpu>::gemm(0, 0, N, N, N, a.get_ptr(), a.ld(), b.get_ptr(), b.ld(), c.get_ptr(), c.ld());
     }
+    std::cout << "end of gemm loop" << std::endl;
 }
 
 int main(int argn, char **argv)
 {
-    Platform::initialize(true);
+    bool init_cublas = (argn == 1) ? true : false;
+    Platform::initialize(true, init_cublas);
 
     //== mdarray<complex16, 2> a(2000, 2000);
     //== mdarray<complex16, 2> b(2000, 2000);
@@ -43,8 +54,10 @@ int main(int argn, char **argv)
     //== c.copy_to_host();
     //== std::cout << "after zgemm" << std::endl;
     //== t.stop();
-
-    test_gemm();
+    
+    int N = 3000;
+    mdarray<complex16, 2> c(N, N);
+    test_gemm(N, c);
 
     sirius::Timer::print();
 
