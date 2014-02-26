7603dce5b0abd7c67e88347413e6648a80a35975
toxa81 <toxa81@gmail.com>
2014-02-26 13:43:56 +0100

    resurrecting the full-potential GPU implementation

---
+++ apps/utils/unit_cell_tools.cpp
@@ -0,0 +1,96 @@
+#include <sirius.h>
+
+using namespace sirius;
+
+int main(int argn, char** argv)
+{
+    Platform::initialize(1);
+    Global p;
+    p.read_unit_cell_input();
+
+    if (argn > 2)
+    {
+        int scell[3][3];
+
+        std::string key(argv[1]);
+        std::string value(argv[2]);
+
+        if (key == "--supercell")
+        {
+            std::stringstream s(value);
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++) s >> scell[i][j];
+            }
+            
+            std::cout << std::endl;
+            std::cout << "supercell vectors (lattice coordinates) : " << std::endl;
+            for (int i = 0; i < 3; i++)
+            {
+                std::cout << "A" << i << " : ";
+                for (int j = 0; j < 3; j++) std::cout << scell[i][j] << " ";
+                std::cout << std::endl;
+            }
+
+            double scell_lattice_vectors[3][3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    scell_lattice_vectors[i][x] = scell[i][0] * p.unit_cell()->lattice_vectors(0, x) +
+                                                  scell[i][1] * p.unit_cell()->lattice_vectors(1, x) +
+                                                  scell[i][2] * p.unit_cell()->lattice_vectors(2, x);
+                }
+            }
+
+            std::cout << "supercell vectors (Cartesian coordinates) : " << std::endl;
+            for (int i = 0; i < 3; i++)
+            {
+                std::cout << "A" << i << " : ";
+                for (int x = 0; x < 3; x++) std::cout << scell_lattice_vectors[i][x] << " ";
+                std::cout << std::endl;
+            }
+
+            std::cout << "volume ratio : " << std::abs(matrix3d<int>(scell).det()) << std::endl;
+
+            Global psc;
+            psc.unit_cell()->set_lattice_vectors(scell_lattice_vectors[0], scell_lattice_vectors[1], scell_lattice_vectors[2]);
+    
+            for (int iat = 0; iat < (int)p.unit_cell_input_section_.labels_.size(); iat++)
+            {
+                psc.unit_cell()->add_atom_type(iat, p.unit_cell_input_section_.labels_[iat], p.esm_type());
+                for (int ia = 0; ia < (int)p.unit_cell_input_section_.coordinates_[iat].size(); ia++)
+                {
+                    vector3d<double> va(&p.unit_cell_input_section_.coordinates_[iat][ia][0]);
+
+                    for (int i0 = -4; i0 <= 4; i0++)
+                    {
+                        for (int i1 = -4; i1 <= 4; i1++)
+                        {
+                            for (int i2 = -4; i2 <= 4; i2++)
+                            {
+                                vector3d<double> T(i0, i1, i2);
+                                vector3d<double> vc = p.unit_cell()->get_cartesian_coordinates(va + T);
+                                vector3d<double> vf = psc.unit_cell()->get_fractional_coordinates(vc);
+
+                                auto vr = Utils::reduce_coordinates(vf);
+
+                                if (psc.unit_cell()->atom_id_by_position(vr.first) == -1) psc.unit_cell()->add_atom(iat, &vr.first[0]);
+                            }
+                        }
+                    }
+                }
+            }
+            
+            psc.unit_cell()->get_symmetry();
+            psc.unit_cell()->print_info();
+            psc.unit_cell()->write_json();
+        }
+    }
+
+    //p.unit_cell()->get_symmetry();
+    //p.unit_cell()->print_info();
+
+    Platform::finalize();
+
+}
