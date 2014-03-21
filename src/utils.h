758cd7983e1953ee41be14e3450d0d4ae7edafd2
toxa81 <toxa81@gmail.com>
2014-03-21 14:57:42 +0100

    small fix

--- src/utils.h
+++ src/utils.h
@@ -74,7 +74,7 @@ class Utils
             char buf[100];
         
             int len = snprintf(buf, 100, "%.14f", argument);
-            for (int i = len; i >= 1; i--) 
+            for (int i = len - 1; i >= 1; i--) 
             {
                 if (buf[i] == '0' && buf[i - 1] == '0') 
                 {
