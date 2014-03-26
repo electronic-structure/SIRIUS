5a60b0316c08cf53185c95c679fe47f852f0a40c
toxa81 <toxa81@gmail.com>
2014-03-26 12:01:12 +0100

    update of the tests

--- apps/tests/splindex.cpp
+++ apps/tests/splindex.cpp
@@ -18,8 +18,26 @@ void test1(void)
             printf(" global index : %i => local index : %i, rank: %i\n", k, iloc, rank);
         }
     }
+}
 
-
+void test1a(void)
+{
+    printf("\n");
+    printf("split 17 elements between 4 ranks in block-cyclic distribution\n");
+    printf("\n");
+    for (int i = 0; i < 4; i++)
+    {
+        splindex<block_cyclic> spl(17, 4, i, 2);
+        printf("rank : %i, local size : %i\n", i, spl.local_size());
+        
+        printf("local index and rank for each element:\n");
+        for (int k = 0; k < 17; k++)
+        {
+            int iloc = spl.location(_splindex_offs_, k);
+            int rank = spl.location(_splindex_rank_, k);
+            printf(" global index : %i => local index : %i, rank: %i\n", k, iloc, rank);
+        }
+    }
 }
 
 void test2()
@@ -33,8 +51,9 @@ void test2()
         splindex<block> spl(17, 4, i);
         
         #pragma omp parallel
-        for (auto it = spl.begin(); it.valid(); it++)
+        for (auto it = splindex_iterator<block>(spl); it.valid(); it++)
         {
+            #pragma omp flush
             printf("thread_id: %i, local index : %i, global index : %i\n", Platform::thread_id(), it.idx_local(), it.idx());
         }
     }
@@ -42,8 +61,82 @@ void test2()
 
 }
 
+void test3()
+{
+    printf("\n");
+    printf("test3\n");
+    for (int num_ranks = 1; num_ranks < 17; num_ranks++)
+    {
+        for (int N = 1; N < 113; N++)
+        {
+            splindex<block> spl(N, num_ranks);
+            int sz = 0;
+            for (int i = 0; i < num_ranks; i++) sz += spl.local_size(i);
+            if (sz != N) 
+            {
+                std::cout << "wrong sum of local sizes" << std::endl;
+                exit(0);
+            }
+            for (int i = 0; i < N; i++)
+            {
+                int rank = spl.location(_splindex_rank_, i);
+                int offset = spl.location(_splindex_offs_, i);
+                if (i != spl.global_index(offset, rank))
+                {
+                    std::cout << "wrong index" << std::endl;
+                    exit(0);
+                }
+            }
+        }
+    }
+}
+
+void test4()
+{
+    printf("\n");
+    printf("test4\n");
+    for (int bs = 1; bs < 17; bs++)
+    {
+        for (int num_ranks = 1; num_ranks < 13; num_ranks++)
+        {
+            for (int N = 1; N < 1113; N++)
+            {
+                splindex<block_cyclic> spl(N, num_ranks, bs);
+                int sz = 0;
+                for (int i = 0; i < num_ranks; i++) sz += spl.local_size(i);
+                if (sz != N) 
+                {
+                    std::cout << "wrong sum of local sizes" << std::endl;
+                    exit(0);
+                }
+
+                for (int i = 0; i < N; i++)
+                {
+                    int rank = spl.location(_splindex_rank_, i);
+                    int offset = spl.location(_splindex_offs_, i);
+                    if (i != spl.global_index(offset, rank))
+                    {
+                        std::cout << "wrong index" << std::endl;
+                        std::cout << "bs = " << bs << std::endl
+                                  << "num_ranks =  " << num_ranks << std::endl
+                                  << "N = " << N << std::endl
+                                  << "idx = " << i << std::endl
+                                  << "rank = " << rank << std::endl
+                                  << "offset = " << offset << std::endl
+                                  << "computed index = " << spl.global_index(offset, rank) << std::endl;
+                        exit(0);
+                    }
+                }
+            }
+        }
+    }
+}
+
 int main(int argn, char** argv)
 {
     test1();
+    test1a();
     test2();
+    test3();
+    test4();
 }
---
+++ apps/tests/test_zgemm.cpp
@@ -0,0 +1,70 @@
+#include <sirius.h>
+
+void test_gemm(int M, int N, int K, mdarray<double_complex, 2>& c)
+{
+    sirius::Timer t("test_gemm"); 
+    
+    mdarray<double_complex, 2> a(M, K);
+    mdarray<double_complex, 2> b(K, N);
+
+    for (int j = 0; j < K; j++)
+    {
+        for (int i = 0; i < N; i++) a(i, j) = type_wrapper<double_complex>::random();
+    }
+    for (int j = 0; j < N; j++)
+    {
+        for (int i = 0; i < K; i++) b(i, j) = type_wrapper<double_complex>::random();
+    }
+
+    sirius::Timer t1("gemm_only"); 
+    blas<cpu>::gemm(0, 0, M, N, K, a.ptr(), a.ld(), b.ptr(), b.ld(), c.ptr(), c.ld());
+    t1.stop();
+    printf("performance (GFlops) : %12.6f\n", double(8 * M * N * K) / 1e9 / t1.value());
+}
+
+int main(int argn, char **argv)
+{
+    if (argn != 4)
+    {
+        printf("Usage: %s M N K\n", argv[0]);
+        exit(0);
+    }
+    int M, N, K;
+    std::istringstream(argv[1]) >> M;
+    std::istringstream(argv[2]) >> N;
+    std::istringstream(argv[3]) >> K;
+       
+    bool init_cublas = (argn == 1) ? true : false;
+    Platform::initialize(true, init_cublas);
+
+    //== mdarray<double_complex, 2> a(2000, 2000);
+    //== mdarray<double_complex, 2> b(2000, 2000);
+    //== mdarray<double_complex, 2> c(2000, 2000);
+
+
+    //== a.allocate_on_device();
+    //== b.allocate_on_device();
+    //== c.allocate_on_device();
+    //== 
+    //== double_complex zone(1, 0);
+    //== double_complex zzero(0, 0);
+
+    //== sirius::Timer t("zgemm", false);
+    //== t.start();
+    //== for (int i = 0; i < 10; i++)
+    //== {
+    //==     blas<gpu>::gemm(0, 0, 2000, 2000, 2000, &zone, a.get_ptr_device(), a.ld(), b.get_ptr_device(), b.ld(), 
+    //==                     &zzero, c.get_ptr_device(), c.ld());
+    //== }
+    //== c.copy_to_host();
+    //== std::cout << "after zgemm" << std::endl;
+    //== t.stop();
+    
+    //int N = 3000;
+    mdarray<double_complex, 2> c(M, N);
+    test_gemm(M, N, K, c);
+
+    //sirius::Timer::print();
+
+    Platform::finalize();
+}
