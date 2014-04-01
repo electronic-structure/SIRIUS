8a7a8b2d6dd0927434145d0b1689aa45a3bfbb04
toxa81 <toxa81@gmail.com>
2014-04-01 17:01:26 +0200

    update of plot utility

--- apps/dft_loop/plot.cpp
+++ apps/dft_loop/plot.cpp
@@ -3,6 +3,47 @@
 int main(int argn, char** argv)
 {
     using namespace sirius;
+
+    cmd_args args;
+    args.register_key("--origin=", "{vector3d} plot origin");
+    args.register_key("--b1=", "{vector3d} 1st boundary vector");
+    args.register_key("--b2=", "{vector3d} 2nd boundary vector");
+    args.register_key("--N1=", "{int} number of 1st boundary vector divisions");
+    args.register_key("--N2=", "{int} number of 2nd boundary vector divisions");
+    args.register_key("--coordinates=", "{cart | frac} Cartesian or fractional coordinates");
+    args.parse_args(argn, argv);
+    
+    if (argn == 1)
+    {
+        printf("Usage: ./plot [options] \n");
+        args.print_help();
+        exit(0);
+    }
+
+    vector3d<double> origin = args.value< vector3d<double> >("origin");
+    vector3d<double> b1 = args.value< vector3d<double> >("b1");
+    vector3d<double> b2 = args.value< vector3d<double> >("b2");
+
+    int N1 = args.value<int>("N1");
+    int N2 = args.value<int>("N2");
+
+
+    std::string coords = args["coordinates"];
+    bool cart;
+    if (coords == "cart")
+    {
+        cart = true;
+    }
+    else if (coords == "frac")
+    {
+        cart = false;
+    }
+    else
+    {
+        terminate(__FILE__, __LINE__, "wrong type of coordinates");
+        cart = false; // make compiler happy
+    }
+
     
     Platform::initialize(1);
 
@@ -40,37 +81,36 @@ int main(int argn, char** argv)
 
         density->generate_pw_coefs();
 
-
-
-        vector3d<double> p0(0.0, 0.0, 0);
-        vector3d<double> p1(5, 0, 0);
-        vector3d<double> p2(0, 5, 0);
-
-        int N1 = 200;
-        int N2 = 200;
+        splindex<block> spl_N2(N2, Platform::num_mpi_ranks(), Platform::mpi_rank());
 
         mdarray<double, 2> rho(N1, N2);
-        rho.zero();
-
-        for (int i1 = 0; i1 < N1 - 1; i1++)
+        
+        Timer t1("compute_density");
+        for (int j2 = 0; j2 < spl_N2.local_size(); j2++)
         {
-            for (int i2 = 0; i2 < N2 - 1; i2++)
+            int i2 = spl_N2[j2];
+
+            #pragma omp parallel for
+            for (int i1 = 0; i1 < N1; i1++)
             {
                 vector3d<double> v;
-                for (int x = 0; x < 3; x++) v[x] = p0[x] + double(i1) * p1[x] / N1 + double(i2) * p2[x] / N2;
-                //auto vc = parameters.unit_cell()->get_cartesian_coordinates(v);
+                for (int x = 0; x < 3; x++) v[x] = origin[x] + double(i1) * b1[x] / (N1 - 1) + 
+                                                               double(i2) * b2[x] / (N2 - 1);
+
+                if (!cart) v = parameters.unit_cell()->get_cartesian_coordinates(v);
+
                 rho(i1, i2) = density->rho()->value(parameters, v);
             }
         }
+        t1.stop();
 
-        HDF5_tree h5out("rho.h5", true);
-        h5out.write("rho", rho);
+        Platform::allgather(&rho(0, 0), spl_N2.global_offset() * N1, spl_N2.local_size() * N1, MPI_COMM_WORLD);
 
-
-
-
-
-
+        if (Platform::mpi_rank() == 0)
+        {
+            HDF5_tree h5out("rho.h5", true);
+            h5out.write("rho", rho);
+        }
 
         delete density;
         delete potential;
