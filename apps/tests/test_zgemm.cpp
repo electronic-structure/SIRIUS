86c93c93c98e68e642aeccbcf74fa6794aff9a96
toxa81 <toxa81@gmail.com>
2014-04-01 11:26:59 +0200

    update of test_zgemm.cpp

--- apps/tests/test_zgemm.cpp
+++ apps/tests/test_zgemm.cpp
@@ -16,7 +16,7 @@ void test_gemm(int M, int N, int K, mdarray<double_complex, 2>& c)
         for (int i = 0; i < K; i++) b(i, j) = type_wrapper<double_complex>::random();
     }
 
-    printf("testing zgemm with M, N, K = %i, %i, %i\n", M, N, K);
+    printf("testing serial zgemm with M, N, K = %i, %i, %i\n", M, N, K);
     sirius::Timer t1("gemm_only"); 
     blas<cpu>::gemm(0, 0, M, N, K, a.ptr(), a.ld(), b.ptr(), b.ld(), c.ptr(), c.ld());
     t1.stop();
@@ -24,52 +24,93 @@ void test_gemm(int M, int N, int K, mdarray<double_complex, 2>& c)
     printf("performance (GFlops) : %12.6f\n", 8e-9 * M * N * K / t1.value());
 }
 
+#ifdef _SCALAPACK_
+void test_pgemm(int M, int N, int K, int nrow, int ncol)
+{
+    int context = linalg<scalapack>::create_blacs_context(MPI_COMM_WORLD);
+    Cblacs_gridinit(&context, "C", nrow, ncol);
+
+    dmatrix<double_complex> a(M, K, context);
+    dmatrix<double_complex> b(K, N, context);
+    dmatrix<double_complex> c(M, N, context);
+    c.zero();
+
+    for (int ic = 0; ic < a.num_cols_local(); ic++)
+    {
+        for (int ir = 0; ir < a.num_rows_local(); ir++) a(ir, ic) = type_wrapper<double_complex>::random();
+    }
+    for (int ic = 0; ic < b.num_cols_local(); ic++)
+    {
+        for (int ir = 0; ir < b.num_rows_local(); ir++) b(ir, ic) = type_wrapper<double_complex>::random();
+    }
+
+    if (Platform::mpi_rank() == 0)
+    {
+        printf("testing serial zgemm with M, N, K = %i, %i, %i\n", M, N, K);
+    }
+    sirius::Timer t1("gemm_only"); 
+    blas<cpu>::gemm(0, 0, M, N, K, complex_one, a, b, complex_zero, c);
+    t1.stop();
+    if (Platform::mpi_rank() == 0)
+    {
+        printf("execution time (sec) : %12.6f\n", t1.value());
+        printf("performance (GFlops) : %12.6f\n", 8e-9 * M * N * K / t1.value() / nrow / ncol);
+    }
+
+    linalg<scalapack>::free_blacs_context(context);
+}
+#endif
+
 int main(int argn, char **argv)
 {
-    if (argn != 4)
+    cmd_args args;
+    args.register_key("--M=", "{int} M");
+    args.register_key("--N=", "{int} N");
+    args.register_key("--K=", "{int} K");
+    args.register_key("--nrow=", "{int} number of row MPI ranks");
+    args.register_key("--ncol=", "{int} number of column MPI ranks");
+    args.register_key("--bs=", "{int} cyclic block size");
+
+    args.parse_args(argn, argv);
+    if (argn == 1)
     {
-        printf("Usage: %s M N K\n", argv[0]);
+        printf("Usage: %s [options]\n", argv[0]);
+        args.print_help();
         exit(0);
     }
-    int M, N, K;
-    std::istringstream(argv[1]) >> M;
-    std::istringstream(argv[2]) >> N;
-    std::istringstream(argv[3]) >> K;
-       
-    bool init_cublas = (argn == 1) ? true : false;
-    Platform::initialize(true, init_cublas);
 
-    //== mdarray<double_complex, 2> a(2000, 2000);
-    //== mdarray<double_complex, 2> b(2000, 2000);
-    //== mdarray<double_complex, 2> c(2000, 2000);
+    int nrow = 1;
+    int ncol = 1;
 
+    if (args.exist("nrow")) nrow = args.value<int>("nrow");
+    if (args.exist("ncol")) ncol = args.value<int>("ncol");
 
-    //== a.allocate_on_device();
-    //== b.allocate_on_device();
-    //== c.allocate_on_device();
-    //== 
-    //== double_complex zone(1, 0);
-    //== double_complex zzero(0, 0);
+    int M = args.value<int>("M");
+    int N = args.value<int>("N");
+    int K = args.value<int>("K");
 
-    //== sirius::Timer t("zgemm", false);
-    //== t.start();
-    //== for (int i = 0; i < 10; i++)
-    //== {
-    //==     blas<gpu>::gemm(0, 0, 2000, 2000, 2000, &zone, a.get_ptr_device(), a.ld(), b.get_ptr_device(), b.ld(), 
-    //==                     &zzero, c.get_ptr_device(), c.ld());
-    //== }
-    //== c.copy_to_host();
-    //== std::cout << "after zgemm" << std::endl;
-    //== t.stop();
-    
-    mdarray<double_complex, 2> c(M, N);
-    for (int j = 0; j < N; j++)
+    Platform::initialize(true);
+
+    if (nrow * ncol == 1)
     {
-        for (int i = 0; i < M; i++) c(i, j) = complex_zero;
+        mdarray<double_complex, 2> c(M, N);
+        for (int j = 0; j < N; j++)
+        {
+            for (int i = 0; i < M; i++) c(i, j) = complex_zero;
+        }
+        test_gemm(M, N, K, c);
     }
-    test_gemm(M, N, K, c);
-
-    //sirius::Timer::print();
+    else
+    {
+        #ifdef _SCALAPACK_
+        int bs = args.value<int>("bs");
+        linalg<scalapack>::set_cyclic_block_size(bs);
+        splindex<block_cyclic>::set_cyclic_block_size(bs);
+        test_pgemm(M, N, K, nrow, ncol);
+        #else
+        terminate(__FILE__, __LINE__, "not compiled with ScaLAPACK support");
+        #endif
+    }
 
     Platform::finalize();
 }
