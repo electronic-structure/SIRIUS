9db64e52e6f5d3f24116682b20458b8e96ae78fe
toxa81 <toxa81@gmail.com>
2014-04-14 16:18:46 +0200

    fix in plot

--- apps/dft_loop/plot.py
+++ apps/dft_loop/plot.py
@@ -9,7 +9,7 @@ f = h5py.File("rho.h5", "r")
 d = f["/rho"]
 
 plt.axis('off')
-plt.imshow(d, cmap = cmap, norm = norm)
+plt.imshow(d, cmap = cmap, norm = norm, origin = 'lower')
 plt.colorbar()
 plt.contour(d, [1, 5, 10, 20, 30, 40, 50])
 plt.savefig("rho.pdf", format="pdf")
