96d5503c11a76dec41a6e012a879e0737ece1b74
toxa81 <toxa81@gmail.com>
2014-05-22 15:55:34 +0200

    update of dft_loop example

--- apps/dft_loop/dft_loop.cpp
+++ apps/dft_loop/dft_loop.cpp
@@ -1,114 +1,118 @@
 #include <sirius.h>
 
-int main(int argn, char** argv)
+using namespace sirius;
+
+void dft_loop()
 {
-    using namespace sirius;
+    Global parameters;
+
+    JSON_tree parser("sirius.json");
+
+    parameters.read_unit_cell_input();
+
+    parameters.set_lmax_apw(parser["lmax_apw"].get(10));
+    parameters.set_lmax_pot(parser["lmax_pot"].get(10));
+    parameters.set_lmax_rho(parser["lmax_rho"].get(10));
+    parameters.set_pw_cutoff(parser["pw_cutoff"].get(20.0));
+    parameters.set_aw_cutoff(parser["aw_cutoff"].get(7.0));
+    parameters.set_gk_cutoff(parser["gk_cutoff"].get(7.0));
     
-    Platform::initialize(1);
+    parameters.unit_cell()->set_auto_rmt(parser["auto_rmt"].get(0));
+    int num_mag_dims = parser["num_mag_dims"].get(0);
+    int num_spins = (num_mag_dims == 0) ? 1 : 2;
     
+    parameters.set_num_mag_dims(num_mag_dims);
+    parameters.set_num_spins(num_spins);
+
+    parameters.initialize();
+    
+    Potential* potential = new Potential(parameters);
+    potential->allocate();
+
+    std::vector<int> ngridk = parser["ngridk"].get(std::vector<int>(3, 1));
+        
+    int numkp = ngridk[0] * ngridk[1] * ngridk[2];
+    int ik = 0;
+    mdarray<double, 2> kpoints(3, numkp);
+    std::vector<double> kpoint_weights(numkp);
+
+    for (int i0 = 0; i0 < ngridk[0]; i0++) 
     {
-        Global parameters;
-
-        JSON_tree parser("sirius.json");
-
-        parameters.read_unit_cell_input();
-
-        parameters.set_lmax_apw(parser["lmax_apw"].get(10));
-        parameters.set_lmax_pot(parser["lmax_pot"].get(10));
-        parameters.set_lmax_rho(parser["lmax_rho"].get(10));
-        parameters.set_pw_cutoff(parser["pw_cutoff"].get(20.0));
-        parameters.set_aw_cutoff(parser["aw_cutoff"].get(7.0));
-        parameters.set_gk_cutoff(parser["gk_cutoff"].get(7.0));
-        
-        parameters.unit_cell()->set_auto_rmt(parser["auto_rmt"].get(0));
-        int num_mag_dims = parser["num_mag_dims"].get(0);
-        int num_spins = (num_mag_dims == 0) ? 1 : 2;
-        
-        parameters.set_num_mag_dims(num_mag_dims);
-        parameters.set_num_spins(num_spins);
-
-        parameters.initialize();
-        
-        Potential* potential = new Potential(parameters);
-        potential->allocate();
-
-        std::vector<int> ngridk = parser["ngridk"].get(std::vector<int>(3, 1));
-            
-        int numkp = ngridk[0] * ngridk[1] * ngridk[2];
-        int ik = 0;
-        mdarray<double, 2> kpoints(3, numkp);
-        std::vector<double> kpoint_weights(numkp);
-
-        for (int i0 = 0; i0 < ngridk[0]; i0++) 
+        for (int i1 = 0; i1 < ngridk[1]; i1++) 
         {
-            for (int i1 = 0; i1 < ngridk[1]; i1++) 
+            for (int i2 = 0; i2 < ngridk[2]; i2++)
             {
-                for (int i2 = 0; i2 < ngridk[2]; i2++)
-                {
-                    kpoints(0, ik) = double(i0) / ngridk[0];
-                    kpoints(1, ik) = double(i1) / ngridk[1];
-                    kpoints(2, ik) = double(i2) / ngridk[2];
-                    kpoint_weights[ik] = 1.0 / numkp;
-                    ik++;
-                }
+                kpoints(0, ik) = double(i0) / ngridk[0];
+                kpoints(1, ik) = double(i1) / ngridk[1];
+                kpoints(2, ik) = double(i2) / ngridk[2];
+                kpoint_weights[ik] = 1.0 / numkp;
+                ik++;
             }
         }
-
-        K_set ks(parameters);
-        ks.add_kpoints(kpoints, &kpoint_weights[0]);
-        ks.initialize();
-        
-        Density* density = new Density(parameters);
-        density->allocate();
-        
-        if (Utils::file_exists(storage_file_name))
-        {
-            density->load();
-            potential->load();
-        }
-        else
-        {
-            density->initial_density();
-
-            switch(parameters.esm_type())
-            {
-                case full_potential_lapwlo:
-                case full_potential_pwlo:
-                {
-                    potential->generate_effective_potential(density->rho(), density->magnetization());
-                    break;
-                }
-                case ultrasoft_pseudopotential:
-                {
-                    potential->generate_effective_potential(density->rho(), density->rho_pseudo_core(), density->magnetization());
-                    break;
-                }
-                default:
-                {
-                    stop_here
-                }
-            }
-
-        }
-        
-        DFT_ground_state dft(parameters, potential, density, &ks);
-        double potential_tol = parser["potential_tol"].get(1e-4);
-        double energy_tol = parser["energy_tol"].get(1e-4);
-
-        dft.scf_loop(potential_tol, energy_tol, parser["num_dft_iter"].get(100));
-
-        //dft.relax_atom_positions();
-
-        parameters.write_json_output();
-
-        delete density;
-        delete potential;
-        
-        parameters.clear();
-
-        Timer::print();
     }
 
+    K_set ks(parameters);
+    ks.add_kpoints(kpoints, &kpoint_weights[0]);
+    ks.initialize();
+    
+    Density* density = new Density(parameters);
+    density->allocate();
+    
+    if (Utils::file_exists(storage_file_name))
+    {
+        density->load();
+        potential->load();
+    }
+    else
+    {
+        density->initial_density();
+
+        switch(parameters.esm_type())
+        {
+            case full_potential_lapwlo:
+            case full_potential_pwlo:
+            {
+                potential->generate_effective_potential(density->rho(), density->magnetization());
+                break;
+            }
+            case ultrasoft_pseudopotential:
+            {
+                potential->generate_effective_potential(density->rho(), density->rho_pseudo_core(), density->magnetization());
+                break;
+            }
+            default:
+            {
+                stop_here
+            }
+        }
+
+    }
+    
+    DFT_ground_state dft(parameters, potential, density, &ks);
+    double potential_tol = parser["potential_tol"].get(1e-4);
+    double energy_tol = parser["energy_tol"].get(1e-4);
+
+    dft.scf_loop(potential_tol, energy_tol, parser["num_dft_iter"].get(100));
+
+    //dft.relax_atom_positions();
+
+    parameters.write_json_output();
+
+    delete density;
+    delete potential;
+    
+    parameters.clear();
+
+    Timer::print();
+
+}
+
+int main(int argn, char** argv)
+{
+    Platform::initialize(1);
+
+    dft_loop();
+    
     Platform::barrier();
     Platform::finalize();
 }
