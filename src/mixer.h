e21160f31e3e13838beeeb209d56bfac731db6aa
toxa81 <toxa81@gmail.com>
2014-08-27 16:26:25 +0200

    update of communication layer

--- src/mixer.h
+++ src/mixer.h
@@ -58,6 +58,8 @@ class Mixer
         /// output buffer for the whole vector
         mdarray<double, 1> output_buffer_;
 
+        Communicator comm_;
+
         /// Return position in the list of vectors for the given mixing step.
         inline int offset(int step)
         {
@@ -73,7 +75,7 @@ class Mixer
                 //rms += pow(vectors_(i, offset(count_)) - vectors_(i, offset(count_ - 1)), 2);
                 rms += pow(vectors_(i, offset(count_)) - input_buffer_(i), 2);
             }
-            Platform::allreduce(&rms, 1);
+            comm_.allreduce(&rms, 1);
             rms = sqrt(rms / double(size_));
             return rms;
         }
@@ -83,19 +85,20 @@ class Mixer
             for (int i = 0; i < (int)spl_size_.local_size(); i++)
                 vectors_(i, offset(count_)) = beta__ * input_buffer_(i) + (1 - beta__) * vectors_(i, offset(count_ - 1));
 
-            Platform::allgather(&vectors_(0, offset(count_)), output_buffer_.ptr(), (int)spl_size_.global_offset(), 
-                                (int)spl_size_.local_size());
+            comm_.allgather(&vectors_(0, offset(count_)), output_buffer_.ptr(), (int)spl_size_.global_offset(), 
+                            (int)spl_size_.local_size());
         }
 
     public:
 
-        Mixer(size_t size__, int max_history__, double beta__) 
+        Mixer(size_t size__, int max_history__, double beta__, Communicator const& comm__)
             : size_(size__), 
               max_history_(max_history__), 
               beta_(beta__), 
-              count_(0)
+              count_(0),
+              comm_(comm__)
         {
-            spl_size_ = splindex<block>((int)size_, Platform::num_mpi_ranks(), Platform::mpi_rank());
+            spl_size_ = splindex<block>((int)size_, comm_.size(), comm_.rank());
             /* allocate input buffer (local size) */
             input_buffer_.set_dimensions(spl_size_.local_size());
             input_buffer_.allocate();
@@ -116,7 +119,7 @@ class Mixer
             assert(idx < size_t(1 << 31));
 
             auto offs_and_rank = spl_size_.location((int)idx);
-            if (offs_and_rank.second == Platform::mpi_rank()) input_buffer_(offs_and_rank.first) = value;
+            if (offs_and_rank.second == comm_.rank()) input_buffer_(offs_and_rank.first) = value;
         }
 
         inline double* output_buffer()
@@ -150,7 +153,10 @@ class Linear_mixer: public Mixer
     public:
         
         /// Constructor
-        Linear_mixer(size_t size__, double beta0__) : Mixer(size__, 2, beta0__), rms_prev_(0), beta0_(beta0__)
+        Linear_mixer(size_t size__, double beta0__, Communicator const& comm__) 
+            : Mixer(size__, 2, beta0__, comm__),
+              rms_prev_(0),
+              beta0_(beta0__)
         {
         }
 
@@ -207,7 +213,8 @@ class Broyden_mixer: public Mixer
     
     public:
 
-        Broyden_mixer(size_t size__, int max_history__, double beta__) : Mixer(size__, max_history__, beta__)
+        Broyden_mixer(size_t size__, int max_history__, double beta__, Communicator const& comm__) 
+            : Mixer(size__, max_history__, beta__, comm__)
         {
             residuals_.set_dimensions(spl_size_.local_size(), max_history__);
             residuals_.allocate();
@@ -245,7 +252,7 @@ class Broyden_mixer: public Mixer
                         S(j2, j1) = S(j1, j2);
                     }
                 }
-                Platform::allreduce(S.ptr(), (int)S.size());
+                comm_.allreduce(S.ptr(), (int)S.size());
                 for (int j1 = 0; j1 < N; j1++)
                 { 
                     for (int j2 = 0; j2 < N; j2++) S(j1, j2) /= size_;
@@ -304,148 +311,148 @@ class Broyden_mixer: public Mixer
         }
 };
 
-class Pulay_mixer: public Mixer
-{
-    private:
-
-        mdarray<double, 2> residuals_;
-    
-    public:
-
-        Pulay_mixer(size_t size__, int max_history__, double beta__) : Mixer(size__, max_history__, beta__)
-        {
-            residuals_.set_dimensions(spl_size_.local_size(), max_history__);
-            residuals_.allocate();
-        }
-
-        double mix()
-        {
-            Timer t("sirius::Pulay_mixer::mix");
-
-            for (int i = 0; i < (int)spl_size_.local_size(); i++) 
-                residuals_(i, offset(count_)) = input_buffer_(i) - vectors_(i, offset(count_));
-
-            int N = std::min(count_, max_history_ - 1);
-
-            if (count_ > max_history_)
-            {
-                mdarray<long double, 2> S(N, N);
-                S.zero();
-
-                for (int j1 = 0; j1 < N; j1++)
-                {
-                    for (int j2 = 0; j2 < N; j2++)
-                    {
-                        for (int i = 0; i < (int)spl_size_.local_size(); i++) 
-                        {
-                            S(j1, j2) += residuals_(i, offset(count_ - j1 - 1)) * residuals_(i, offset(count_ - j2 - 1));
-                        }
-                    }
-                }
-                Platform::allreduce(S.ptr(), (int)S.size());
-                for (int j1 = 0; j1 < N; j1++)
-                { 
-                    for (int j2 = 0; j2 < N; j2++) S(j1, j2) /= size_;
-                }
-
-                mdarray<double, 2> s(N + 1, N + 1);
-                s.zero();
-                for (int j = 0; j < N; j++)
-                {
-                    for (int i = 0; i < N; i++) s(i, j) = (double)S(i, j);
-                    s(j, N) = s(N, j) = 1.0;
-                }
-                
-                std::cout << "matrix of residuals : " << std::endl;
-                for (int i = 0; i <= N; i++)
-                {
-                    for (int j = 0; j <= N; j++) printf("%18.10f ", s(j, i));
-                    printf("\n");
-                }
-
-                linalg<lapack>::invert_ge(s.ptr(), N + 1);
-
-
-                //memcpy(&vectors_(0, offset(count_ + 1)), &vectors_(0, offset(count_)), spl_size_.local_size() * sizeof(double));
-                memset(&vectors_(0, offset(count_ + 1)), 0, spl_size_.local_size() * sizeof(double));
-                for (int j = 0; j < N; j++)
-                {
-                    for (int i = 0; i < (int)spl_size_.local_size(); i++)
-                    {
-                        vectors_(i, offset(count_ + 1)) += s(j, N) * vectors_(i, offset(count_ - j - 1));
-                        vectors_(i, offset(count_ + 1)) += beta_ * s(j, N) * residuals_(i, offset(count_ - j - 1));
-                    }
-                }
-
-                Platform::allgather(&vectors_(0, offset(count_ + 1)), output_buffer_.ptr(), (int)spl_size_.global_offset(), 
-                                    (int)spl_size_.local_size());
-
-
-                count_++;
-            }
-            else
-            {
-                count_++;
-                mix_linear(beta_);
-            }
-
-            return rms_deviation();
-        }
-};
-
-class Adaptive_mixer: public Mixer
-{
-    private:
-
-        //mdarray<double, 2> residuals_;
-    
-    public:
-
-        Adaptive_mixer(size_t size__, int max_history__, double beta__) : Mixer(size__, max_history__, beta__)
-        {
-            // residuals_.set_dimensions(spl_size_.local_size(), max_history__);
-            // residuals_.allocate();
-        }
-
-        double mix()
-        {
-            Timer t("sirius::Adaptive_mixer::mix");
-
-            //== for (int i = 0; i < spl_size_.local_size(); i++) 
-            //==     residuals_(i, offset(count_)) = input_buffer_(i) - vectors_(i, offset(count_));
-
-            count_++;
-
-            int N = std::min(count_, max_history_);
-
-            if (N > 1)
-            {
-                for (int j = 0; j <= 10; j++)
-                {
-                    //==double k0 = (1 - beta_) * double(j) / 10;
-                    //==double k1 = (1 - beta_) * double(10 - j) / 10;
-                    for (int i = 0; i < (int)spl_size_.local_size(); i++)
-                    {
-                        vectors_(i, offset(count_)) = 0.5 * (1 - beta_) * vectors_(i, offset(count_ - 2)) + 
-                                                      0.5 * (1 - beta_) * vectors_(i, offset(count_ - 1)) + 
-                                                      beta_ * input_buffer_(i); 
-                    }
-                    //==double rms = rms_deviation();
-                    //==if (Platform::mpi_rank() == 0) std::cout << " j = " << j << ", rms = " << rms << std::endl;
-
-                    Platform::allgather(&vectors_(0, offset(count_)), output_buffer_.ptr(), (int)spl_size_.global_offset(), 
-                                        (int)spl_size_.local_size());
-                }
-               
-            }
-            else
-            {
-                mix_linear(beta_);
-            }
-
-            return rms_deviation();
-        }
-};
+//== class Pulay_mixer: public Mixer
+//== {
+//==     private:
+//== 
+//==         mdarray<double, 2> residuals_;
+//==     
+//==     public:
+//== 
+//==         Pulay_mixer(size_t size__, int max_history__, double beta__) : Mixer(size__, max_history__, beta__)
+//==         {
+//==             residuals_.set_dimensions(spl_size_.local_size(), max_history__);
+//==             residuals_.allocate();
+//==         }
+//== 
+//==         double mix()
+//==         {
+//==             Timer t("sirius::Pulay_mixer::mix");
+//== 
+//==             for (int i = 0; i < (int)spl_size_.local_size(); i++) 
+//==                 residuals_(i, offset(count_)) = input_buffer_(i) - vectors_(i, offset(count_));
+//== 
+//==             int N = std::min(count_, max_history_ - 1);
+//== 
+//==             if (count_ > max_history_)
+//==             {
+//==                 mdarray<long double, 2> S(N, N);
+//==                 S.zero();
+//== 
+//==                 for (int j1 = 0; j1 < N; j1++)
+//==                 {
+//==                     for (int j2 = 0; j2 < N; j2++)
+//==                     {
+//==                         for (int i = 0; i < (int)spl_size_.local_size(); i++) 
+//==                         {
+//==                             S(j1, j2) += residuals_(i, offset(count_ - j1 - 1)) * residuals_(i, offset(count_ - j2 - 1));
+//==                         }
+//==                     }
+//==                 }
+//==                 Platform::allreduce(S.ptr(), (int)S.size());
+//==                 for (int j1 = 0; j1 < N; j1++)
+//==                 { 
+//==                     for (int j2 = 0; j2 < N; j2++) S(j1, j2) /= size_;
+//==                 }
+//== 
+//==                 mdarray<double, 2> s(N + 1, N + 1);
+//==                 s.zero();
+//==                 for (int j = 0; j < N; j++)
+//==                 {
+//==                     for (int i = 0; i < N; i++) s(i, j) = (double)S(i, j);
+//==                     s(j, N) = s(N, j) = 1.0;
+//==                 }
+//==                 
+//==                 std::cout << "matrix of residuals : " << std::endl;
+//==                 for (int i = 0; i <= N; i++)
+//==                 {
+//==                     for (int j = 0; j <= N; j++) printf("%18.10f ", s(j, i));
+//==                     printf("\n");
+//==                 }
+//== 
+//==                 linalg<lapack>::invert_ge(s.ptr(), N + 1);
+//== 
+//== 
+//==                 //memcpy(&vectors_(0, offset(count_ + 1)), &vectors_(0, offset(count_)), spl_size_.local_size() * sizeof(double));
+//==                 memset(&vectors_(0, offset(count_ + 1)), 0, spl_size_.local_size() * sizeof(double));
+//==                 for (int j = 0; j < N; j++)
+//==                 {
+//==                     for (int i = 0; i < (int)spl_size_.local_size(); i++)
+//==                     {
+//==                         vectors_(i, offset(count_ + 1)) += s(j, N) * vectors_(i, offset(count_ - j - 1));
+//==                         vectors_(i, offset(count_ + 1)) += beta_ * s(j, N) * residuals_(i, offset(count_ - j - 1));
+//==                     }
+//==                 }
+//== 
+//==                 Platform::allgather(&vectors_(0, offset(count_ + 1)), output_buffer_.ptr(), (int)spl_size_.global_offset(), 
+//==                                     (int)spl_size_.local_size());
+//== 
+//== 
+//==                 count_++;
+//==             }
+//==             else
+//==             {
+//==                 count_++;
+//==                 mix_linear(beta_);
+//==             }
+//== 
+//==             return rms_deviation();
+//==         }
+//== };
+//== 
+//== class Adaptive_mixer: public Mixer
+//== {
+//==     private:
+//== 
+//==         //mdarray<double, 2> residuals_;
+//==     
+//==     public:
+//== 
+//==         Adaptive_mixer(size_t size__, int max_history__, double beta__) : Mixer(size__, max_history__, beta__)
+//==         {
+//==             // residuals_.set_dimensions(spl_size_.local_size(), max_history__);
+//==             // residuals_.allocate();
+//==         }
+//== 
+//==         double mix()
+//==         {
+//==             Timer t("sirius::Adaptive_mixer::mix");
+//== 
+//==             //== for (int i = 0; i < spl_size_.local_size(); i++) 
+//==             //==     residuals_(i, offset(count_)) = input_buffer_(i) - vectors_(i, offset(count_));
+//== 
+//==             count_++;
+//== 
+//==             int N = std::min(count_, max_history_);
+//== 
+//==             if (N > 1)
+//==             {
+//==                 for (int j = 0; j <= 10; j++)
+//==                 {
+//==                     //==double k0 = (1 - beta_) * double(j) / 10;
+//==                     //==double k1 = (1 - beta_) * double(10 - j) / 10;
+//==                     for (int i = 0; i < (int)spl_size_.local_size(); i++)
+//==                     {
+//==                         vectors_(i, offset(count_)) = 0.5 * (1 - beta_) * vectors_(i, offset(count_ - 2)) + 
+//==                                                       0.5 * (1 - beta_) * vectors_(i, offset(count_ - 1)) + 
+//==                                                       beta_ * input_buffer_(i); 
+//==                     }
+//==                     //==double rms = rms_deviation();
+//==                     //==if (Platform::mpi_rank() == 0) std::cout << " j = " << j << ", rms = " << rms << std::endl;
+//== 
+//==                     Platform::allgather(&vectors_(0, offset(count_)), output_buffer_.ptr(), (int)spl_size_.global_offset(), 
+//==                                         (int)spl_size_.local_size());
+//==                 }
+//==                
+//==             }
+//==             else
+//==             {
+//==                 mix_linear(beta_);
+//==             }
+//== 
+//==             return rms_deviation();
+//==         }
+//== };
 
 }
 
