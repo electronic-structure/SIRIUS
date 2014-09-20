6daec703b9f4af2911c4f512444dd5a93123d8f0
toxa81 <toxa81@gmail.com>
2014-09-20 18:11:42 +0200

    test update

--- apps/tests/test_gpudirect.cpp
+++ apps/tests/test_gpudirect.cpp
@@ -4,20 +4,21 @@ using namespace sirius;
 
 void test()
 {
-    int N = 200;
-    mdarray<double_complex, 2> A(N, N);
-    mdarray<double_complex, 2> B(N, N);
+    int N = 500;
+    int K = 30000;
+    mdarray<double_complex, 2> A(N, K);
+    mdarray<double_complex, 2> B(K, N);
     mdarray<double_complex, 2> C(N, N);
 
-    for (int j = 0; j < N; j++)
+    for (int j = 0; j < K; j++)
     {
         for (int i = 0; i < N; i++)
         {
             A(i, j) = 1.0;
-            B(i, j) = 1.0;
+            B(j, i) = 1.0;
         }
     }
-    blas<cpu>::gemm(2, 0, N, N, N, A.ptr(), A.ld(), B.ptr(), B.ld(), C.ptr(), C.ld());
+    blas<cpu>::gemm(0, 0, N, N, K, A.at<cpu>(), A.ld(), B.at<cpu>(), B.ld(), C.at<cpu>(), C.ld());
     Platform::comm_world().allreduce(C.ptr(), (int)C.size());
 
     std::cout << C(0, 0) << " " << C(N - 1, N - 1) << std::endl;
@@ -28,8 +29,8 @@ void test()
     B.copy_to_device();
     C.allocate_on_device();
 
-    blas<gpu>::gemm(2, 0, N, N, N, A.ptr_device(), A.ld(), B.ptr_device(), B.ld(), C.ptr_device(), C.ld());
-    Platform::comm_world().allreduce(C.ptr_device(), (int)C.size());
+    blas<gpu>::gemm(0, 0, N, N, K, A.at<gpu>(), A.ld(), B.at<gpu>(), B.ld(), C.at<gpu>(), C.ld());
+    Platform::comm_world().allreduce(C.at<gpu>(), (int)C.size());
     C.copy_to_host();
 
     std::cout << C(0, 0) << " " << C(N - 1, N - 1) << std::endl;
