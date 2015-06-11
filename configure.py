cfa39c8f42135751c0c7cd8d6e79b52c8c6d6197
toxa81 <toxa81@gmail.com>
2015-06-11 13:50:48 +0200

    working on fft-mpi

--- configure.py
+++ configure.py
@@ -7,7 +7,7 @@ import json
 
 packages = {
     "fftw" : ["http://www.fftw.org/fftw-3.3.4.tar.gz", 
-              ["--enable-fortran", "--disable-mpi", "--enable-openmp", "--enable-threads"]
+              ["--enable-fortran", "--enable-mpi", "--enable-openmp", "--enable-threads"]
              ],
     "gsl"  : ["ftp://ftp.gnu.org/gnu/gsl/gsl-1.16.tar.gz", 
               ["--disable-shared"]
@@ -61,6 +61,12 @@ def configure_package(package_name, platform):
     new_env["FC"] = platform["FC"]
     new_env["F77"] = platform["FC"]
     new_env["FCCPP"] = platform["FCCPP"]
+    
+    if (package_name == "fftw"):
+        new_env["CC"] = platform["MPI_CXX"]
+        new_env["CXX"] = platform["MPI_CXX"]
+        new_env["FC"] = platform["MPI_FC"]
+        new_env["F77"] = platform["MPI_FC"]
 
     p = subprocess.Popen(["./configure"] + package[1], cwd = "./libs/" + package_dir, env = new_env)
     p.wait()
@@ -86,7 +92,7 @@ def configure_package(package_name, platform):
                   "\tcd ./libs/" + package_dir + "; make clean\n"]
 
     if (package_name == "fftw"):
-        retval = ["-I" + cwdlibs + package_dir + "/api", 
+        retval = ["-I" + cwdlibs + package_dir + "/api -I" + cwdlibs + package_dir + "/mpi", 
                   cwdlibs + package_dir + "/.libs/libfftw3.a " + cwdlibs + package_dir + "/threads/.libs/libfftw3_threads.a",
                   "\tcd ./libs/" + package_dir + "; make\n",
                   "\tcd ./libs/" + package_dir + "; make clean\n"]
--- src/platform.cpp
+++ src/platform.cpp
@@ -24,6 +24,7 @@
 
 #include "platform.h"
 #include <fftw3.h>
+#include <fftw3-mpi.h>
 
 int Platform::num_fft_threads_ = -1;
 
@@ -38,18 +39,19 @@ extern "C" void libsci_acc_finalize();
 
 void Platform::initialize(bool call_mpi_init__)
 {
-    if (call_mpi_init__) MPI_Init(NULL, NULL);
-    //== int provided;
-    //== if (call_mpi_init) 
-    //== {
-    //==     MPI_Init_thread(NULL, NULL, MPI_THREAD_MULTIPLE, &provided);
-    //== }
+    //if (call_mpi_init__) MPI_Init(NULL, NULL);
 
-    //== MPI_Query_thread(&provided);
-    //== if (provided != MPI_THREAD_MULTIPLE && rank() == 0)
-    //== {
-    //==     printf("Warning! MPI_THREAD_MULTIPLE level of thread support is not provided.\n");
-    //== }
+    int provided;
+    if (call_mpi_init__) 
+    {
+        MPI_Init_thread(NULL, NULL, MPI_THREAD_FUNNELED, &provided);
+    }
+
+    MPI_Query_thread(&provided);
+    if (provided != MPI_THREAD_FUNNELED && rank() == 0)
+    {
+        printf("Warning! MPI_THREAD_FUNNELED level of thread support is not provided.\n");
+    }
 
     #ifdef __GPU
     //cuda_initialize();
@@ -69,12 +71,17 @@ void Platform::initialize(bool call_mpi_init__)
     libsci_acc_init();
     #endif
     #ifdef __FFTW_THREADED
-    if (!fftw_init_threads())
+    if (provided >= MPI_THREAD_FUNNELED)
     {
-        printf("error in fftw_init_threads()\n");
-        exit(0);
+        if (!fftw_init_threads())
+        {
+            printf("error in fftw_init_threads()\n");
+            exit(0);
+        }
     }
     #endif
+
+    fftw_mpi_init();
 
     assert(sizeof(int) == 4);
     assert(sizeof(double) == 8);
