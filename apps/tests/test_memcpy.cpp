ff505de499162439676170ef812492a48d941b51
toxa81 <toxa81@gmail.com>
2016-01-18 10:17:10 +0100

    update test

--- apps/tests/test_memcpy.cpp
+++ apps/tests/test_memcpy.cpp
@@ -2,6 +2,11 @@
 
 using namespace sirius;
 
+inline void memcpy_simple_1(char* dest__, char* src__, size_t n__)
+{
+    for (size_t i = 0; i < n__; i++) dest__[i] = src__[i];
+}
+
 int main(int argn, char** argv)
 {
     cmd_args args;
@@ -16,14 +21,34 @@ int main(int argn, char** argv)
     }
 
     sirius::initialize(1);
-    int n = 10000000;
-    std::vector<double_complex> v1(n, 1.0);
-    std::vector<double_complex> v2(n, 2.0);
+    int n = 20000000;
+    std::vector<double> v1(n, 1.0);
+    std::vector<double> v2(n, 2.0);
 
-    double t = omp_get_wtime();
+    double t = -omp_get_wtime();
+    std::memcpy(&v1[0], &v2[0], n * sizeof(double));
+    t += omp_get_wtime();
+    printf("memcpy(stdlib) bandwidth: %f GB/s \n", double(2 * n * sizeof(double)) / t / (1 << 30));
+
+    t = -omp_get_wtime();
     sirius::memcpy_simple(&v1[0], &v2[0], n);
-    t = (omp_get_wtime() - t);
-    printf("bandwidth: %f GB/s \n", double(n * sizeof(double_complex)) / t / (1 << 30));
+    t += omp_get_wtime();
+    printf("memcpy(sirius) bandwidth: %f GB/s \n", double(2 * n * sizeof(double)) / t / (1 << 30));
+
+    t = -omp_get_wtime();
+    memcpy_simple_1((char*)&v1[0], (char*)&v2[0], n * sizeof(double));
+    t += omp_get_wtime();
+    printf("memcpy(simple) bandwidth: %f GB/s \n", double(2 * n * sizeof(double)) / t / (1 << 30));
+
+    t = -omp_get_wtime();
+    std::memset(&v1[0], 0, n * sizeof(double));
+    t += omp_get_wtime();
+    printf("memset(stdlib) bandwidth: %f GB/s \n", double(n * sizeof(double)) / t / (1 << 30));
+
+    t = -omp_get_wtime();
+    sirius::memset_simple(&v1[0], 0.0, n);
+    t += omp_get_wtime();
+    printf("memset(sirius) bandwidth: %f GB/s \n", double(n * sizeof(double)) / t / (1 << 30));
 
 
     sirius::finalize();
