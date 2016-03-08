598b2ee21714ee6739779e2921606b8cd16caed0
toxa81 <toxa81@gmail.com>
2016-03-08 11:36:24 +0100

    minor updates

--- src/beta_projectors.cpp
+++ src/beta_projectors.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2013-2015 Anton Kozhevnikov, Thomas Schulthess
+// Copyright (c) 2013-2016 Anton Kozhevnikov, Thomas Schulthess
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without modification, are permitted provided that 
