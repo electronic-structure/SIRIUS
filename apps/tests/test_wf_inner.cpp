be17b097926254b9fa33c5729d998411c102c5a1
toxa81 <toxa81@gmail.com>
2016-03-24 13:16:24 +0100

    test non-blocking reduce

--- apps/tests/test_wf_inner.cpp
+++ apps/tests/test_wf_inner.cpp
@@ -31,8 +31,12 @@ double test_gemm(int M, int N, int K, std::vector<int> mpi_grid)
 
     mdarray<double_complex, 2> a_tmp(spl_K.local_size(), c.num_rows_local(0));
     mdarray<double_complex, 2> b_tmp(spl_K.local_size(), c.num_cols_local(0));
-    mdarray<double_complex, 2> c_tmp(c.num_rows_local(0), c.num_cols_local(0));
+    mdarray<double_complex, 3> c_tmp(c.num_rows_local(0), c.num_cols_local(0), 2);
 
+    std::array<MPI_Request, 2> req = {MPI_REQUEST_NULL, MPI_REQUEST_NULL};
+    std::array<std::pair<int, int>, 2> pos;
+    
+    int s = 0;
     for (int rank_col = 0; rank_col < mpi_grid[1]; rank_col++)
     {
         for (int rank_row = 0; rank_row < mpi_grid[0]; rank_row++)
@@ -42,16 +46,53 @@ double test_gemm(int M, int N, int K, std::vector<int> mpi_grid)
 
             for (int i = 0; i < c.num_cols_local(rank_col); i++)
                 std::memcpy(&b_tmp(0, i), &b(0, c.spl_col().global_index(i, rank_col)), spl_K.local_size() * sizeof(double_complex));
-            
-            linalg<CPU>::gemm(2, 0, c.num_rows_local(rank_row), c.num_cols_local(rank_col), spl_K.local_size(),
-                              a_tmp.at<CPU>(), a_tmp.ld(), b_tmp.at<CPU>(), b_tmp.ld(), c_tmp.at<CPU>(), c_tmp.ld());
 
-            mpi_comm_world().reduce(c_tmp.at<CPU>(), c_tmp.ld() * c.num_cols_local(rank_col), blacs_grid.cart_rank(rank_row, rank_col));
-            
-            if (mpi_comm_world().rank() == blacs_grid.cart_rank(rank_row, rank_col))
+            if (req[s % 2] != MPI_REQUEST_NULL)
             {
+                printf("wait of %i\n", s % 2);
+                MPI_Wait(&req[s % 2], MPI_STATUS_IGNORE);
+
+                if (mpi_comm_world().rank() == blacs_grid.cart_rank(pos[s % 2].first, pos[s % 2].second))
+                {
+                    printf("copy of %i to (%i,%i)\n", s % 2, pos[s % 2].first, pos[s % 2].second);
+                    for (int i = 0; i < c.num_cols_local(); i++)
+                        std::memcpy(&c(0, i), &c_tmp(0, i, s % 2), c.num_rows_local() * sizeof(double_complex));
+                }
+            }
+
+            pos[s % 2].first = rank_row;
+            pos[s % 2].second = rank_col;
+
+            
+            printf("zgemm to %i\n", s % 2);
+            linalg<CPU>::gemm(2, 0, c.num_rows_local(rank_row), c.num_cols_local(rank_col), spl_K.local_size(),
+                              a_tmp.at<CPU>(), a_tmp.ld(), b_tmp.at<CPU>(), b_tmp.ld(), c_tmp.at<CPU>(0, 0, s % 2), c_tmp.ld());
+
+            printf("ireduce of %i\n", s % 2);
+            mpi_comm_world().ireduce(c_tmp.at<CPU>(0, 0, s % 2), c_tmp.ld() * c.num_cols_local(rank_col), blacs_grid.cart_rank(rank_row, rank_col), &req[s % 2]);
+            
+            //if (mpi_comm_world().rank() == blacs_grid.cart_rank(rank_row, rank_col))
+            //{
+            //    for (int i = 0; i < c.num_cols_local(); i++)
+            //        std::memcpy(&c(0, i), &c_tmp(0, i), c.num_rows_local() * sizeof(double_complex));
+            //}
+
+            s++;
+        }
+    }
+
+    for (int s: {0, 1})
+    {
+        if (req[s] != MPI_REQUEST_NULL)
+        {
+            printf("wait of %i\n", s);
+            MPI_Wait(&req[s], MPI_STATUS_IGNORE);
+
+            if (mpi_comm_world().rank() == blacs_grid.cart_rank(pos[s].first, pos[s].second))
+            {
+                printf("copy of %i to (%i,%i)\n", s, pos[s].first, pos[s].second);
                 for (int i = 0; i < c.num_cols_local(); i++)
-                    std::memcpy(&c(0, i), &c_tmp(0, i), c.num_rows_local() * sizeof(double_complex));
+                    std::memcpy(&c(0, i), &c_tmp(0, i, s), c.num_rows_local() * sizeof(double_complex));
             }
         }
     }
@@ -62,7 +103,9 @@ double test_gemm(int M, int N, int K, std::vector<int> mpi_grid)
     if (mpi_comm_world().rank() == 0)
     {
         printf("execution time (sec) : %12.6f\n", tval);
-        printf("performance (GFlops / rank) : %12.6f\n", perf);
+        printf("global matrix sizes: %i %i %i\n", M, N, K);
+        printf("number of ranks: %i\n", mpi_comm_world().size());
+        printf("performance (GFlops / rank): %12.6f\n", perf);
     }
     for (int i = 0; i < c.num_cols_local(); i++)
     {
