a105a972392874a23650ffb6f3ed9a62956401ba
toxa81 <toxa81@gmail.com>
2016-05-11 18:11:36 +0200

    update inverse fine structure

--- src/constants.h
+++ src/constants.h
@@ -33,14 +33,21 @@ const int major_version = 3;
 const int minor_version = 6;
 
 /// NIST value for the inverse fine structure (http://physics.nist.gov/cuu/Constants/index.html)
-const double speed_of_light = 137.035999074; 
+const double speed_of_light = 137.035999139; 
 
+// This value reproduces NIST ScRLDA total energy much better.
+const double speed_of_light = 137.0359895;
+
+/// \f$ \pi \f$
 const double pi = 3.1415926535897932385;
 
+/// \f$ 2\pi \f$
 const double twopi = 6.2831853071795864769;
 
+/// \f$ 4\pi \f$
 const double fourpi = 12.566370614359172954;
 
+/// First spherical harmonic \f$ Y_{00} = \frac{1}{\sqrt{4\pi}} \f$.
 const double y00 = 0.28209479177387814347;
 
 const double ha2ev = 27.21138505;
