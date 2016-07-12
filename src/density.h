c31cc2fc0b5ff68d4de6fee5aacbf17ea8a77ed1
toxa81 <toxa81@gmail.com>
2016-07-12 12:17:13 +0200

    move small function back to cpp

--- src/density.h
+++ src/density.h
@@ -312,7 +312,20 @@ class Density
         void generate_valence_density_it(K_set& ks);
        
         /// Generate charge density of core states
-        void generate_core_charge_density();
+        void generate_core_charge_density()
+        {
+            PROFILE_WITH_TIMER("sirius::Density::generate_core_charge_density");
+
+            for (int icloc = 0; icloc < unit_cell_.spl_num_atom_symmetry_classes().local_size(); icloc++) {
+                int ic = unit_cell_.spl_num_atom_symmetry_classes(icloc);
+                unit_cell_.atom_symmetry_class(ic).generate_core_charge_density(ctx_.core_relativity());
+            }
+
+            for (int ic = 0; ic < unit_cell_.num_atom_symmetry_classes(); ic++) {
+                int rank = unit_cell_.spl_num_atom_symmetry_classes().local_rank(ic);
+                unit_cell_.atom_symmetry_class(ic).sync_core_charge_density(ctx_.comm(), rank);
+            }
+        }
 
         void generate_pseudo_core_charge_density();
 
