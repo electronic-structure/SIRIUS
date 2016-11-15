d987aa5626c64faa4e788292839345a6254d2d61
toxa81 <toxa81@gmail.com>
2016-11-15 22:54:12 +0100

    cleanup

--- src/periodic_function.h
+++ src/periodic_function.h
@@ -162,9 +162,6 @@ class Periodic_function: public Smooth_periodic_function<T>
             }
         }
         
-        /// Copy from source
-        //void copy(Periodic_function<T>* src);
-
         inline void copy_to_global_ptr(T* f_mt__, T* f_it__) const
         {
             std::memcpy(f_it__, this->f_rg_.template at<CPU>(), this->fft_->local_size() * sizeof(T));
@@ -451,27 +448,24 @@ class Periodic_function: public Smooth_periodic_function<T>
             assert(&unit_cell_ == &g__->unit_cell_);
             assert(&comm_ == &g__->comm_);
             
-            T result = 0.0;
-            T ri = 0.0;
+            T result{0};
+            T ri{0};
             
-            if (!parameters_.full_potential())
-            {
+            if (!parameters_.full_potential()) {
                 #pragma omp parallel
                 {
-                    T ri_t = 0;
+                    T ri_t{0};
                     
                     #pragma omp for
-                    for (int irloc = 0; irloc < this->fft_->local_size(); irloc++)
+                    for (int irloc = 0; irloc < this->fft_->local_size(); irloc++) {
                         ri_t += type_wrapper<T>::conjugate(this->f_rg(irloc)) * g__->f_rg(irloc);
+                    }
         
                     #pragma omp critical
                     ri += ri_t;
                 }
-            }
-            else
-            {
-                for (int irloc = 0; irloc < this->fft_->local_size(); irloc++)
-                {
+            } else {
+                for (int irloc = 0; irloc < this->fft_->local_size(); irloc++) {
                     ri += type_wrapper<T>::conjugate(this->f_rg(irloc)) * g__->f_rg(irloc) * 
                           this->step_function_.theta_r(irloc);
                 }
@@ -479,11 +473,11 @@ class Periodic_function: public Smooth_periodic_function<T>
                     
             ri *= (unit_cell_.omega() / this->fft_->size());
             this->fft_->comm().allreduce(&ri, 1);
-            
-            if (parameters_.full_potential())
-            {
-                for (int ialoc = 0; ialoc < unit_cell_.spl_num_atoms().local_size(); ialoc++)
+
+            if (parameters_.full_potential()) {
+                for (int ialoc = 0; ialoc < unit_cell_.spl_num_atoms().local_size(); ialoc++) {
                     result += sirius::inner(f_mt(ialoc), g__->f_mt(ialoc));
+                }
                 comm_.allreduce(&result, 1);
             }
         
