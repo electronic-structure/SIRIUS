e02eca91d716f7993c5849c7ba74dcb8ec6cc7d4
toxa81 <toxa81@gmail.com>
2016-12-07 19:01:00 +0100

    print checksums

--- src/Band/solve.hpp
+++ src/Band/solve.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2013-2014 Anton Kozhevnikov, Thomas Schulthess
+// Copyright (c) 2013-2016 Anton Kozhevnikov, Thomas Schulthess
 // All rights reserved.
 // 
 // Redistribution and use in source and binary forms, with or without modification, are permitted provided that 
@@ -160,60 +160,49 @@ inline void Band::solve_sv(K_point* kp,
 
     //== if (ctx_.so_correction()) apply_so_correction(kp->fv_states_col(), hpsi);
 
+    int nfv = ctx_.num_fv_states();
+    int bs  = ctx_.cyclic_block_size();
+
     #ifdef __GPU
-    if (ctx_.processing_unit() == GPU && kp->num_ranks() == 1) {
-        STOP();
-        //kp->fv_states<true>().coeffs().allocate(memory_t::device);
-        //kp->fv_states<true>().coeffs().copy_to_device();
+    if (ctx_.processing_unit() == GPU) {
+        kp->fv_states().allocate_on_device();
+        kp->fv_states().copy_to_device(0, nfv);
+        for (int i = 0; i < ctx_.num_mag_comp(); i++) {
+            hpsi[i].allocate_on_device();
+            hpsi[i].copy_to_device(0, nfv);
+        }
+    }
+    #endif
+
+    #ifdef __PRINT_OBJECT_CHECKSUM
+    auto z1 = kp->fv_states().checksum(0, nfv);
+    DUMP("checksum(fv_states): %18.10f %18.10f", std::real(z1), std::imag(z1));
+    for (int i = 0; i < ctx_.num_mag_comp(); i++) {
+        z1 = hpsi[i].checksum(0, nfv);
+        DUMP("checksum(hpsi[i]): %18.10f %18.10f", std::real(z1), std::imag(z1));
     }
     #endif
  
-    int nfv = ctx_.num_fv_states();
-    int bs = ctx_.cyclic_block_size();
-
     if (ctx_.num_mag_dims() != 3) {
         dmatrix<double_complex> h(nfv, nfv, ctx_.blacs_grid(), bs, bs);
-
-        if (ctx_.processing_unit() == GPU && kp->num_ranks() == 1) {
-            #ifdef __GPU
-            STOP();
-            //h.allocate(memory_t::device);
-            #endif
+        #ifdef __GPU
+        if (kp->num_ranks() == 1 && ctx_.processing_unit() == GPU) {
+            h.allocate(memory_t::device);
         }
-
+        #endif
         /* perform one or two consecutive diagonalizations */
         for (int ispn = 0; ispn < ctx_.num_spins(); ispn++) {
+
+            /* compute <wf_i | h * wf_j> */
             inner(kp->fv_states(), 0, nfv, hpsi[ispn], 0, nfv, h, 0, 0);
-            //if (ctx_.processing_unit() == GPU && kp->num_ranks() == 1) {
-            //    #ifdef __GPU
-            //    runtime::Timer t4("sirius::Band::solve_sv|zgemm");
-            //    hpsi[ispn]->coeffs().allocate(memory_t::device);
-            //    hpsi[ispn]->coeffs().copy_to_device();
-            //    linalg<GPU>::gemm(2, 0, nfv, nfv, fvsz,
-            //                      &alpha, 
-            //                      kp->fv_states<true>().coeffs().at<GPU>(), kp->fv_states<true>().coeffs().ld(),
-            //                      hpsi[ispn]->coeffs().at<GPU>(), hpsi[ispn]->coeffs().ld(),
-            //                      &beta,
-            //                      h.at<GPU>(), h.ld());
-            //    h.copy_to_host();
-            //    hpsi[ispn]->coeffs().deallocate_on_device();
-            //    double tval = t4.stop();
-            //    DUMP("effective zgemm performance: %12.6f GFlops", 
-            //         8e-9 * ctx_.num_fv_states() * ctx_.num_fv_states() * fvsz / tval);
-            //    #else
-            //    TERMINATE_NO_GPU
-            //    #endif
-            //} else {
-            //    STOP();
-            //    ///* compute <wf_i | h * wf_j> for up-up or dn-dn block */
-            //    //linalg<CPU>::gemm(2, 0, nfv, nfv, fvsz, complex_one, kp->fv_states<true>().prime(), hpsi[ispn]->prime(),
-            //    //                  complex_zero, h);
-            //}
             
             for (int i = 0; i < nfv; i++) {
                 h.add(i, i, kp->fv_eigen_value(i));
             }
-        
+            #ifdef __PRINT_OBJECT_CHECKSUM
+            auto z1 = h.checksum();
+            DUMP("checksum(h): %18.10f %18.10f", std::real(z1), std::imag(z1));
+            #endif
             sddk::timer t1("sirius::Band::solve_sv|stdevp");
             std_evp_solver().solve(nfv, h.at<CPU>(), h.ld(), &band_energies[ispn * nfv],
                                    kp->sv_eigen_vectors(ispn).at<CPU>(), kp->sv_eigen_vectors(ispn).ld());
@@ -221,7 +210,11 @@ inline void Band::solve_sv(K_point* kp,
     } else {
         int nb = ctx_.num_bands();
         dmatrix<double_complex> h(nb, nb, ctx_.blacs_grid(), bs, bs);
-
+        #ifdef __GPU
+        if (kp->num_ranks() == 1 && ctx_.processing_unit() == GPU) {
+            h.allocate(memory_t::device);
+        }
+        #endif
         /* compute <wf_i | h * wf_j> for up-up block */
         inner(kp->fv_states(), 0, nfv, hpsi[0], 0, nfv, h, 0, 0);
         /* compute <wf_i | h * wf_j> for dn-dn block */
@@ -243,18 +236,24 @@ inline void Band::solve_sv(K_point* kp,
             h.add(i,       i,       kp->fv_eigen_value(i));
             h.add(i + nfv, i + nfv, kp->fv_eigen_value(i));
         }
+        #ifdef __PRINT_OBJECT_CHECKSUM
+        auto z1 = h.checksum();
+        DUMP("checksum(h): %18.10f %18.10f", std::real(z1), std::imag(z1));
+        #endif
         sddk::timer t1("sirius::Band::solve_sv|stdevp");
         std_evp_solver().solve(nb, h.at<CPU>(), h.ld(), &band_energies[0],
                                kp->sv_eigen_vectors(0).at<CPU>(), kp->sv_eigen_vectors(0).ld());
     }
- 
-    if (ctx_.processing_unit() == GPU && kp->num_ranks() == 1) {
-        #ifdef __GPU
-        STOP();
-        //kp->fv_states<true>().coeffs().deallocate_on_device();
-        #endif
-    }
 
+    #ifdef __GPU
+    if (ctx_.processing_unit() == GPU) {
+        kp->fv_states().deallocate_on_device();
+        for (int i = 0; i < ctx_.num_mag_comp(); i++) {
+            hpsi[i].deallocate_on_device();
+        }
+    }
+    #endif
+ 
     kp->set_band_energies(&band_energies[0]);
 }
 
