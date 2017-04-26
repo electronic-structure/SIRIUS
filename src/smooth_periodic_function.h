4b16dad4f662d2e10773e6ce4d504d471eca99e6
toxa81 <toxa81@gmail.com>
2017-04-26 14:19:27 +0200

    update smooth periodic function

--- src/smooth_periodic_function.h
+++ src/smooth_periodic_function.h
@@ -28,203 +28,6 @@
 
 namespace sirius {
 
-///// Smooth periodic function on the regular real-space grid or in plane-wave domain.
-///** Main purpose of this class is to provide a storage and representation of a smooth (Fourier-transformable)
-// *  periodic function.
-// */
-//template <typename T>
-//class Smooth_periodic_function
-//{
-//    protected:
-//
-//        /// FFT driver.
-//        FFT3D* fft_{nullptr};
-//
-//        /// Distribution of G-vectors.
-//        Gvec const* gvec_{nullptr};
-//        
-//        /// Function on the regular real-space grid.
-//        mdarray<T, 1> f_rg_;
-//        
-//        /// Local set of plane-wave expansion coefficients.
-//        mdarray<double_complex, 1> f_pw_local_;
-//
-//    public:
-//
-//        Smooth_periodic_function() 
-//        {
-//        }
-//
-//        Smooth_periodic_function(FFT3D& fft__)
-//            : fft_(&fft__)
-//        {
-//            f_rg_ = mdarray<T, 1>(fft_->local_size());
-//        }
-//
-//        Smooth_periodic_function(FFT3D& fft__, Gvec const& gvec__)
-//            : fft_(&fft__),
-//              gvec_(&gvec__)
-//        {
-//            f_rg_ = mdarray<T, 1>(fft_->local_size());
-//            f_pw_local_ = mdarray<double_complex, 1>(gvec_->partition().gvec_count_fft());
-//        }
-//
-//        inline T& f_rg(int ir__)
-//        {
-//            return f_rg_(ir__);
-//        }
-//
-//        inline T const& f_rg(int ir__) const
-//        {
-//            return f_rg_(ir__);
-//        }
-//        
-//        inline double_complex& f_pw_local(int ig__)
-//        {
-//            return f_pw_local_(ig__);
-//        }
-//
-//        inline const double_complex& f_pw_local(int ig__) const
-//        {
-//            return f_pw_local_(ig__);
-//        }
-//
-//        FFT3D& fft()
-//        {
-//            assert(fft_ != nullptr);
-//            return *fft_;
-//        }
-//
-//        FFT3D const& fft() const
-//        {
-//            assert(fft_ != nullptr);
-//            return *fft_;
-//        }
-//
-//        Gvec const& gvec() const
-//        {
-//            assert(gvec_ != nullptr);
-//            return *gvec_;
-//        }
-//
-//        void fft_transform(int direction__)
-//        {
-//            PROFILE("sirius::Smooth_periodic_function::fft_transform");
-//
-//            assert(gvec_ != nullptr);
-//
-//            switch (direction__) {
-//                case 1: {
-//                    fft_->transform<1>(gvec_->partition(), &f_pw_local_(0));
-//                    fft_->output(&f_rg_(0));
-//                    break;
-//                }
-//                case -1: {
-//                    fft_->input(&f_rg_(0));
-//                    fft_->transform<-1>(gvec_->partition(), &f_pw_local_(0));
-//                    break;
-//                }
-//                default: {
-//                    TERMINATE("wrong fft direction");
-//                }
-//            }
-//        }
-//};
-//
-///// Gradient of the smooth periodic function.
-//template<typename T>
-//class Smooth_periodic_function_gradient
-//{
-//    private:
-//        
-//        Smooth_periodic_function<T>* f_;
-//
-//        std::array<Smooth_periodic_function<T>, 3> grad_f_;
-//
-//    public:
-//
-//        Smooth_periodic_function_gradient() : f_(nullptr)
-//        {
-//        }
-//
-//        Smooth_periodic_function_gradient(Smooth_periodic_function<T>& f__) : f_(&f__)
-//        {
-//            for (int x: {0, 1, 2}) grad_f_[x] = Smooth_periodic_function<T>(f_->fft(), f_->gvec());
-//        }
-//
-//        Smooth_periodic_function<T>& operator[](const int idx__)
-//        {
-//            return grad_f_[idx__];
-//        }
-//
-//        Smooth_periodic_function<T>& f()
-//        {
-//            assert(f_ != nullptr);
-//
-//            return *f_;
-//        }
-//};
-//
-///// Gradient of the function in the plane-wave domain.
-//inline Smooth_periodic_function_gradient<double> gradient(Smooth_periodic_function<double>& f__)
-//{
-//    Smooth_periodic_function_gradient<double> g(f__);
-//
-//    #pragma omp parallel for
-//    for (int igloc = 0; igloc < f__.gvec().partition().gvec_count_fft(); igloc++)
-//    {
-//        int ig = f__.gvec().partition().gvec_offset_fft() + igloc;
-//
-//        auto G = f__.gvec().gvec_cart(ig);
-//        for (int x: {0, 1, 2}) g[x].f_pw_local(igloc) = f__.f_pw_local(igloc) * double_complex(0, G[x]);
-//    }
-//    return std::move(g);
-//}
-//
-///// Laplacian of the function in the plane-wave domain.
-//inline Smooth_periodic_function<double> laplacian(Smooth_periodic_function<double>& f__)
-//{
-//    Smooth_periodic_function<double> g(f__.fft(), f__.gvec());
-//    
-//    #pragma omp parallel for
-//    for (int igloc = 0; igloc < f__.gvec().partition().gvec_count_fft(); igloc++)
-//    {
-//        int ig = f__.gvec().partition().gvec_offset_fft() + igloc;
-//
-//        auto G = f__.gvec().gvec_cart(ig);
-//        g.f_pw_local(igloc) = f__.f_pw_local(igloc) * double_complex(-std::pow(G.length(), 2), 0);
-//    }
-//
-//    return std::move(g);
-//}
-//
-//template <typename T>
-//Smooth_periodic_function<T> operator*(Smooth_periodic_function_gradient<T>& grad_f__, 
-//                                      Smooth_periodic_function_gradient<T>& grad_g__)
-//
-//{
-//    assert(&grad_f__.f().fft() == &grad_g__.f().fft());
-//    assert(&grad_f__.f().gvec() == &grad_g__.f().gvec());
-//    
-//    Smooth_periodic_function<T> result(grad_f__.f().fft());
-//
-//    #pragma omp parallel for
-//    for (int ir = 0; ir < grad_f__.f().fft().local_size(); ir++)
-//    {
-//        double d = 0;
-//        for (int x: {0, 1, 2})
-//        {
-//            d += grad_f__[x].f_rg(ir) * grad_g__[x].f_rg(ir);
-//        }
-//        result.f_rg(ir) = d;
-//    }
-//
-//    return std::move(result);
-//}
-//
-///// Experimental features.
-//namespace experimental {
-
 /// Representation of a smooth (Fourier-transformable) periodic function.
 /** The class is designed to handle periodic functions such as density or potential, defined on a regular FFT grid.
  *  The following functionality is expected:
@@ -283,7 +86,7 @@ class Smooth_periodic_function
         {
             f_rg_       = mdarray<T, 1>(fft_->local_size());
             f_pw_fft_   = mdarray<double_complex, 1>(gvec_->partition().gvec_count_fft());
-            f_pw_local_ = mdarray<double_complex, 1>(gvec_->gvec_count(gvec_->comm().rank()));
+            f_pw_local_ = mdarray<double_complex, 1>(gvec_->count());
 
             /* check ordering of mpi ranks */
             int rank = fft_->comm().rank() * gvec_->comm_ortho_fft().size() + gvec_->comm_ortho_fft().rank();
@@ -318,9 +121,24 @@ class Smooth_periodic_function
             return f_pw_local_(ig__);
         }
 
+        inline double_complex const& f_pw_local(int ig__) const
+        {
+            return f_pw_local_(ig__);
+        }
+
         inline double_complex& f_pw_fft(int ig__)
         {
             return f_pw_fft_(ig__);
+        }
+
+        inline double_complex f_0() const
+        {
+            double_complex z;
+            if (gvec_->comm().rank() == 0) {
+                z = f_pw_local_(0);
+            }
+            gvec_->comm().bcast(&z, 1, 0);
+            return z;
         }
 
         FFT3D& fft()
@@ -378,6 +196,11 @@ class Smooth_periodic_function
             fft_->comm().allgather(f_pw_fft_.at<CPU>(), fpw.data(), gvec_->partition().gvec_offset_fft(), gvec_->partition().gvec_count_fft());
 
             return std::move(fpw);
+        }
+
+        inline void scatter_f_pw(std::vector<double_complex> const& f_pw__)
+        {
+            std::copy(&f_pw__[gvec_->offset()], &f_pw__[gvec_->offset()] + gvec_->count(), &f_pw_local_(0));
         }
 };
 
@@ -480,8 +303,6 @@ Smooth_periodic_function<T> operator*(Smooth_periodic_function_gradient<T>& grad
 
     return std::move(result);
 }
-
-//} // namespace experimental
 
 } // namespace sirius
 
