af50f21ce28a9e4f61fd9b2650d030ae54548830
toxa81 <toxa81@gmail.com>
2017-07-19 18:32:31 +0200

    small fix in unit test

--- apps/unit_tests/test_wf_ortho.cpp
+++ apps/unit_tests/test_wf_ortho.cpp
@@ -45,7 +45,7 @@ void test_wf_ortho(std::vector<int> mpi_grid_dims__,
     orthogonalize<double_complex>(0, num_bands__, phi, hphi, ovlp, tmp);
     orthogonalize<double_complex>(num_bands__, num_bands__, phi, hphi, ovlp, tmp);
 
-    inner(phi, 0, 2 * num_bands__, phi, 0, 2 * num_bands__, ovlp, 0, 0);
+    inner(phi, 0, 2 * num_bands__, phi, 0, 2 * num_bands__, 0.0, ovlp, 0, 0);
 
     for (int j = 0; j < ovlp.num_cols_local(); j++) {
         for (int i = 0; i < ovlp.num_rows_local(); i++) {
