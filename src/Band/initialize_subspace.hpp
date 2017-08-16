0727009b81a999c31e228cd30a244d79ef13c654
toxa81 <toxa81@gmail.com>
2017-08-16 10:53:51 +0200

    Randomize linear combination of atomic orbitals
    without this change the Si- example fails to converge

--- src/Band/initialize_subspace.hpp
+++ src/Band/initialize_subspace.hpp
@@ -240,12 +240,12 @@ inline void Band::initialize_subspace(K_point*                                  
     int igk0 = (kp__->comm().rank() == 0) ? 1 : 0;
 
     #pragma omp parallel for schedule(static)
-    for (int i = 0; i < num_phi - num_ao__; i++) {
-    //for (int i = 0; i < num_phi; i++) {
+    //for (int i = 0; i < num_phi - num_ao__; i++) {
+    for (int i = 0; i < num_phi; i++) {
         for (int igk_loc = igk0; igk_loc < kp__->num_gkvec_loc(); igk_loc++) {
             /* global index of G+k vector */
             int igk = kp__->idxgk(igk_loc);
-            phi.component(0).pw_coeffs().prime(igk_loc, num_ao__ + i) += tmp[igk & 0xFFF] * 1e-5;
+            phi.component(0).pw_coeffs().prime(igk_loc, i) += tmp[igk & 0xFFF] * 1e-5;
         }
     }
 
