fd20550fcdddc23b720f3c2dcdefffb853db7381
toxa81 <toxa81@gmail.com>
2017-09-25 09:00:33 +0200

    fix hash printing

--- src/runtime.h
+++ src/runtime.h
@@ -12,7 +12,6 @@
 #include <chrono>
 #include <omp.h>
 #include <unistd.h>
-#include <inttypes.h>
 #include <cstring>
 #include <cstdarg>
 #include "config.h"
@@ -209,9 +208,9 @@ inline void print_checksum(std::string label__, std::complex<double> cs__)
     printf("checksum(%s): %18.12f %18.12f\n", label__.c_str(), cs__.real(), cs__.imag());
 }
 
-inline void print_hash(std::string label__, uint64_t hash__)
+inline void print_hash(std::string label__, unsigned long long int hash__)
 {
-    printf("hash(%s): %" PRIx64 "\n", label__.c_str(), hash__);
+    printf("hash(%s): %llx\n", label__.c_str(), hash__);
 }
 
 inline void print_memory_usage(const char* file__, int line__)
