b97c3cd686a28cfe9d1180792aa9410df1a0ca14
Mathieu Taillefumier <mathieu.taillefumier@cscs.ch>
2018-06-14 16:10:11 +0200

    Fix another typo

--- src/Geometry/force.hpp
+++ src/Geometry/force.hpp
@@ -1,4 +1,4 @@
-vv// Copyright (c) 2013-2018 Anton Kozhevnikov, Ilia Sivkov, Thomas Schulthess
+// Copyright (c) 2013-2018 Anton Kozhevnikov, Ilia Sivkov, Thomas Schulthess
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without modification, are permitted provided that
