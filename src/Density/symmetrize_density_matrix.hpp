0399c53ce6ffe6724310c67a09561346acd859f9
toxa81 <toxa81@gmail.com>
2018-06-28 08:31:59 +0200

    apply clang-format

--- src/Density/symmetrize_density_matrix.hpp
+++ src/Density/symmetrize_density_matrix.hpp
@@ -10,7 +10,7 @@ inline void Density::symmetrize_density_matrix()
                                   ndm, unit_cell_.num_atoms());
     dm.zero();
 
-    int lmax = unit_cell_.lmax();
+    int lmax  = unit_cell_.lmax();
     int lmmax = Utils::lmmax(lmax);
 
     mdarray<double, 2> rotm(lmmax, lmmax);
@@ -18,15 +18,15 @@ inline void Density::symmetrize_density_matrix()
     double alpha = 1.0 / double(sym.num_mag_sym());
 
     for (int i = 0; i < sym.num_mag_sym(); i++) {
-        int pr = sym.magnetic_group_symmetry(i).spg_op.proper;
+        int  pr   = sym.magnetic_group_symmetry(i).spg_op.proper;
         auto eang = sym.magnetic_group_symmetry(i).spg_op.euler_angles;
-        int isym = sym.magnetic_group_symmetry(i).isym;
+        int  isym = sym.magnetic_group_symmetry(i).isym;
         SHT::rotation_matrix(lmax, eang, pr, rotm);
         auto spin_rot_su2 = SHT::rotation_matrix_su2(sym.magnetic_group_symmetry(i).spin_rotation);
 
         for (int ia = 0; ia < unit_cell_.num_atoms(); ia++) {
             auto& atom_type = unit_cell_.atom(ia).type();
-            int ja = sym.sym_table(ia, isym);
+            int   ja        = sym.sym_table(ia, isym);
 
             for (int xi1 = 0; xi1 < unit_cell_.atom(ia).mt_basis_size(); xi1++) {
                 int l1  = atom_type.indexb(xi1).l;
@@ -55,17 +55,16 @@ inline void Density::symmetrize_density_matrix()
                     /* magnetic symmetrization */
                     if (ndm == 1) {
                         dm(xi1, xi2, 0, ia) += dm_rot_spatial[0];
-                    } else  {
+                    } else {
                         double_complex spin_dm[2][2] = {
-                            {dm_rot_spatial[0],            dm_rot_spatial[2]},
-                            {std::conj(dm_rot_spatial[2]), dm_rot_spatial[1]}
-                        };
-                        
+                            {dm_rot_spatial[0], dm_rot_spatial[2]},
+                            {std::conj(dm_rot_spatial[2]), dm_rot_spatial[1]}};
+
                         /* spin blocks of density matrix are: uu, dd, ud
                            the mapping from linear index (0, 1, 2) of density matrix components is:
                              for the first spin index: k & 1, i.e. (0, 1, 2) -> (0, 1, 0)
                              for the second spin index: min(k, 1), i.e. (0, 1, 2) -> (0, 1, 1)
-                        */     
+                        */
                         for (int k = 0; k < ndm; k++) {
                             for (int is = 0; is < 2; is++) {
                                 for (int js = 0; js < 2; js++) {
@@ -95,4 +94,3 @@ inline void Density::symmetrize_density_matrix()
         print_hash("density_matrix", h);
     }
 }
-
