355ed71efeb1b60f74282d08564df086725525db
Simon Pintarelli <simon.pintarelli@gmail.com>
2018-08-29 10:56:09 +0200

    python module: various changes
    - OT allow x close to 0
    - add some more assert statements
    - load pw-coefficients from hdf5 (restart)

--- python_module/sirius/ot/ot_transformations.py
+++ python_module/sirius/ot/ot_transformations.py
@@ -56,16 +56,17 @@ def c(x, c0):
         w = np.sqrt(w)
         R = matview(R)
         err = np.linalg.norm(R.H * R - np.eye(*R.shape), 'fro')
+        # TODO: remove: check that we are not loosing accuracy for small x
+        assert(np.isclose(R * np.diag(w**2) * R.H, XX).all())
         assert(err < 1e-11)
 
-        Wsin = np.diag(np.sin(w))
-        sinU = np.dot(np.dot(R, Wsin), R.H)
+        Wsinc = np.diag(np.sinc(w/np.pi))
+        sincU = np.dot(np.dot(R, Wsinc), R.H)
 
         Wcos = np.diag(np.cos(w))
         cosU = np.dot(np.dot(R, Wcos), R.H)
-        invU = np.dot(np.dot(R, np.diag(1. / w)), R.H)
 
-        return np.dot(c0, cosU) + np.dot(np.dot(x, invU), sinU)
+        return np.dot(c0, cosU) + np.dot(x, sincU)
 
 
 class ConstrainedGradient:
@@ -101,21 +102,17 @@ class ConstrainedGradient:
         assert (err < 1e-10)
 
         # pre-compute matrix functions sin, cos, and inverse of U
-        Wsin = np.diag(np.sin(w))
-        sinU = np.dot(np.dot(R, Wsin), R.H)
-        assert(isinstance(sinU, np.matrix))
+        Wsinc = np.diag(np.sinc(w/np.pi))
+        sincU = np.dot(np.dot(R, Wsinc), R.H)
         # cos
         Wcos = np.diag(np.cos(w))
         cosU = np.dot(np.dot(R, Wcos), R.H)
         assert(isinstance(cosU, np.matrix))
-        # inv
-        invU = np.dot(np.dot(R, np.diag(1. / w)), R.H)
-        assert(isinstance(invU, np.matrix))
 
         # compute c(c0, x)
-        c = np.dot(c0, cosU) + np.dot(np.dot(x, invU), sinU)
+        c = np.dot(c0, cosU) + np.dot(x, sincU)
 
-        return c, Λ, invU, sinU, R
+        return c, Λ, sincU, R
 
     def __call__(self, x, ki=None, ispn=None):
         """
@@ -142,7 +139,7 @@ class ConstrainedGradient:
                 # plane-wave coefficients c(x, c0)
                 c = PwCoeffs(dtype=x.dtype)
                 for key, val in xk.items():
-                    fX[key] = {k: t for k, t in zip(['c', 'Λ', 'invU', 'sinU', 'R'],
+                    fX[key] = {k: t for k, t in zip(['c', 'Λ', 'sincU', 'R'],
                                                     self._prepare_fX(x=val, c0=self.c0[key]))}
                     # this makes a copy ...
                     c[key] = fX[key]['c']
@@ -155,8 +152,7 @@ class ConstrainedGradient:
                                                 c0=self.c0[key],
                                                 Λ=fX[key]['Λ'],
                                                 Hc=Hc_loc,
-                                                invU=fX[key]['invU'],
-                                                sinU=fX[key]['sinU'],
+                                                sincU=fX[key]['sincU'],
                                                 R=fX[key]['R'])
                     Gx_out[key] = Gx
                     Hx_out[key] = Hx
@@ -167,7 +163,7 @@ class ConstrainedGradient:
             assert(ispn is not None)
             assert(ki is not None)
             # compute matrix functions
-            c, Λ, invU, sinU, R = self._prepare_fX(x, self.c0)
+            c, Λ, sincU, R = self._prepare_fX(x, self.c0)
             # compute ∂E/∂c
             Hc = self.hamiltonian(c, ki=ki, ispn=ispn)
             # return ∂E/∂x as np.matrix
@@ -175,18 +171,18 @@ class ConstrainedGradient:
                                       c0=np.matrix(self.c0, copy=False),
                                       Λ=Λ,
                                       Hc=Hc,
-                                      invU=invU,
-                                      sinU=sinU,
+                                      sincU=sincU,
                                       R=R)
 
     @staticmethod
-    def _apply_single(x, c0, Λ, Hc, invU, sinU, R):
+    def _apply_single(x, c0, Λ, Hc, sincU, R):
         """
         Computes ∂E/∂x
         x    -- OT coefficients, where PW-coefficients are given by c = c(x, c0)
         c0   -- initial coefficients
         Λ    -- eigenvalues of sqrt(x.H * x)
         Hc   -- gradient
+        sincU -- sinc(U)
         R    -- eigenvectors of U (in columns)
         """
         # D¹: TODO repeat formula from
@@ -194,34 +190,50 @@ class ConstrainedGradient:
         #   for electronic structure calculations. , 118(10), 4365–4369.
         #   http://dx.doi.org/10.1063/1.1543154
         assert(isinstance(R, np.matrix))
-        assert(isinstance(invU, np.matrix))
-        assert(isinstance(sinU, np.matrix))
+        assert(isinstance(sincU, np.matrix))
         assert(isinstance(Hc, np.matrix))
         assert(isinstance(c0, np.matrix))
 
-        v = np.sin(np.sqrt(Λ)) / np.sqrt(Λ)
+        v = np.sinc(np.sqrt(Λ)/np.pi)
         v = v[:, np.newaxis]
-        # TODO: mask diagonal elements (get rid of warnings)
         diffL = Λ[:, np.newaxis] - Λ[:, np.newaxis].T
         mask = np.abs(diffL) < 1e-10
         D1 = np.ma.masked_array(v - v.T, mask=mask) / diffL
         # fill masked entries with correct formula
         irow, icol = np.where(mask)
+
+        def f1(la):
+            """
+            D1 matrix entries, where lambda1=lambda2 or lambda1=lambda2=0
+            """
+            pos_index = la > 1e-10
+            out = np.zeros(la.shape, dtype=la.dtype)
+            out[pos_index] = 0.5*(np.cos(np.sqrt(la[pos_index])) / la[pos_index]
+                                  - np.sin(np.sqrt(la[pos_index])) / (la[pos_index]**(1.5)))
+            out[~pos_index] = -1/6
+            # if (~pos_index).any():
+            #     print('OUTPUT: encountered small eigenvalue!')
+            if (la < -1e-10).any():
+                raise Exception
+            return out
+
         # D1(x1,x2) = 1/2 ( cos(sqrt(x)) / x - sin(sqrt(x)) / x**(3/2) )  if x1==x2
-        D1[irow, icol] = 0.5*(np.cos(np.sqrt(Λ[irow])) / Λ[irow] - np.sin(np.sqrt(Λ[irow])) / (Λ[irow]**(1.5)))
+        # D1[irow, icol] = 0.5*(np.cos(np.sqrt(Λ[irow])) / Λ[irow] - np.sin(np.sqrt(Λ[irow])) / (Λ[irow]**(1.5)))
+        D1[irow, icol] = f1(Λ[irow])
+
         # D²: TODO insert formula
         v = np.cos(np.sqrt(Λ))
         v = v[:, np.newaxis]
         D2 = np.ma.masked_array(v - v.T, mask=mask) / diffL
         # D2(x1, x2) = -1/2 sin(sqrt(x)) / sqrt(x) if x1==x2
-        D2[irow, icol] = -0.5*np.sin(np.sqrt(Λ[irow])) / np.sqrt(Λ[irow])
+        D2[irow, icol] = -0.5*np.sinc(np.sqrt(Λ[irow])/np.pi)
 
         # compute K: TODO copy/paste formula from the paper
         RtHCtxR = np.array(R.H * (Hc.H * x) * R)
         RtHCtc0R = np.array(R.H * Hc.H * c0 * R)
         K = np.matrix(RtHCtxR*np.array(D1) + RtHCtc0R*np.array(D2))
         # compute ∂E/∂x
-        dEdx = Hc*invU*sinU + x*(R*(K.H + K)*R.H)
+        dEdx = Hc*sincU + x*(R*(K.H + K)*R.H)
 
         # Lagrange multiplier
         # TODO: this can be precomputed and stored
