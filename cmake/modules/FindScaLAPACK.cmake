aec20dd4ef2003a344ae83a7b2fc43afb485a372
toxa81 <toxa81@gmail.com>
2018-09-20 09:11:25 +0200

    tweak of cmake scripts

--- cmake/modules/FindScaLAPACK.cmake
+++ cmake/modules/FindScaLAPACK.cmake
@@ -2,23 +2,21 @@ include(FindPackageHandleStandardArgs)
 find_package(PkgConfig REQUIRED)
 
 pkg_search_module(_SCALAPACK scalapack)
-find_library(SCALAPACK_LIBRARIES NAMES scalapack
+find_library(SCALAPACK_LIBRARIES
+  NAMES scalapack
   HINTS
-#  ${_SCALAPACK_LIBRARY_DIRS}
-  ENV SALAPACKROOT
-  /usr/lib
-  PATH_SUFFIXES
-  lib
+  ${_SCALAPACK_LIBRARY_DIRS}
+  ENV SCALAPACKROOT
+  /usr
+  PATH_SUFFIXES lib
   DOC "scalapack library path")
 
 find_path(SCALAPACK_INCLUDE_DIR
-  pblas.h
-  PATH_SUFFIXES
-  include
-  src
+  NAMES pblas.h
+  PATH_SUFFIXES include src
   HINTS
   ENV SCALAPACKROOT
-#  ${_SCALAPACK_INCLUDE_DIRS}
-  /usr/lib)
+  ${_SCALAPACK_INCLUDE_DIRS}
+  /usr)
 
 find_package_handle_standard_args(ScaLAPACK DEFAULT_MSG SCALAPACK_LIBRARIES SCALAPACK_INCLUDE_DIR)
