911939e74e0a514064cae44f5702c64a9d450c5b
Simon Pintarelli <simon.pintarelli@gmail.com>
2018-10-03 21:10:55 +0200

    python module: ot_basic cosmetics/formatting

--- python_module/sirius/ot/ot_basic.py
+++ python_module/sirius/ot/ot_basic.py
@@ -20,9 +20,9 @@ def pp_total_energy(potential, density, k_point_set, ctx):
     # TODO: Ewald energy is constant...
     return (k_point_set.valence_eval_sum() - potential.energy_vxc(density) -
             potential.PAW_one_elec_energy() - 0.5 * potential.energy_vha() -
-            energy_bxc(density, potential, ctx.num_mag_dims()) +
-            potential.energy_exc(density) + potential.PAW_total_energy() +
-            ewald_energy(ctx, gvec, unit_cell))
+            energy_bxc(density, potential,
+                       ctx.num_mag_dims()) + potential.energy_exc(density) +
+            potential.PAW_total_energy() + ewald_energy(ctx, gvec, unit_cell))
 
 
 class Energy:
@@ -60,10 +60,9 @@ class Energy:
                 k, ispn = key
                 self.kpointset[k].spinor_wave_functions().pw_coeffs(ispn)[:] = val
                 assert(np.isclose(matview(val).H*val, np.eye(val.shape[1])).all())
-                # TODO: don't know if copy is required
-                # function below copies everything twice, TODO: loop over k-view
+            # copy to device (if needed)
             for ki in cn.kvalues():
-                psi = self.kpointset[k].spinor_wave_functions()
+                psi = self.kpointset[ki].spinor_wave_functions()
                 if psi.preferred_memory_t() == MemoryEnum.device:
                     psi.copy_to_gpu()
             # update density, potential
@@ -97,7 +96,7 @@ class Energy:
                                       1 / (bnd_occ * w))
                 for j, ek in enumerate(benergies):
                     if bnd_occ[j] > 1e-10:
-                        assert(np.abs(np.imag(ek)) < 1e-10)
+                        assert (np.abs(np.imag(ek)) < 1e-10)
                         self.kpointset[k].set_band_energy(j, ispn, np.real(ek))
                     else:
                         self.kpointset[k].set_band_energy(j, ispn, 0)
@@ -118,7 +117,7 @@ class Energy:
             self.potential.fft_transform(1)
             # after updating H to the new position, we can compute new band energies
             bnd_occ = k.band_occupancy(ispn)
-            assert(np.isclose(bnd_occ, (bnd_occ+1e-4).astype(int)).all())
+            assert (np.isclose(bnd_occ, (bnd_occ + 1e-4).astype(int)).all())
             w = k.weight()
             yn = self.H(cn, ki=ki, ispn=ispn)
             # Hc is scaled by band occupancies, need to divide here to get correct band energies
@@ -165,8 +164,10 @@ class ApplyHamiltonian:
                 # copy coefficients from Psi_y
                 for i, _ in ispn_coeffs:
                     bnd_occ = np.array(kpoint.band_occupancy(i))
-                    assert(np.isclose(bnd_occ, (bnd_occ+1e-4).astype(int)).all())
-                    out[(k, i)] = np.array(Psi_y.pw_coeffs(i), copy=False) * bnd_occ * w
+                    assert (np.isclose(bnd_occ,
+                                       (bnd_occ + 1e-4).astype(int)).all())
+                    out[(k, i)] = np.array(
+                        Psi_y.pw_coeffs(i), copy=False) * bnd_occ * w
             # end for
             return out
         else:
@@ -190,7 +191,9 @@ class ApplyHamiltonian:
         from ..coefficient_array import PwCoeffs
 
         if not isinstance(cn, PwCoeffs):
-            raise TypeError('argument to ApplyHamiltonian.__matmul__ must be of type PwCoeffs')
+            raise TypeError(
+                'argument to ApplyHamiltonian.__matmul__ must be of type PwCoeffs'
+            )
         return self.apply(cn)
 
     def __call__(self, cn, ki=None, ispn=None):
