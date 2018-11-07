electronic-structure/SIRIUS

commit 9341a9d986ce8d516d17e35235b0e363b396ba44

Do not calculate the core corrections for forces and stresses if the core radial integrals are not set up

src/Geometry/stress.hpp
@@ -861,6 +861,22 @@ class Stress {
     {
         stress_core_.zero();
 
+        bool empty = true;
+
+        /* check if the core atomic wave functions are set up or not */
+        /* if not then the core correction is simply ignored */
+
+        for (int ia = 0; (ia < ctx_.unit_cell.num_atoms()) && empty; ia++) {
+            Atom& atom = ctx_.unit_cell.atom(ia);
+            if (!atom.type().ps_core_charge_density().empty()) {
+                empty = false;
+           }
+        }
+
+        if (empty) {
+            return stress_core_;
+        }
+
         potential_.xc_potential().fft_transform(-1);
 
         auto& ri_dg = ctx_.ps_core_ri_djl();

src/Geometry/force.hpp
@@ -516,7 +516,9 @@ class Force
             #pragma omp parallel for
             for (int ia = 0; ia < unit_cell.num_atoms(); ia++) {
                 Atom& atom = unit_cell.atom(ia);
-
+                if (atom.type().ps_core_charge_density().empty()) {
+                    continue;
+                }
                 int iat = atom.type_id();
 
                 for (int igloc = 0; igloc < gvec_count; igloc++) {

commit 7543f3bc16f727b55841bdafb333b1ab9a3e68e1

Fix a compilation issue originated from commit 9341a9d986ce8d516d17e35235b0e363b396ba44
No modification of the code excepted a typo that render the compilation void. the typo was introduced by commit 9341a9d986ce8d516d17e35235b0e363b396ba44.

src/Geometry/stress.hpp
@@ -866,8 +866,8 @@ class Stress {
         /* check if the core atomic wave functions are set up or not */
         /* if not then the core correction is simply ignored */
 
-        for (int ia = 0; (ia < ctx_.unit_cell.num_atoms()) && empty; ia++) {
-            Atom& atom = ctx_.unit_cell.atom(ia);
+        for (int ia = 0; (ia < ctx_.unit_cell().num_atoms()) && empty; ia++) {
+            Atom& atom = ctx_.unit_cell().atom(ia);
             if (!atom.type().ps_core_charge_density().empty()) {
                 empty = false;
            }