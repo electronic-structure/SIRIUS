55b0674fccd4f807153b5e0f1bd65467912a99e0
Simon Pintarelli <simon.pintarelli@cscs.ch>
2018-11-28 12:00:47 +0100

    pybind11: set_atom_positions properly use given strides

--- python_module/unit_cell_setters.hpp
+++ python_module/unit_cell_setters.hpp
@@ -15,6 +15,9 @@ void set_atom_positions(Unit_cell& unit_cell, pybind11::buffer positions)
 
     buffer_info info = positions.request();
     int npositions = info.shape[0];
+    if (info.ndim != 2) {
+        throw std::runtime_error("set_atom_positions: wrong dimension (expected 2d array)");
+    }
     if (info.shape[1] != 3) {
         throw std::runtime_error("set_atom_positions: wrong shape");
     }
@@ -24,10 +27,15 @@ void set_atom_positions(Unit_cell& unit_cell, pybind11::buffer positions)
     if (info.format != format_descriptor<double>::format())
         throw std::runtime_error("Incompatible format: expected a double array!");
 
+    const double* ptr = static_cast<double*>(info.ptr);
+    int s0 = info.strides[0] / sizeof(double);
+    int s1 = info.strides[1] / sizeof(double);
     for (int ia = 0; ia < npositions; ++ia) {
         auto& atom = unit_cell.atom(ia);
-        double* pos = static_cast<double*>(info.ptr) + 3*ia;
-        atom.set_position({pos[0], pos[1], pos[2]});
+        double x = *(ptr + s0*ia + s1*0);
+        double y = *(ptr + s0*ia + s1*1);
+        double z = *(ptr + s0*ia + s1*2);
+        atom.set_position({x, y, z});
     }
 }
 
@@ -61,7 +69,6 @@ void set_lattice_vectors(Unit_cell& unit_cell,
     auto l3 = to_vector(l3buf);
 
     unit_cell.set_lattice_vectors(l1, l2, l3);
-
 }
 
 }  // sirius
