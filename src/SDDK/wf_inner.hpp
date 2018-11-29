4f9bf61ff553f7f452d51352a859ce2f5db13a60
toxa81 <toxa81@gmail.com>
2018-11-29 10:50:28 +0100

    try many small mpi_allreduce

--- src/SDDK/wf_inner.hpp
+++ src/SDDK/wf_inner.hpp
@@ -570,26 +570,30 @@ inline void inner(memory_t        mem__,
         }
         return;
     } else if (result__.comm().size() == 1) { /* parallel wave-functions distribution but sequential diagonalization */
-        mdarray<T, 2> tmp(m__, n__);
+        //mdarray<T, 2> tmp(m__, n__);
+        //if (is_device_memory(mem__)) {
+        //    tmp.allocate(memory_t::device);
+        //}
+        T* buf = result__.at(mem__, irow0__, jcol0__);
+        local_inner(i0__, m__, j0__, n__, buf, result__.ld(), stream_id(-1));
         if (is_device_memory(mem__)) {
-            tmp.allocate(memory_t::device);
-        }
-        T* buf = tmp.at(mem__);
-        local_inner(i0__, m__, j0__, n__, buf, m__, stream_id(-1));
-        if (is_device_memory(mem__)) {
-            tmp.copy_to(memory_t::host);
+            acc::copyout(result__.at(memory_t::host, irow0__, jcol0__), result__.ld(),
+                         result__.at(memory_t::device, irow0__, jcol0__), result__.ld(),
+                         m__, n__);
         }
         utils::timer t1("sddk::inner|mpi");
-        comm.allreduce(&tmp[0], static_cast<int>(tmp.size()));
-        t1.stop();
-        utils::timer t2("sddk::inner|store");
-        #pragma omp parallel for schedule(static)
         for (int j = 0; j < n__; j++) {
-            for (int i = 0; i < m__; i++) {
-                result__(irow0__ + i, jcol0__ + j) = tmp(i, j);
-            }
+            comm.allreduce(&result__(irow0__, jcol0__ + j), m__);
         }
-        t2.stop();
+        t1.stop();
+        //utils::timer t2("sddk::inner|store");
+        //#pragma omp parallel for schedule(static)
+        //for (int j = 0; j < n__; j++) {
+        //    for (int i = 0; i < m__; i++) {
+        //        result__(irow0__ + i, jcol0__ + j) = tmp(i, j);
+        //    }
+        //}
+        //t2.stop();
 #ifdef __GPU
         if (is_device_memory(mem__)) {
             acc::copyin(result__.at(memory_t::device, irow0__, jcol0__), result__.ld(),
