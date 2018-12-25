d26a5e7ebeca56cb96dd790cb5ba7c9ac1b5bab9
Simon Pintarelli <simon.pintarelli@cscs.ch>
2018-12-25 14:21:43 +0100

    cmake: make_version_hpp
    run git rev-parse in source directory, catch errors

--- make_version_hpp.py
+++ make_version_hpp.py
@@ -2,6 +2,7 @@ import sys
 import subprocess
 import datetime
 import json
+import os
 
 # Use github REST API to query the tags
 request_str = 'https://api.github.com/repos/electronic-structure/SIRIUS/tags'
@@ -17,7 +18,7 @@ def to_string(s):
         return s.decode('utf-8')
 
 
-def get_sha(vstr):
+def get_sha(vstr, dirname):
     """
     Get SHA hash of the code.
     Try to call git. If git command is not found or this is not a git repository, try to read
@@ -29,25 +30,30 @@ def get_sha(vstr):
     try:
         p = subprocess.Popen(["git", "rev-parse", "HEAD"],
                              stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
+                             stderr=subprocess.PIPE,
+                             cwd=dirname)
         sha_str = p.communicate()[0].strip()
-        if p.returncode != 0: raise
-    except:
+        if p.returncode is not 0:
+            raise RuntimeError
+        return to_string(sha_str)
+    except RuntimeError:
         # python2 and python3 handle URL requests differently
-        if sys.version_info < (3, 0):
-            import urllib2
-            data = json.load(urllib2.urlopen(request_str))
-        else:
-            import urllib.request
-            req = urllib.request.Request(request_str)
-            data = json.load(urllib.request.urlopen(req))
-        # serach for a version tag
-        for e in data:
-            if e['name'] == 'v' + vstr:
-                sha_str = e['commit']['sha']
-                break
-
-    return to_string(sha_str)
+        try:
+            if sys.version_info < (3, 0):
+                from urllib2 import urlopen, URLError
+                data = json.load(urlopen(request_str))
+            else:
+                from urllib.request import Request, urlopen, URLError
+                req = Request(request_str)
+                data = json.load(urlopen(req))
+            # search for a version tag
+            for e in data:
+                if e['name'] == 'v' + vstr:
+                    sha_str = e['commit']['sha']
+                    break
+            return to_string(sha_str)
+        except URLError:
+            return 'GIT_SHA_UNKNOWN'
 
 
 def get_branch(sha_str, vstr):
@@ -60,8 +66,9 @@ def get_branch(sha_str, vstr):
                              stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
         branch_name = p.communicate()[0].strip()
-        if p.returncode != 0: raise
-    except:
+        if p.returncode is not 0:
+            raise RuntimeError
+    except RuntimeError:
         if sha_str:
             branch_name = 'release tag v%s' % vstr
     return to_string(branch_name)
@@ -74,21 +81,21 @@ def main():
     print("#ifndef __VERSION_HPP__")
     print("#define __VERSION_HPP__")
 
-    now = datetime.datetime.now()
+    fname = sys.argv[1]  # path to VERSION file
 
     version_str = ""
     # get version string from the file
     try:
         with open(sys.argv[1]) as vf:
             version_str = vf.readline().strip()
-    except:
+    except OSError:
         pass
-    sha_str = get_sha(version_str)
+    sha_str = get_sha(version_str, os.path.dirname(fname))
     branch_name = get_branch(sha_str, version_str)
 
     print("const char* const git_hash = \"%s\";" % sha_str)
     print("const char* const git_branchname = \"%s\";" % branch_name)
-    #print("const char* const build_date = \"%s\";"%(now.strftime("%a, %e %b %Y %H:%M:%S")))
+    # print("const char* const build_date = \"%s\";"%(now.strftime("%a, %e %b %Y %H:%M:%S")))
     print("#endif")
 
 
