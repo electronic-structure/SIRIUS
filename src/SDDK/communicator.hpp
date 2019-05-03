5ca8553399984ab08c1d709823f418e0fff93bb7
Simon Pintarelli <simon.pintarelli@cscs.ch>
2019-05-03 20:55:42 +0200

    acc::set_device_id, avoid cuda multithreading bugs
    According to
    
    https://devblogs.nvidia.com/cuda-pro-tip-always-set-current-device-avoid-multithreading-bugs/
    
    `cudaSetDevice` must be called in the beginning of each omp parallel region.
    Preparatory initialization in a omp parallel region isn't enough since new
    threads might be spawned later.

--- src/SDDK/communicator.hpp
+++ src/SDDK/communicator.hpp
@@ -835,26 +835,27 @@ inline int get_device_id(int num_devices__)
         return id;
     }
     if (id == -1) {
-        int r = Communicator::world().rank();
-        char name[MPI_MAX_PROCESSOR_NAME];
-        int len;
-        CALL_MPI(MPI_Get_processor_name, (name, &len));
-        std::vector<size_t> hash(Communicator::world().size());
-        hash[r] = std::hash<std::string>{}(std::string(name, len));
-        Communicator::world().allgather(hash.data(), r, 1);
-        std::map<size_t, std::vector<int>> rank_map;
-        for (int i = 0; i < Communicator::world().size(); i++) {
-            rank_map[hash[i]].push_back(i);
-        }
-        for (int i = 0; i < (int)rank_map[hash[r]].size(); i++) {
-            if (rank_map[hash[r]][i] == r) {
-                id = i % num_devices__;
-                break;
+        #pragma omp single
+        {
+            int r = Communicator::world().rank();
+            char name[MPI_MAX_PROCESSOR_NAME];
+            int len;
+            CALL_MPI(MPI_Get_processor_name, (name, &len));
+            std::vector<size_t> hash(Communicator::world().size());
+            hash[r] = std::hash<std::string>{}(std::string(name, len));
+            Communicator::world().allgather(hash.data(), r, 1);
+            std::map<size_t, std::vector<int>> rank_map;
+            for (int i = 0; i < Communicator::world().size(); i++) {
+                rank_map[hash[i]].push_back(i);
+            }
+            for (int i = 0; i < (int)rank_map[hash[r]].size(); i++) {
+                if (rank_map[hash[r]][i] == r) {
+                    id = i % num_devices__;
+                    break;
+                }
             }
         }
-        if (id == -1) {
-            throw std::runtime_error("get_device_id(): wrong device id was found");
-        }
+        assert(id >= 0);
     }
     return id;
 }
