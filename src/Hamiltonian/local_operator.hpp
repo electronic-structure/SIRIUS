5e4c49605d106fe64861ea37207dcdf2829d65f6
toxa81 <toxa81@gmail.com>
2019-07-02 15:23:54 +0200

    add timer

--- src/Hamiltonian/local_operator.hpp
+++ src/Hamiltonian/local_operator.hpp
@@ -91,6 +91,8 @@ class Local_operator
         , gvec_coarse_p_(gvec_coarse_p__)
 
     {
+        PROFILE("sirius::Local_operator");
+
         for (int j = 0; j < ctx_.num_mag_dims() + 1; j++) {
             veff_vec_[j] = Smooth_periodic_function<double>(fft_coarse__, gvec_coarse_p__);
             for (int ir = 0; ir < fft_coarse_.local_size(); ir++) {
