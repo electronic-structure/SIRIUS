395b37a4728f4bcd78badd78f8ce869aa48251fa
Simon Frasch <simon.frasch@cscs.ch>
2019-07-23 15:27:06 +0200

    SDDK folder: split more files into hpp / cpp

---
+++ src/SDDK/dmatrix.cpp
@@ -0,0 +1,322 @@
+// Copyright (c) 2013-2017 Anton Kozhevnikov, Thomas Schulthess
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+// the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
+//    following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+//    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "dmatrix.hpp"
+
+namespace sddk {
+
+    template<typename T>
+    dmatrix<T>::dmatrix(int num_rows__, int num_cols__, BLACS_grid const &blacs_grid__, int bs_row__, int bs_col__,
+                        memory_t mem_type__)
+            : matrix<T>(splindex<splindex_t::block_cyclic>(num_rows__, blacs_grid__.num_ranks_row(), blacs_grid__.rank_row(), bs_row__).local_size(),
+                        splindex<splindex_t::block_cyclic>(num_cols__, blacs_grid__.num_ranks_col(), blacs_grid__.rank_col(), bs_col__).local_size(),
+                        mem_type__)
+            , num_rows_(num_rows__)
+            , num_cols_(num_cols__)
+            , bs_row_(bs_row__)
+            , bs_col_(bs_col__)
+            , blacs_grid_(&blacs_grid__)
+            , spl_row_(num_rows_, blacs_grid__.num_ranks_row(), blacs_grid__.rank_row(), bs_row_)
+            , spl_col_(num_cols_, blacs_grid__.num_ranks_col(), blacs_grid__.rank_col(), bs_col_)
+    {
+        init();
+    }
+
+    template<typename T>
+    dmatrix<T>::dmatrix(int num_rows__, int num_cols__, memory_t mem_type__)
+            : matrix<T>(num_rows__, num_cols__, mem_type__)
+            , num_rows_(num_rows__)
+            , num_cols_(num_cols__)
+            , bs_row_(1)
+            , bs_col_(1)
+            , spl_row_(num_rows_, 1, 0, bs_row_)
+            , spl_col_(num_cols_, 1, 0, bs_col_)
+    {
+    }
+
+    template<typename T>
+    dmatrix<T>::dmatrix(T *ptr__, int num_rows__, int num_cols__, BLACS_grid const &blacs_grid__, int bs_row__,
+                        int bs_col__)
+            : matrix<T>(ptr__,
+                        splindex<splindex_t::block_cyclic>(num_rows__, blacs_grid__.num_ranks_row(), blacs_grid__.rank_row(), bs_row__).local_size(),
+                        splindex<splindex_t::block_cyclic>(num_cols__, blacs_grid__.num_ranks_col(), blacs_grid__.rank_col(), bs_col__).local_size())
+            , num_rows_(num_rows__)
+            , num_cols_(num_cols__)
+            , bs_row_(bs_row__)
+            , bs_col_(bs_col__)
+            , blacs_grid_(&blacs_grid__)
+            , spl_row_(num_rows_, blacs_grid__.num_ranks_row(), blacs_grid__.rank_row(), bs_row_)
+            , spl_col_(num_cols_, blacs_grid__.num_ranks_col(), blacs_grid__.rank_col(), bs_col_)
+    {
+        init();
+    }
+
+    template<typename T>
+    dmatrix<T>::dmatrix(memory_pool &mp__, int num_rows__, int num_cols__, BLACS_grid const &blacs_grid__, int bs_row__,
+                        int bs_col__)
+            : matrix<T>(mp__,
+                        splindex<splindex_t::block_cyclic>(num_rows__, blacs_grid__.num_ranks_row(), blacs_grid__.rank_row(), bs_row__).local_size(),
+                        splindex<splindex_t::block_cyclic>(num_cols__, blacs_grid__.num_ranks_col(), blacs_grid__.rank_col(), bs_col__).local_size())
+            , num_rows_(num_rows__)
+            , num_cols_(num_cols__)
+            , bs_row_(bs_row__)
+            , bs_col_(bs_col__)
+            , blacs_grid_(&blacs_grid__)
+            , spl_row_(num_rows_, blacs_grid__.num_ranks_row(), blacs_grid__.rank_row(), bs_row_)
+            , spl_col_(num_cols_, blacs_grid__.num_ranks_col(), blacs_grid__.rank_col(), bs_col_)
+    {
+        init();
+    }
+
+    template<typename T>
+    dmatrix<T>::dmatrix(T *ptr__, int num_rows__, int num_cols__)
+            : matrix<T>(ptr__, num_rows__, num_cols__)
+            , num_rows_(num_rows__)
+            , num_cols_(num_cols__)
+            , bs_row_(1)
+            , bs_col_(1)
+            , spl_row_(num_rows_, 1, 0, bs_row_)
+            , spl_col_(num_cols_, 1, 0, bs_col_)
+    {
+        init();
+    }
+
+    template<typename T>
+    void dmatrix<T>::set(int ir0__, int jc0__, int mr__, int nc__, T *ptr__, int ld__) {
+        splindex<splindex_t::block_cyclic> spl_r0(ir0__,        blacs_grid().num_ranks_row(), blacs_grid().rank_row(), bs_row_);
+        splindex<splindex_t::block_cyclic> spl_r1(ir0__ + mr__, blacs_grid().num_ranks_row(), blacs_grid().rank_row(), bs_row_);
+
+        splindex<splindex_t::block_cyclic> spl_c0(jc0__,        blacs_grid().num_ranks_col(), blacs_grid().rank_col(), bs_col_);
+        splindex<splindex_t::block_cyclic> spl_c1(jc0__ + nc__, blacs_grid().num_ranks_col(), blacs_grid().rank_col(), bs_col_);
+
+        int m0 = spl_r0.local_size();
+        int m1 = spl_r1.local_size();
+        int n0 = spl_c0.local_size();
+        int n1 = spl_c1.local_size();
+        std::vector<int> map_row(m1 - m0);
+        std::vector<int> map_col(n1 - n0);
+
+        for (int i = 0; i < m1 - m0; i++) {
+            map_row[i] = spl_r1[m0 + i] - ir0__;
+        }
+        for (int j = 0; j < n1 - n0; j++) {
+            map_col[j] = spl_c1[n0 + j] - jc0__;
+        }
+
+        //#pragma omp parallel for
+        for (int j = 0; j < n1 - n0; j++) {
+            for (int i = 0; i < m1 - m0; i++) {
+                (*this)(m0 + i, n0 + j) = ptr__[map_row[i] + ld__ * map_col[j]];
+            }
+        }
+    }
+
+    template<typename T>
+    void dmatrix<T>::set(const int irow_glob, const int icol_glob, T val) {
+        auto r = spl_row_.location(irow_glob);
+        if (blacs_grid_->rank_row() == r.rank) {
+            auto c = spl_col_.location(icol_glob);
+            if (blacs_grid_->rank_col() == c.rank) {
+                (*this)(r.local_index, c.local_index) = val;
+            }
+        }
+    }
+
+    template<typename T>
+    void dmatrix<T>::add(const int irow_glob, const int icol_glob, T val) {
+        auto r = spl_row_.location(irow_glob);
+        if (blacs_grid_->rank_row() == r.rank) {
+            auto c = spl_col_.location(icol_glob);
+            if (blacs_grid_->rank_col() == c.rank) {
+                (*this)(r.local_index, c.local_index) += val;
+            }
+        }
+    }
+
+    template<typename T>
+    void dmatrix<T>::add(double beta__, const int irow_glob, const int icol_glob, T val) {
+        auto r = spl_row_.location(irow_glob);
+        if (blacs_grid_->rank_row() == r.rank) {
+            auto c = spl_col_.location(icol_glob);
+            if (blacs_grid_->rank_col() == c.rank) {
+                (*this)(r.local_index, c.local_index) = (*this)(r.local_index, c.local_index) * beta__ + val;
+            }
+        }
+    }
+
+    template<typename T>
+    void dmatrix<T>::make_real_diag(int n__) {
+        for (int i = 0; i < n__; i++) {
+            auto r = spl_row_.location(i);
+            if (blacs_grid_->rank_row() == r.rank) {
+                auto c = spl_col_.location(i);
+                if (blacs_grid_->rank_col() == c.rank) {
+                    T v = (*this)(r.local_index, c.local_index);
+                    (*this)(r.local_index, c.local_index) = std::real(v);
+                }
+            }
+        }
+    }
+
+    template<typename T>
+    mdarray<T, 1> dmatrix<T>::get_diag(int n__) {
+        mdarray<T, 1> d(n__);
+        d.zero();
+
+        for (int i = 0; i < n__; i++) {
+            auto r = spl_row_.location(i);
+            if (blacs_grid_->rank_row() == r.rank) {
+                auto c = spl_col_.location(i);
+                if (blacs_grid_->rank_col() == c.rank) {
+                    d[i] = (*this)(r.local_index, c.local_index);
+                }
+            }
+        }
+        blacs_grid_->comm().allreduce(d.template at(memory_t::host), n__);
+        return d;
+    }
+
+    template<typename T>
+    void dmatrix<T>::save_to_hdf5(std::string name__, int m__, int n__) {
+        mdarray<T, 2> full_mtrx(m__, n__);
+        full_mtrx.zero();
+
+        for (int j = 0; j < this->num_cols_local(); j++) {
+            for (int i = 0; i < this->num_rows_local(); i++) {
+                if (this->irow(i) < m__ &&  this->icol(j) < n__) {
+                    full_mtrx(this->irow(i), this->icol(j)) = (*this)(i, j);
+                }
+            }
+        }
+        this->comm().allreduce(full_mtrx.template at(memory_t::host), static_cast<int>(full_mtrx.size()));
+
+        if (this->blacs_grid().comm().rank() == 0) {
+            HDF5_tree h5(name__, hdf5_access_t::truncate);
+            h5.write("nrow", m__);
+            h5.write("ncol", n__);
+            h5.write("mtrx", full_mtrx);
+        }
+    }
+
+template <>
+void dmatrix<double_complex>::serialize(std::string name__, int n__) const
+{
+    mdarray<double_complex, 2> full_mtrx(num_rows(), num_cols());
+    full_mtrx.zero();
+
+    for (int j = 0; j < num_cols_local(); j++) {
+        for (int i = 0; i < num_rows_local(); i++) {
+            full_mtrx(irow(i), icol(j)) = (*this)(i, j);
+        }
+    }
+    blacs_grid_->comm().allreduce(full_mtrx.at(memory_t::host), static_cast<int>(full_mtrx.size()));
+
+    //json dict;
+    //dict["mtrx_re"] = json::array();
+    //for (int i = 0; i < num_rows(); i++) {
+    //    dict["mtrx_re"].push_back(json::array());
+    //    for (int j = 0; j < num_cols(); j++) {
+    //        dict["mtrx_re"][i].push_back(full_mtrx(i, j).real());
+    //    }
+    //}
+
+    if (blacs_grid_->comm().rank() == 0) {
+        //std::cout << "mtrx: " << name__ << std::endl;
+        // std::cout << dict.dump(4);
+
+        printf("matrix label: %s\n", name__.c_str());
+        printf("{\n");
+        for (int i = 0; i < n__; i++) {
+            printf("{");
+            for (int j = 0; j < n__; j++) {
+                printf("%18.13f + I * %18.13f", full_mtrx(i, j).real(), full_mtrx(i, j).imag());
+                if (j != n__ - 1) {
+                    printf(",");
+                }
+            }
+            if (i != n__ - 1) {
+                printf("},\n");
+            } else {
+                printf("}\n");
+            }
+        }
+        printf("}\n");
+    }
+
+    // std::ofstream ofs(aiida_output_file, std::ofstream::out | std::ofstream::trunc);
+    // ofs << dict.dump(4);
+}
+
+    template <>
+void dmatrix<double>::serialize(std::string name__, int n__) const
+{
+    mdarray<double, 2> full_mtrx(num_rows(), num_cols());
+    full_mtrx.zero();
+
+    for (int j = 0; j < num_cols_local(); j++) {
+        for (int i = 0; i < num_rows_local(); i++) {
+            full_mtrx(irow(i), icol(j)) = (*this)(i, j);
+        }
+    }
+    blacs_grid_->comm().allreduce(full_mtrx.at(memory_t::host), static_cast<int>(full_mtrx.size()));
+
+    //json dict;
+    //dict["mtrx"] = json::array();
+    //for (int i = 0; i < num_rows(); i++) {
+    //    dict["mtrx"].push_back(json::array());
+    //    for (int j = 0; j < num_cols(); j++) {
+    //        dict["mtrx"][i].push_back(full_mtrx(i, j));
+    //    }
+    //}
+
+    //if (blacs_grid_->comm().rank() == 0) {
+    //    std::cout << "mtrx: " << name__ << std::endl;
+    //    std::cout << dict.dump(4);
+    //}
+
+    // std::ofstream ofs(aiida_output_file, std::ofstream::out | std::ofstream::trunc);
+    // ofs << dict.dump(4);
+
+    if (blacs_grid_->comm().rank() == 0) {
+        printf("matrix label: %s\n", name__.c_str());
+        printf("{\n");
+        for (int i = 0; i < n__; i++) {
+            printf("{");
+            for (int j = 0; j < n__; j++) {
+                printf("%18.13f", full_mtrx(i, j));
+                if (j != n__ - 1) {
+                    printf(",");
+                }
+            }
+            if (i != n__ - 1) {
+                printf("},\n");
+            } else {
+                printf("}\n");
+            }
+        }
+        printf("}\n");
+    }
+}
+
+
+    // instantiate for required types
+    template class dmatrix<double>;
+    template class dmatrix<double_complex>;
+
+}
---
+++ src/SDDK/matrix_storage.cpp
@@ -0,0 +1,308 @@
+// Copyright (c) 2013-2017 Anton Kozhevnikov, Thomas Schulthess
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+// the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
+//    following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+//    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "matrix_storage.hpp"
+
+namespace sddk {
+
+
+    template<typename T>
+    void matrix_storage<T, matrix_storage_t::slab>::set_num_extra(int n__, int idx0__, memory_pool *mp__) {
+        PROFILE("sddk::matrix_storage::set_num_extra");
+
+        auto& comm_col = gvp_->comm_ortho_fft();
+
+        /* this is how n columns of the matrix will be distributed between columns of the MPI grid */
+        spl_num_col_ = splindex<splindex_t::block>(n__, comm_col.size(), comm_col.rank());
+
+        T* ptr{nullptr};
+        T* ptr_d{nullptr};
+        int ncol{0};
+
+        /* trivial case */
+        if (!is_remapped()) {
+            assert(num_rows_loc_ == gvp_->gvec_count_fft());
+            ncol = n__;
+            ptr = prime_.at(memory_t::host, 0, idx0__);
+            if (prime_.on_device()) {
+                ptr_d = prime_.at(memory_t::device, 0, idx0__);
+            }
+        } else {
+            /* maximum local number of matrix columns */
+            ncol = splindex_base<int>::block_size(n__, comm_col.size());
+            /* upper limit for the size of swapped extra matrix */
+            size_t sz = gvp_->gvec_count_fft() * ncol;
+            /* reallocate buffers if necessary */
+            if (extra_buf_.size() < sz) {
+                utils::timer t1("sddk::matrix_storage::set_num_extra|alloc");
+                if (mp__) {
+                    send_recv_buf_ = mdarray<T, 1>(*mp__, sz, "matrix_storage.send_recv_buf_");
+                    extra_buf_     = mdarray<T, 1>(*mp__, sz, "matrix_storage.extra_buf_");
+                } else {
+                    send_recv_buf_ = mdarray<T, 1>(sz, memory_t::host, "matrix_storage.send_recv_buf_");
+                    extra_buf_     = mdarray<T, 1>(sz, memory_t::host, "matrix_storage.extra_buf_");
+                }
+            }
+            ptr = extra_buf_.at(memory_t::host);
+        }
+        /* create the extra storage */
+        extra_ = mdarray<T, 2>(ptr, ptr_d, gvp_->gvec_count_fft(), ncol, "matrix_storage.extra_");
+    }
+
+    template<typename T>
+    void matrix_storage<T, matrix_storage_t::slab>::remap_from(const dmatrix<T> &mtrx__, int irow0__) {
+        PROFILE("sddk::matrix_storage::remap_from");
+
+        auto& comm = mtrx__.blacs_grid().comm();
+
+        /* cache cartesian ranks */
+        mdarray<int, 2> cart_rank(mtrx__.blacs_grid().num_ranks_row(), mtrx__.blacs_grid().num_ranks_col());
+        for (int i = 0; i < mtrx__.blacs_grid().num_ranks_col(); i++) {
+            for (int j = 0; j < mtrx__.blacs_grid().num_ranks_row(); j++) {
+                cart_rank(j, i) = mtrx__.blacs_grid().cart_rank(j, i);
+            }
+        }
+
+        if (send_recv_buf_.size() < prime_.size()) {
+            send_recv_buf_ = mdarray<T, 1>(prime_.size(), memory_t::host, "matrix_storage::send_recv_buf_");
+        }
+
+        block_data_descriptor rd(comm.size());
+        rd.counts[comm.rank()] = num_rows_loc();
+        comm.allgather(rd.counts.data(), comm.rank(), 1);
+        rd.calc_offsets();
+
+        block_data_descriptor sd(comm.size());
+
+        /* global index of column */
+        int j0 = 0;
+        /* actual number of columns in the submatrix */
+        int ncol = num_cols_;
+
+        splindex<splindex_t::block_cyclic> spl_col_begin(j0, mtrx__.num_ranks_col(), mtrx__.rank_col(), mtrx__.bs_col());
+        splindex<splindex_t::block_cyclic> spl_col_end(j0 + ncol, mtrx__.num_ranks_col(), mtrx__.rank_col(), mtrx__.bs_col());
+
+        int local_size_col = spl_col_end.local_size() - spl_col_begin.local_size();
+
+        for (int rank_row = 0; rank_row < comm.size(); rank_row++) {
+            if (!rd.counts[rank_row]) {
+                continue;
+            }
+            /* global index of column */
+            int i0 = rd.offsets[rank_row];
+            /* actual number of rows in the submatrix */
+            int nrow = rd.counts[rank_row];
+
+            assert(nrow != 0);
+
+            splindex<splindex_t::block_cyclic> spl_row_begin(irow0__ + i0, mtrx__.num_ranks_row(), mtrx__.rank_row(),
+                                                             mtrx__.bs_row());
+            splindex<splindex_t::block_cyclic> spl_row_end(irow0__ + i0 + nrow, mtrx__.num_ranks_row(), mtrx__.rank_row(),
+                                                           mtrx__.bs_row());
+
+            int local_size_row = spl_row_end.local_size() - spl_row_begin.local_size();
+
+            mdarray<T, 1> buf(local_size_row * local_size_col);
+
+            /* fetch elements of sub-matrix matrix */
+            if (local_size_row) {
+                for (int j = 0; j < local_size_col; j++) {
+                    std::memcpy(&buf[local_size_row * j],
+                                &mtrx__(spl_row_begin.local_size(), spl_col_begin.local_size() + j),
+                                local_size_row * sizeof(T));
+                }
+            }
+
+            sd.counts[comm.rank()] = local_size_row * local_size_col;
+            comm.allgather(sd.counts.data(), comm.rank(), 1);
+            sd.calc_offsets();
+
+            /* collect buffers submatrix */
+            T* send_buf = (buf.size() == 0) ? nullptr : &buf[0];
+            T* recv_buf = (send_recv_buf_.size() == 0) ? nullptr : &send_recv_buf_[0];
+            comm.gather(send_buf, recv_buf, sd.counts.data(), sd.offsets.data(), rank_row);
+
+            if (comm.rank() == rank_row) {
+                /* unpack data */
+                std::vector<int> counts(comm.size(), 0);
+                for (int jcol = 0; jcol < ncol; jcol++) {
+                    auto pos_jcol = mtrx__.spl_col().location(j0 + jcol);
+                    for (int irow = 0; irow < nrow; irow++) {
+                        auto pos_irow = mtrx__.spl_row().location(irow0__ + i0 + irow);
+                        int rank      = cart_rank(pos_irow.rank, pos_jcol.rank);
+
+                        prime_(irow, jcol) = send_recv_buf_[sd.offsets[rank] + counts[rank]];
+                        counts[rank]++;
+                    }
+                }
+                for (int rank = 0; rank < comm.size(); rank++) {
+                    assert(sd.counts[rank] == counts[rank]);
+                }
+            }
+        }
+    }
+
+    template<typename T>
+    void matrix_storage<T, matrix_storage_t::slab>::remap_backward(int n__, int idx0__) {
+        PROFILE("sddk::matrix_storage::remap_backward");
+
+        /* trivial case when extra storage mirrors the prime storage */
+        if (!is_remapped()) {
+            return;
+        }
+
+        auto& comm_col = gvp_->comm_ortho_fft();
+
+        auto& row_distr = gvp_->gvec_fft_slab();
+
+        assert(n__ == spl_num_col_.global_index_size());
+
+        /* local number of columns */
+        int n_loc = spl_num_col_.local_size();
+
+        /* reorder sending blocks */
+#pragma omp parallel for
+        for (int i = 0; i < n_loc; i++) {
+            for (int j = 0; j < comm_col.size(); j++) {
+                int offset = row_distr.offsets[j];
+                int count  = row_distr.counts[j];
+                if (count) {
+                    std::memcpy(&send_recv_buf_[offset * n_loc + count * i], &extra_(offset, i), count * sizeof(T));
+                }
+            }
+        }
+
+        /* send and recieve dimensions */
+        block_data_descriptor sd(comm_col.size()), rd(comm_col.size());
+        for (int j = 0; j < comm_col.size(); j++) {
+            sd.counts[j] = spl_num_col_.local_size(comm_col.rank()) * row_distr.counts[j];
+            rd.counts[j] = spl_num_col_.local_size(j) * row_distr.counts[comm_col.rank()];
+        }
+        sd.calc_offsets();
+        rd.calc_offsets();
+
+        T* recv_buf = (num_rows_loc_ == 0) ? nullptr : prime_.at(memory_t::host, 0, idx0__);
+
+        utils::timer t1("sddk::matrix_storage::remap_backward|mpi");
+        comm_col.alltoall(send_recv_buf_.at(memory_t::host), sd.counts.data(), sd.offsets.data(), recv_buf,
+                          rd.counts.data(), rd.offsets.data());
+        t1.stop();
+
+        /* move data back to device */
+        if (prime_.on_device()) {
+            prime_.copy_to(memory_t::device, idx0__ * num_rows_loc(), n__ * num_rows_loc());
+        }
+    }
+
+    template<typename T>
+    void matrix_storage<T, matrix_storage_t::slab>::remap_forward(int n__, int idx0__, memory_pool *mp__) {
+        PROFILE("sddk::matrix_storage::remap_forward");
+
+        set_num_extra(n__, idx0__, mp__);
+
+        /* trivial case when extra storage mirrors the prime storage */
+        if (!is_remapped()) {
+            return;
+        }
+
+        auto& row_distr = gvp_->gvec_fft_slab();
+
+        auto& comm_col = gvp_->comm_ortho_fft();
+
+        /* local number of columns */
+        int n_loc = spl_num_col_.local_size();
+
+        /* send and recieve dimensions */
+        block_data_descriptor sd(comm_col.size()), rd(comm_col.size());
+        for (int j = 0; j < comm_col.size(); j++) {
+            sd.counts[j] = spl_num_col_.local_size(j) * row_distr.counts[comm_col.rank()];
+            rd.counts[j] = spl_num_col_.local_size(comm_col.rank()) * row_distr.counts[j];
+        }
+        sd.calc_offsets();
+        rd.calc_offsets();
+
+        T* send_buf = (num_rows_loc_ == 0) ? nullptr : prime_.at(memory_t::host, 0, idx0__);
+
+        utils::timer t1("sddk::matrix_storage::remap_forward|mpi");
+        comm_col.alltoall(send_buf, sd.counts.data(), sd.offsets.data(), send_recv_buf_.at(memory_t::host),
+                          rd.counts.data(), rd.offsets.data());
+        t1.stop();
+
+        /* reorder recieved blocks */
+#pragma omp parallel for
+        for (int i = 0; i < n_loc; i++) {
+            for (int j = 0; j < comm_col.size(); j++) {
+                int offset = row_distr.offsets[j];
+                int count  = row_distr.counts[j];
+                if (count) {
+                    std::memcpy(&extra_(offset, i), &send_recv_buf_[offset * n_loc + count * i], count * sizeof(T));
+                }
+            }
+        }
+    }
+
+    template<typename T>
+    void matrix_storage<T, matrix_storage_t::slab>::scale(memory_t mem__, int i0__, int n__, double beta__) {
+        if (is_host_memory(mem__)) {
+            for (int i = 0; i < n__; i++) {
+                for (int j = 0; j < num_rows_loc(); j++) {
+                    prime(j, i0__ + i) *= beta__;
+                }
+            }
+        } else {
+#if defined(__GPU)
+            scale_matrix_elements_gpu((acc_complex_double_t*)prime().at(mem__, 0, i0__), prime().ld(), num_rows_loc(),
+                                      n__, beta__);
+#endif
+        }
+    }
+
+    template<typename T>
+    double_complex matrix_storage<T, matrix_storage_t::slab>::checksum(device_t pu__, int i0__, int n__) {
+        double_complex cs(0, 0);
+
+        switch (pu__) {
+            case device_t::CPU: {
+                for (int i = 0; i < n__; i++) {
+                    for (int j = 0; j < num_rows_loc(); j++) {
+                        cs += prime(j, i0__ + i);
+                    }
+                }
+                break;
+            }
+            case device_t::GPU: {
+                mdarray<double_complex, 1> cs1(n__, memory_t::host, "checksum");
+                cs1.allocate(memory_t::device).zero(memory_t::device);
+#if defined(__GPU)
+                add_checksum_gpu(prime().at(memory_t::device, 0, i0__), num_rows_loc(), n__, cs1.at(memory_t::device));
+                cs1.copy_to(memory_t::host);
+                cs = cs1.checksum();
+#endif
+                break;
+            }
+        }
+        return cs;
+    }
+
+
+    // insantiate required types
+    template class matrix_storage<double, matrix_storage_t::slab>;
+    template class matrix_storage<double_complex, matrix_storage_t::slab>;
+
+}
