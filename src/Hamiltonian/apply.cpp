ee4555d565082e1bbaa0094c205cd82a7224570d
toxa81 <toxa81@gmail.com>
2019-08-29 12:57:28 +0200

    remove old code

--- src/Hamiltonian/apply.cpp
+++ src/Hamiltonian/apply.cpp
@@ -99,58 +99,6 @@ void Hamiltonian::apply_h_s(K_point* kp__, int ispn__, int N__, int n__, Wave_fu
 
     apply_non_local_d_q<T>(spin_range(ispn__), N__, n__, kp__->beta_projectors(), phi__, &D(), hphi__, &Q(), sphi__);
 
-    // for (int i = 0; i < kp__->beta_projectors().num_chunks(); i++) {
-    //    /* generate beta-projectors for a block of atoms */
-    //    kp__->beta_projectors().generate(i);
-    //    /* non-collinear case */
-    //    if (ispn__ == 2) {
-    //        for (int ispn = 0; ispn < 2; ispn++) {
-
-    //            auto beta_phi = kp__->beta_projectors().inner<T>(i, phi__, ispn, N__, n__);
-    //            if (ctx_.control().print_checksum_ && kp__->comm().rank() == 0) {
-    //                std::stringstream s;
-    //                s << "<beta|phi_" << ispn << ">";
-    //                auto cs = beta_phi.checksum();
-    //                utils::print_checksum(s.str(), cs);
-    //            }
-
-    //            if (hphi__) {
-    //                /* apply diagonal spin blocks */
-    //                D().apply(i, ispn, *hphi__, N__, n__, kp__->beta_projectors(), beta_phi);
-    //                /* apply non-diagonal spin blocks */
-    //                /* xor 3 operator will map 0 to 3 and 1 to 2 */
-    //                D().apply(i, ispn ^ 3, *hphi__, N__, n__, kp__->beta_projectors(), beta_phi);
-    //            }
-
-    //            if (sphi__) {
-    //                /* apply Q operator (diagonal in spin) */
-    //                Q().apply(i, ispn, *sphi__, N__, n__, kp__->beta_projectors(), beta_phi);
-    //                /* apply non-diagonal spin blocks */
-    //                if (ctx_.so_correction()) {
-    //                    Q().apply(i, ispn ^ 3, *sphi__, N__, n__, kp__->beta_projectors(), beta_phi);
-    //                }
-    //            }
-    //        }
-    //    } else { /* non-magnetic or collinear case */
-
-    //        auto beta_phi = kp__->beta_projectors().inner<T>(i, phi__, ispn__, N__, n__);
-    //        if (ctx_.control().print_checksum_ && kp__->comm().rank() == 0) {
-    //            std::stringstream s;
-    //            s << "<beta|phi_" << ispn__ << ">";
-    //            auto cs = beta_phi.checksum();
-    //            utils::print_checksum(s.str(), cs);
-    //        }
-
-    //        if (hphi__) {
-    //            D().apply(i, ispn__, *hphi__, N__, n__, kp__->beta_projectors(), beta_phi);
-    //        }
-
-    //        if (sphi__) {
-    //            Q().apply(i, ispn__, *sphi__, N__, n__, kp__->beta_projectors(), beta_phi);
-    //        }
-    //    }
-    //}
-
     /* apply the hubbard potential if relevant */
     if (ctx_.hubbard_correction() && !ctx_.gamma_point() && (hphi__ != NULL)) {
 
--- src/Kernels/mul_by_veff.cu
+++ src/Kernels/mul_by_veff.cu
@@ -1,4 +1,4 @@
-// Copyright (c) 2013-2018 Anton Kozhevnikov, Thomas Schulthess
+// Copyright (c) 2013-2019 Anton Kozhevnikov, Thomas Schulthess
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without modification, are permitted provided that
@@ -19,86 +19,11 @@
 
 /** \file mul_by_veff.cu
  *
- *  \brief CUDA kernel to multiply wave-functions by effective potential.
+ *  \brief CUDA kernels to multiply wave-functions by effective potential.
  */
 
 #include "../SDDK/GPU/cuda_common.hpp"
 #include "../SDDK/GPU/acc_runtime.hpp"
-
-//__global__ void mul_by_veff0_gpu_kernel(int                    size__,
-//                                        double* const*         veff__,
-//                                        acc_complex_double_t*       buf__)
-//{
-//    int i = blockDim.x * blockIdx.x + threadIdx.x;
-//    if (i < size__) {
-//        acc_complex_double_t z = buf__[i];
-//        double v0 = veff__[0][i];
-//        buf__[i] = make_accDoubleComplex(z.x * v0, z.y * v0);
-//    }
-//}
-//
-//__global__ void mul_by_veff1_gpu_kernel(int                    size__,
-//                                        double* const*         veff__,
-//                                        acc_complex_double_t*       buf__)
-//{
-//    int i = blockDim.x * blockIdx.x + threadIdx.x;
-//    if (i < size__) {
-//        acc_complex_double_t z = buf__[i];
-//        double v1 = veff__[1][i];
-//        buf__[i] = make_accDoubleComplex(z.x * v1, z.y * v1);
-//    }
-//}
-//
-//__global__ void mul_by_veff2_gpu_kernel(int                    size__,
-//                                        double* const*         veff__,
-//                                        acc_complex_double_t*       buf__)
-//{
-//    int i = blockDim.x * blockIdx.x + threadIdx.x;
-//    if (i < size__) {
-//        acc_complex_double_t z = buf__[i];
-//        acc_complex_double_t v = make_accDoubleComplex(veff__[2][i], -veff__[3][i]);  
-//        buf__[i] = accCmul(z, v);
-//    }
-//}
-//
-//__global__ void mul_by_veff3_gpu_kernel(int                    size__,
-//                                        double* const*         veff__,
-//                                        acc_complex_double_t*       buf__)
-//{
-//    int i = blockDim.x * blockIdx.x + threadIdx.x;
-//    if (i < size__) {
-//        acc_complex_double_t z = buf__[i];
-//        acc_complex_double_t v = make_accDoubleComplex(veff__[2][i], veff__[3][i]);
-//        buf__[i] = accCmul(z, v);
-//    }
-//}
-//
-//extern "C" void mul_by_veff_gpu(int ispn__, int size__, double* const* veff__, acc_complex_double_t* buf__)
-//{
-//    dim3 grid_t(64);
-//    dim3 grid_b(num_blocks(size__, grid_t.x));
-//
-//    switch (ispn__) {
-//        case 0: {
-//            accLaunchKernel((mul_by_veff0_gpu_kernel), dim3(grid_b), dim3(grid_t), 0, 0, size__, veff__, buf__);
-//            break;
-//        }
-//        case 1: {
-//            accLaunchKernel((mul_by_veff1_gpu_kernel), dim3(grid_b), dim3(grid_t), 0, 0, size__, veff__, buf__);
-//            break;
-//        }
-//        case 2: {
-//            accLaunchKernel((mul_by_veff2_gpu_kernel), dim3(grid_b), dim3(grid_t), 0, 0, size__, veff__, buf__);
-//            break;
-//        }
-//
-//        case 3: {
-//            accLaunchKernel((mul_by_veff3_gpu_kernel), dim3(grid_b), dim3(grid_t), 0, 0, size__, veff__, buf__);
-//            break;
-//        }
-//    }
-//}
-
 
 __global__ void mul_by_veff_real_real_gpu_kernel(int nr__, double* buf__, double const* veff__)
 {
