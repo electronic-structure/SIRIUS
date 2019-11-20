6bb9e29ad57d30077304e59bd0ed1603925eb0e8
toxa81 <toxa81@gmail.com>
2019-11-20 15:44:25 +0100

    update script

--- check_format.py
+++ check_format.py
@@ -2,20 +2,23 @@ import sys
 import subprocess
 import os
 from subprocess import Popen, PIPE
+import difflib
 
 env_copy = os.environ.copy()
 
-files = [name.strip() for name in sys.stdin.readlines()]
+all_files = [name.strip() for name in sys.stdin.readlines()]
+files = []
+for f in all_files:
+    extension = os.path.splitext(f)[1][1:]
+    if extension in ['h', 'hpp', 'cpp']:
+        files.append(f)
 
 print("files to check", files)
-
-#files = ['src/input.hpp']
 
 enable_shell = False
 
 status = 0
 for name in files:
-    print("%s: "%name, end='')
     with open(name, 'r') as f:
         original = f.read()
     p1 = subprocess.Popen(["sed", r"s/#pragma omp/\/\/#pragma omp/g", name], shell=enable_shell, stdout=subprocess.PIPE)
@@ -24,9 +27,11 @@ for name in files:
     formatted = p3.communicate()[0].decode('utf-8')
     p3.wait()
     if (original == formatted):
-        print("OK")
+        print("%s: OK"%name)
     else:
-        print("needs formatting")
+        print("%s: non-zero diff found"%name)
+        for line in difflib.unified_diff(original.splitlines(), formatted.splitlines(), fromfile='original', tofile='formatted'):
+            print(line)
         status += 1
 
 sys.exit(status)
