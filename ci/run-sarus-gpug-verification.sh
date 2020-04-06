e43b6368edfe029283605bc68009c914dc7c903c
Harmen Stoppels <harmenstoppels@gmail.com>
2020-04-06 14:52:11 +0200

    Move mpi flag to sarus not srun

--- ci/run-sarus-gpug-verification.sh
+++ ci/run-sarus-gpug-verification.sh
@@ -11,13 +11,9 @@
 
 set -e
 
-(
-    export CRAY_CUDA_MPS=1
-    export OMP_NUM_THREADS=3
-    export MPICH_MAX_THREAD_SAFETY=multiple
-    module list
-    echo "run-gpup-verification: running on $(hostname)"
-    srun -n4 -c2 --unbuffered --hint=nomultithread --mpi sarus run --mount=type=bind,source=$REPO_FOLDER,destination=$REPO_FOLDER $SARUS_IMAGE bash -c 'cd $REPO_FOLDER/verification/test01 && sirius.scf --test_against=output_ref.json --control.std_evp_solver_name=scalapack --control.gen_evp_solver_name=scalapack --control.mpi_grid_dims=2:2 --control.processing_unit=gpu'
-
-)
-
+export CRAY_CUDA_MPS=1
+export OMP_NUM_THREADS=3
+export MPICH_MAX_THREAD_SAFETY=multiple
+module list
+echo "run-gpup-verification: running on $(hostname)"
+srun -n4 -c2 --unbuffered --hint=nomultithread sarus run --mpi --mount=type=bind,source=$REPO_FOLDER,destination=$REPO_FOLDER $SARUS_IMAGE bash -c 'cd $REPO_FOLDER/verification/test01 && sirius.scf --test_against=output_ref.json --control.std_evp_solver_name=scalapack --control.gen_evp_solver_name=scalapack --control.mpi_grid_dims=2:2 --control.processing_unit=gpu'
