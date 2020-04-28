24be833e1a8ad8e48638e2552cdfeffaa3e03d97
Simon Frasch <simon.frasch@cscs.ch>
2020-04-28 15:50:13 +0200

    updated ROCm interface / build system

--- src/density/augmentation_operator.hpp
+++ src/density/augmentation_operator.hpp
@@ -191,6 +191,11 @@ class Augmentation_operator_gvec_deriv
         return q_pw_;
     }
 
+    mdarray<double, 2> & q_pw()
+    {
+        return q_pw_;
+    }
+
     double q_pw(int i__, int ig__) const
     {
         return q_pw_(i__, ig__);
