b0b09257f7640aea1a9a1df6488ac2e0f13aa72c
toxa81 <toxa81@gmail.com>
2020-05-08 11:41:46 +0200

    fix reframe config

--- reframe/config.py
+++ reframe/config.py
@@ -1,113 +1,101 @@
-
-class ReframeSettings:
-    job_poll_intervals = [1, 2, 3]
-    job_submit_timeout = 60
-    checks_path = ['checks/']
-    checks_path_recurse = False
-    site_configuration = {
-        'systems': {
-            'osx': {
-                'descr': 'OSX notebook with MacPort',
-                'hostnames': ['localhost'],
-                'modules_system': None,
-                'resourcesdir': '',
-                'partitions': {
-                    'cpu': {
-                        'scheduler': 'local+mpirun',
-                        'environs': ['PrgEnv-gnu'],
-                        'descr': 'CPU execution',
-                        'max_jobs': 1
-                    }
+site_configuration = {
+    'systems': [
+        {
+            'name' : 'osx',
+            'descr': 'OSX notebook with MacPort',
+            'hostnames': ['localhost'],
+            'resourcesdir': '',
+            'partitions': [
+                {
+                    'name' : 'cpu',
+                    'scheduler': 'local',
+                    'launcher' : 'mpirun',
+                    'environs': ['builtin'],
+                    'descr': 'CPU execution',
+                    'max_jobs': 1
                 }
-            },
-            'linux': {
-                'descr': 'Ubuntu linux box',
-                'hostnames': ['localhost'],
-                'modules_system': None,
-                'resourcesdir': '',
-                'partitions': {
-                    'cpu': {
-                        'scheduler': 'local+local',
-                        'environs': ['PrgEnv-gnu'],
-                        'descr': 'CPU execution',
-                        'max_jobs': 1
-                    }
-                }
-            }
+            ]
         },
-
-        'environments': {
-            'osx': {
-                'PrgEnv-gnu': {
-                    'type': 'ProgEnvironment',
-                    'modules': [],
-                    'cc':  'mpicc',
-                    'cxx': 'mpic++',
-                    'ftn': 'mpif90',
+        {
+            'name' : 'linux',
+            'descr': 'Ubuntu linux box',
+            'hostnames': ['localhost'],
+            'resourcesdir': '',
+            'partitions': [
+                {
+                    'name' : 'cpu',
+                    'scheduler': 'local',
+                    'launcher' : 'local',
+                    'environs': ['builtin'],
+                    'descr': 'CPU execution',
+                    'max_jobs': 1
                 }
-            },
-            'linux': {
-                'PrgEnv-gnu': {
-                    'type': 'ProgEnvironment',
-                    'modules': [],
-                    'cc':  'mpicc',
-                    'cxx': 'mpic++',
-                    'ftn': 'mpif90',
-                }
-            }
+            ]
         }
-    }
-
-    logging_config = {
-        'level': 'DEBUG',
-        'handlers': [
-            {
-                'type': 'file',
-                'name': 'reframe.log',
-                'level': 'DEBUG',
-                'format': '[%(asctime)s] %(levelname)s: '
-                          '%(check_info)s: %(message)s',
-                'append': False,
-            },
-
-            # Output handling
-            {
-                'type': 'stream',
-                'name': 'stdout',
-                'level': 'INFO',
-                'format': '%(message)s'
-            },
-            {
-                'type': 'file',
-                'name': 'reframe.out',
-                'level': 'INFO',
-                'format': '%(message)s',
-                'append': False,
-            }
-        ]
-    }
-
-    perf_logging_config = {
-        'level': 'DEBUG',
-        'handlers': [
-            {
-                'type': 'filelog',
-                'prefix': '%(check_system)s/%(check_partition)s',
-                'level': 'INFO',
-                'format': (
-                    '%(check_job_completion_time)s|reframe %(version)s|'
-                    '%(check_info)s|jobid=%(check_jobid)s|'
-                    'num_tasks=%(check_num_tasks)s|'
-                    '%(check_perf_var)s=%(check_perf_value)s|'
-                    'ref=%(check_perf_ref)s '
-                    '(l=%(check_perf_lower_thres)s, '
-                    'u=%(check_perf_upper_thres)s)|'
-                    '%(check_perf_unit)s'
-                ),
-                'append': True
-            }
-        ]
-    }
-
-settings = ReframeSettings()
+    ],
+    'environments': [
+        {
+            'name': 'builtin',
+            'target_systems': ['osx:cpu', 'linux:cpu'],
+            'cc': 'mpicc',
+            'cxx': 'mpic++',
+            'ftn': 'mpif90'
+        }
+    ],
+    'logging': [
+        {
+            'level': 'debug',
+            'handlers': [
+                {
+                    'type': 'file',
+                    'name': 'reframe.log',
+                    'level': 'debug',
+                    'format': '[%(asctime)s] %(levelname)s: %(check_info)s: %(message)s',   # noqa: E501
+                    'append': False
+                },
+                {
+                    'type': 'stream',
+                    'name': 'stdout',
+                    'level': 'info',
+                    'format': '%(message)s'
+                },
+                {
+                    'type': 'file',
+                    'name': 'reframe.out',
+                    'level': 'info',
+                    'format': '%(message)s',
+                    'append': False
+                }
+            ],
+            'handlers_perflog': [
+                {
+                    'type': 'filelog',
+                    'prefix': '%(check_system)s/%(check_partition)s',
+                    'level': 'info',
+                    'format': '%(check_job_completion_time)s|reframe %(version)s|%(check_info)s|jobid=%(check_jobid)s|num_tasks=%(check_num_tasks)s|%(check_perf_var)s=%(check_perf_value)s|ref=%(check_perf_ref)s (l=%(check_perf_lower_thres)s, u=%(check_perf_upper_thres)s)|%(check_perf_unit)s',   # noqa: E501
+                    'datefmt': '%FT%T%:z',
+                    'append': True
+                },
+                {
+                    'type': 'graylog',
+                    'address': 'graylog-server:12345',
+                    'level': 'info',
+                    'format': '%(message)s',
+                    'extras': {
+                        'facility': 'reframe',
+                        'data-version': '1.0',
+                    }
+                }
+            ]
+        }
+    ],
+    'general': [
+        {
+            'check_search_path': [
+                'checks/'
+            ],
+            'check_search_recursive': True
+        }
+    ]
+}
 
