0898f26c4129bd1941a5de34b95f48ef84327370
Simon Pintarelli <simon.pintarelli@cscs.ch>
2020-06-14 14:44:09 +0200

    [python] don't dump density / potential to disk

--- python_module/apps/nlcg.py
+++ python_module/apps/nlcg.py
@@ -21,8 +21,8 @@ if __name__ == '__main__':
         E = kwargs['E']
         def _callback(fn, it, **kwargs):
             if it % interval == 0:
-                kset.ctx().create_storage_file()
-                store_density_potential(E.density, E.potential)
+                # kset.ctx().create_storage_file()
+                # store_density_potential(E.density, E.potential)
                 mag_mom = E.density.compute_atomic_mag_mom()
                 save_state({'f': fn, 'ek': kset.e, 'mag_mom': mag_mom}, kset=kset, prefix='fn_%05d_' % it)
         return _callback
