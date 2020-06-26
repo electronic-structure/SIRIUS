39022dc3ab0b77cbed4cd76a98fb8d3c7bba820e
Simon Pintarelli <simon.pintarelli@cscs.ch>
2020-06-26 13:09:52 +0200

    [nlcg] comment timer

--- apps/nlcg/sirius.nlcg.cpp
+++ apps/nlcg/sirius.nlcg.cpp
@@ -296,12 +296,12 @@ int main(int argn, char** argv)
 
     sirius::finalize(1);
 
-    if (my_rank == 0) {
-        const auto timing_result = ::utils::global_rtgraph_timer.process();
-        std::cout << timing_result.print();
-        std::ofstream ofs("timers.json", std::ofstream::out | std::ofstream::trunc);
-        ofs << timing_result.json();
-    }
+    // if (my_rank == 0) {
+    //     const auto timing_result = ::utils::global_rtgraph_timer.process();
+    //     std::cout << timing_result.print();
+    //     std::ofstream ofs("timers.json", std::ofstream::out | std::ofstream::trunc);
+    //     ofs << timing_result.json();
+    // }
 
     return 0;
 }
