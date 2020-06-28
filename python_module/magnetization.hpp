e8e41fb6771bd5c1451f810ec2df206674f7b4d1
Simon Pintarelli <simon.pintarelli@cscs.ch>
2020-06-28 17:02:29 +0200

    add standalone function to print magnetization per atom

--- python_module/magnetization.hpp
+++ python_module/magnetization.hpp
@@ -1,6 +1,7 @@
 #include <list>
 #include <vector>
 #include <tuple>
+#include <sstream>
 #include "density/density.hpp"
 #include "simulation_context.hpp"
 
@@ -15,6 +16,46 @@ std::vector<double> magnetization(Density& density)
         lm[i] = std::get<0>(result)[i];
 
     return lm;
+}
+
+std::string sprint_magnetization(K_point_set& kset, const Density& density)
+{
+    auto& ctx       = kset.ctx();
+    auto& unit_cell = kset.unit_cell();
+
+    auto result_mag = density.get_magnetisation();
+    // auto total_mag  = std::get<0>(result_mag);
+    // auto it_mag     = std::get<1>(result_mag);
+    auto mt_mag = std::get<2>(result_mag);
+    std::stringstream sstream;
+
+    char buffer[20000];
+
+    if (ctx.num_mag_dims()) {
+        std::sprintf(buffer, "atom              moment                |moment|");
+        sstream << buffer;
+        std::sprintf(buffer ,"\n");
+        sstream << buffer;
+        for (int i = 0; i < 80; i++) {
+            std::sprintf(buffer, "-");
+            sstream << buffer;
+        }
+        std::sprintf(buffer, "\n");
+        sstream << buffer;
+
+        for (int ia = 0; ia < unit_cell.num_atoms(); ia++) {
+            vector3d<double> v(mt_mag[ia]);
+            std::sprintf(buffer, "%4i  [%8.4f, %8.4f, %8.4f]  %10.6f", ia, v[0], v[1], v[2], v.length());
+            sstream << buffer;
+            std::sprintf(buffer, "\n");
+            sstream << buffer;
+        }
+
+        std::sprintf(buffer, "\n");
+        sstream << buffer;
+    }
+
+    return sstream.str();
 }
 
 } // namespace sirius
