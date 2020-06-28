db8ff9fc1ace2b3b6a9ed7619c34cea19eaba470
Simon Pintarelli <simon.pintarelli@cscs.ch>
2020-06-28 16:51:40 +0200

    [pybind] add members of Density, Potential, etc.

--- python_module/py_sirius.cpp
+++ python_module/py_sirius.cpp
@@ -291,7 +291,9 @@ PYBIND11_MODULE(py_sirius, m)
              })
         .def("index_by_gvec", &Gvec::index_by_gvec);
 
-    py::class_<Gvec_partition>(m, "Gvec_partition");
+    py::class_<Gvec_partition>(m, "Gvec_partition")
+        .def_property_readonly("gvec", &Gvec_partition::gvec)
+        .def_property_readonly("gvec_array", &Gvec_partition::get_gvec);
 
     py::class_<vector3d<int>>(m, "vector3d_int")
         .def(py::init<std::vector<int>>())
@@ -346,31 +348,49 @@ PYBIND11_MODULE(py_sirius, m)
         .def(py::init<matrix3d<double>>())
         .def("det", &matrix3d<double>::det);
 
-    py::class_<Potential>(m, "Potential")
-        .def(py::init<Simulation_context&>(), py::keep_alive<1, 2>(), "ctx"_a)
-        .def("generate", &Potential::generate)
-        .def("symmetrize", &Potential::symmetrize)
-        .def("fft_transform", &Potential::fft_transform)
-        .def("save", &Potential::save)
-        .def("load", &Potential::load)
-        .def("energy_vha", &Potential::energy_vha)
-        .def("energy_vxc", &Potential::energy_vxc)
-        .def("energy_exc", &Potential::energy_exc)
-        .def("PAW_total_energy", &Potential::PAW_total_energy)
-        .def("PAW_one_elec_energy", &Potential::PAW_one_elec_energy);
-
     py::class_<Field4D>(m, "Field4D")
-        .def("f_pw_local",
+        .def(
+            "f_pw_local",
             [](py::object& obj, int i) -> py::array_t<complex_double> {
-                Density& density     = obj.cast<Density&>();
-                auto& matrix_storage = density.component_raise(i).f_pw_local();
+                Field4D& field     = obj.cast<Field4D&>();
+                auto& matrix_storage = field.component_raise(i).f_pw_local();
                 int nrows            = matrix_storage.size(0);
                 /* return underlying data as numpy.ndarray view */
                 return py::array_t<complex_double>({nrows}, {1 * sizeof(complex_double)},
                                                    matrix_storage.at(memory_t::host), obj);
             },
             py::keep_alive<0, 1>())
+        .def("f_rg",
+             [](py::object& obj, int i) -> py::array_t<double> {
+                 Field4D& field       = obj.cast<Field4D&>();
+                 auto& matrix_storage = field.component_raise(i).f_rg();
+                 int nrows            = matrix_storage.size(0);
+                 /* return underlying data as numpy.ndarray view */
+                 return py::array_t<double>({nrows}, {1 * sizeof(double)},
+                                            matrix_storage.at(memory_t::host), obj);
+             })
         .def("component", py::overload_cast<int>(&Field4D::component), py::return_value_policy::reference_internal);
+
+    py::class_<Potential, Field4D>(m, "Potential")
+        .def(py::init<Simulation_context&>(), py::keep_alive<1, 2>(), "ctx"_a)
+        .def("generate", &Potential::generate)
+        .def("symmetrize", &Potential::symmetrize)
+        .def("fft_transform", &Potential::fft_transform)
+        .def("save", &Potential::save)
+        .def("load", &Potential::load)
+        .def_property("vxc", py::overload_cast<>(&Potential::xc_potential),
+                      py::overload_cast<>(&Potential::xc_potential), py::return_value_policy::reference_internal)
+        .def_property("exc", py::overload_cast<>(&Potential::xc_energy_density),
+                      py::overload_cast<>(&Potential::xc_energy_density), py::return_value_policy::reference_internal)
+        .def_property("vha", py::overload_cast<>(&Potential::hartree_potential),
+                      py::overload_cast<>(&Potential::hartree_potential), py::return_value_policy::reference_internal)
+        .def_property("vloc", py::overload_cast<>(&Potential::local_potential),
+                      py::overload_cast<>(&Potential::local_potential), py::return_value_policy::reference_internal)
+        .def("energy_vha", &Potential::energy_vha)
+        .def("energy_vxc", &Potential::energy_vxc)
+        .def("energy_exc", &Potential::energy_exc)
+        .def("PAW_total_energy", &Potential::PAW_total_energy)
+        .def("PAW_one_elec_energy", &Potential::PAW_one_elec_energy);
 
     py::class_<Density, Field4D>(m, "Density")
         .def(py::init<Simulation_context&>(), py::keep_alive<1, 2>(), "ctx"_a)
@@ -381,22 +401,33 @@ PYBIND11_MODULE(py_sirius, m)
         .def("mix", &Density::mix)
         .def("symmetrize", &Density::symmetrize)
         .def("symmetrize_density_matrix", &Density::symmetrize_density_matrix)
-        .def("generate", py::overload_cast<K_point_set const&, bool, bool>(&Density::generate), "kpointset"_a, "add_core"_a = true, "transform_to_rg"_a = false)
+        .def("generate", py::overload_cast<K_point_set const&, bool, bool>(&Density::generate), "kpointset"_a,
+             "add_core"_a = true, "transform_to_rg"_a = false)
         .def("generate_paw_loc_density", &Density::generate_paw_loc_density)
         .def("compute_atomic_mag_mom", &Density::compute_atomic_mag_mom)
         .def("save", &Density::save)
         .def("check_num_electrons", &Density::check_num_electrons)
         .def("get_magnetisation", &Density::get_magnetisation)
-        .def("density_matrix",
-             [](py::object& obj) -> py::array_t<complex_double> {
-                 Density& density = obj.cast<Density&>();
-                 auto& dm         = density.density_matrix();
-                 if (dm.at(memory_t::host) == nullptr) {
-                     throw std::runtime_error("trying to access null pointer");
-                 }
-                 return py::array_t<complex_double, py::array::f_style>(
-                     {dm.size(0), dm.size(1), dm.size(2), dm.size(3)}, dm.at(memory_t::host), obj);
-             })
+        .def_property(
+            "density_matrix",
+            [](py::object& obj) -> py::array_t<complex_double> {
+                Density& density = obj.cast<Density&>();
+                auto& dm         = density.density_matrix();
+                if (dm.at(memory_t::host) == nullptr) {
+                    throw std::runtime_error("trying to access null pointer");
+                }
+                return py::array_t<complex_double, py::array::f_style>({dm.size(0), dm.size(1), dm.size(2), dm.size(3)},
+                                                                       dm.at(memory_t::host), obj);
+            },
+            [](py::object& obj) -> py::array_t<complex_double> {
+                Density& density = obj.cast<Density&>();
+                auto& dm         = density.density_matrix();
+                if (dm.at(memory_t::host) == nullptr) {
+                    throw std::runtime_error("trying to access null pointer");
+                }
+                return py::array_t<complex_double, py::array::f_style>({dm.size(0), dm.size(1), dm.size(2), dm.size(3)},
+                                                                       dm.at(memory_t::host), obj);
+            }, py::return_value_policy::reference_internal)
         .def("load", &Density::load);
 
     py::class_<Band>(m, "Band")
@@ -695,12 +726,28 @@ PYBIND11_MODULE(py_sirius, m)
              py::return_value_policy::reference_internal);
 
     py::class_<Smooth_periodic_function<complex_double>>(m, "CSmooth_periodic_function")
-        .def("pw", py::overload_cast<>(&Smooth_periodic_function<complex_double>::f_pw_local), py::return_value_policy::reference_internal)
-        .def("rg", py::overload_cast<>(&Smooth_periodic_function<complex_double>::f_rg), py::return_value_policy::reference_internal);
+        .def("fft", [](Smooth_periodic_function<complex_double>& obj) { return obj.fft_transform(-1); })
+        .def("ifft", [](Smooth_periodic_function<complex_double>& obj) { return obj.fft_transform(1); })
+        .def_property("pw", py::overload_cast<>(&Smooth_periodic_function<complex_double>::f_pw_local),
+                      py::overload_cast<>(&Smooth_periodic_function<complex_double>::f_pw_local),
+                      py::return_value_policy::reference_internal)
+        .def_property("rg", py::overload_cast<>(&Smooth_periodic_function<complex_double>::f_rg),
+                      py::overload_cast<>(&Smooth_periodic_function<complex_double>::f_rg),
+                      py::return_value_policy::reference_internal)
+        .def_property_readonly("gvec_partition", &Smooth_periodic_function<complex_double>::gvec_partition,
+                               py::return_value_policy::reference_internal);
 
-    py::class_<Smooth_periodic_function<double>>(m, "RSmooth_periodic_function")
-        .def("pw", py::overload_cast<>(&Smooth_periodic_function<double>::f_pw_local), py::return_value_policy::reference_internal)
-        .def("rg", py::overload_cast<>(&Smooth_periodic_function<double>::f_rg), py::return_value_policy::reference_internal);
+    py::class_<Smooth_periodic_function<double>>(m, "Smooth_periodic_function")
+        .def("fft", [](Smooth_periodic_function<double>& obj) { return obj.fft_transform(-1); })
+        .def("ifft", [](Smooth_periodic_function<double>& obj) { return obj.fft_transform(1); })
+        .def_property("pw", py::overload_cast<>(&Smooth_periodic_function<double>::f_pw_local),
+                      py::overload_cast<>(&Smooth_periodic_function<double>::f_pw_local),
+                      py::return_value_policy::reference_internal)
+        .def_property("rg", py::overload_cast<>(&Smooth_periodic_function<double>::f_rg),
+                      py::overload_cast<>(&Smooth_periodic_function<double>::f_rg),
+                      py::return_value_policy::reference_internal)
+        .def_property_readonly("gvec_partition", &Smooth_periodic_function<double>::gvec_partition,
+                               py::return_value_policy::reference_internal);
 
     py::class_<Periodic_function<double>, Smooth_periodic_function<double>>(m, "RPeriodic_function");
 
@@ -713,6 +760,7 @@ PYBIND11_MODULE(py_sirius, m)
     m.def("make_sirius_comm", &make_sirius_comm);
     m.def("make_pycomm", &make_pycomm);
     m.def("magnetization", &magnetization);
+    m.def("sprint_magnetization", &sprint_magnetization);
     m.def("apply_hamiltonian", &apply_hamiltonian, "Hamiltonian0"_a, "kpoint"_a, "wf_out"_a,
           "wf_in"_a, py::arg("swf_out") = nullptr);
     m.def("initialize_subspace", &initialize_subspace);
