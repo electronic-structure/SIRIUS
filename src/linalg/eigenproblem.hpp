6d74050ff7c756faab7a3098417a19a20877389f
Harmen Stoppels <harmenstoppels@gmail.com>
2020-08-19 09:44:33 +0200

    Do not use gpu in elpa in cpu mode

--- src/linalg/eigenproblem.hpp
+++ src/linalg/eigenproblem.hpp
@@ -583,7 +583,10 @@ class Eigensolver_elpa : public Eigensolver
         //if (error != ELPA_OK) {
         //    TERMINATE("can't set elpa threads");
         //}
-        elpa_set_integer(handle, "gpu", 1, &error);
+
+        if (acc::num_devices() != 0) {
+            elpa_set_integer(handle, "gpu", 1, &error);
+        }
 
         if (stage_ == 1) {
             elpa_set_integer(handle, "solver", ELPA_SOLVER_1STAGE, &error);
@@ -642,7 +645,10 @@ class Eigensolver_elpa : public Eigensolver
         //if (error != ELPA_OK) {
         //    TERMINATE("can't set elpa threads");
         //}
-        elpa_set_integer(handle, "gpu", 1, &error);
+
+        if (acc::num_devices() != 0) {
+            elpa_set_integer(handle, "gpu", 1, &error);
+        }
 
         if (stage_ == 1) {
             elpa_set_integer(handle, "solver", ELPA_SOLVER_1STAGE, &error);
