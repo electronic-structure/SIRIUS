bc0bdc71f62836ea0655425dae4584fe33d7890f
Harmen Stoppels <harmenstoppels@gmail.com>
2020-08-27 11:56:29 +0200

    Use CTest in Github action now

--- ci/github_build_sirius.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-export SPEC="sirius@develop %gcc@7.5.0 build_type=RelWithDebInfo ^openblas threads=openmp ^mpich"
-mkdir build
-cd build
-spack --color always -e ci build-env $SPEC -- cmake -DCMAKE_INSTALL_PREFIX=$HOME/local -DBUILD_TESTS=1 ..
-spack --color always -e ci build-env $SPEC -- make -j 2 install
