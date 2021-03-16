6867431af360168007ce7e76dbf164e95cf06357
toxa81 <toxa81@gmail.com>
2021-03-16 11:26:08 +0100

    fix

--- src/unit_cell/atom.hpp
+++ src/unit_cell/atom.hpp
@@ -376,7 +376,7 @@ class Atom
     /// Set symmetry class of the atom.
     inline void set_symmetry_class(std::shared_ptr<Atom_symmetry_class> symmetry_class__)
     {
-        symmetry_class_ = symmetry_class__;
+        symmetry_class_ = std::move(symmetry_class__);
     }
 
     /// Set muffin-tin potential and magnetic field.
