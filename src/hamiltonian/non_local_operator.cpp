ce54debc026b94590cf014f062bfa139765af7a6
Man Hin CHENG <chengman@ethz.ch>
2021-06-29 23:29:05 +0200

    template non_local_operator class

--- src/hamiltonian/non_local_operator.cpp
+++ src/hamiltonian/non_local_operator.cpp
@@ -22,13 +22,12 @@
  *  \brief Contains implementation of sirius::Non_local_operator class.
  */
 
-#include "SDDK/omp.hpp"
 #include "non_local_operator.hpp"
-#include "beta_projectors/beta_projectors.hpp"
 
 namespace sirius {
 
-Non_local_operator::Non_local_operator(Simulation_context const& ctx__)
+template <typename T>
+Non_local_operator<T>::Non_local_operator(Simulation_context const& ctx__)
     : ctx_(ctx__)
 {
     PROFILE("sirius::Non_local_operator");
@@ -54,265 +53,21 @@ Non_local_operator::Non_local_operator(Simulation_context const& ctx__)
     }
 }
 
-template <>
-double Non_local_operator::value<double>(int xi1__, int xi2__, int ispn__, int ia__)
-{
-    int nbf = this->ctx_.unit_cell().atom(ia__).mt_basis_size();
-    return this->op_(0, packed_mtrx_offset_(ia__) + xi2__ * nbf + xi1__, ispn__);
-}
-
-template <>
-double_complex Non_local_operator::value<double_complex>(int xi1__, int xi2__, int ispn__, int ia__)
-{
-    int nbf = this->ctx_.unit_cell().atom(ia__).mt_basis_size();
-    return double_complex(this->op_(0, packed_mtrx_offset_(ia__) + xi2__ * nbf + xi1__, ispn__),
-                          this->op_(1, packed_mtrx_offset_(ia__) + xi2__ * nbf + xi1__, ispn__));
-}
-
-template <>
-void Non_local_operator::apply<double_complex>(int chunk__, int ispn_block__, Wave_functions<double>& op_phi__, int idx0__,
-                                               int n__, Beta_projectors_base& beta__,
-                                               matrix<double_complex>& beta_phi__)
-{
-    PROFILE("sirius::Non_local_operator::apply");
-
-    if (is_null_) {
-        return;
-    }
-
-    auto& beta_gk     = beta__.pw_coeffs_a();
-    int num_gkvec_loc = beta__.num_gkvec_loc();
-    int nbeta         = beta__.chunk(chunk__).num_beta_;
-
-    /* setup linear algebra parameters */
-    memory_t mem{memory_t::none};
-    linalg_t la{linalg_t::none};
-    switch (pu_) {
-        case device_t::CPU: {
-            mem = memory_t::host;
-            la  = linalg_t::blas;
-            break;
-        }
-        case device_t::GPU: {
-            mem = memory_t::device;
-            la  = linalg_t::gpublas;
-            break;
-        }
-    }
-
-    auto work = mdarray<double_complex, 1>(nbeta * n__, ctx_.mem_pool(mem));
-
-    /* compute O * <beta|phi> for atoms in a chunk */
-    #pragma omp parallel
-    {
-        acc::set_device_id(sddk::get_device_id(acc::num_devices())); // avoid cuda mth bugs
-
-        #pragma omp for
-        for (int i = 0; i < beta__.chunk(chunk__).num_atoms_; i++) {
-            /* number of beta functions for a given atom */
-            int nbf  = beta__.chunk(chunk__).desc_(static_cast<int>(beta_desc_idx::nbf), i);
-            int offs = beta__.chunk(chunk__).desc_(static_cast<int>(beta_desc_idx::offset), i);
-            int ia   = beta__.chunk(chunk__).desc_(static_cast<int>(beta_desc_idx::ia), i);
-
-            if (nbf) {
-                linalg(la).gemm(
-                    'N', 'N', nbf, n__, nbf, &linalg_const<double_complex>::one(),
-                    reinterpret_cast<double_complex*>(op_.at(mem, 0, packed_mtrx_offset_(ia), ispn_block__)), nbf,
-                    beta_phi__.at(mem, offs, 0), beta_phi__.ld(), &linalg_const<double_complex>::zero(),
-                    work.at(mem, offs), nbeta, stream_id(omp_get_thread_num()));
-            }
-        }
-    }
-    switch (pu_) {
-        case device_t::GPU: {
-            /* wait for previous zgemms */
-            #pragma omp parallel
-            acc::sync_stream(stream_id(omp_get_thread_num()));
-            break;
-        }
-        case device_t::CPU: {
-            break;
-        }
-    }
-
-    int jspn = ispn_block__ & 1;
-
-    /* compute <G+k|beta> * O * <beta|phi> and add to op_phi */
-    linalg(ctx_.blas_linalg_t())
-        .gemm('N', 'N', num_gkvec_loc, n__, nbeta, &linalg_const<double_complex>::one(), beta_gk.at(mem), num_gkvec_loc,
-              work.at(mem), nbeta, &linalg_const<double_complex>::one(),
-              op_phi__.pw_coeffs(jspn).prime().at(op_phi__.preferred_memory_t(), 0, idx0__),
-              op_phi__.pw_coeffs(jspn).prime().ld());
-
-    switch (pu_) {
-        case device_t::GPU: {
-            acc::sync_stream(stream_id(-1));
-            break;
-        }
-        case device_t::CPU: {
-            break;
-        }
-    }
-}
-
-template <>
-void Non_local_operator::apply<double_complex>(int chunk__, int ia__, int ispn_block__, Wave_functions<double>& op_phi__,
-                                               int idx0__, int n__, Beta_projectors_base& beta__,
-                                               matrix<double_complex>& beta_phi__)
-{
-    if (is_null_) {
-        return;
-    }
-
-    auto& beta_gk     = beta__.pw_coeffs_a();
-    int num_gkvec_loc = beta__.num_gkvec_loc();
-
-    int nbf  = beta__.chunk(chunk__).desc_(static_cast<int>(beta_desc_idx::nbf), ia__);
-    int offs = beta__.chunk(chunk__).desc_(static_cast<int>(beta_desc_idx::offset), ia__);
-    int ia   = beta__.chunk(chunk__).desc_(static_cast<int>(beta_desc_idx::ia), ia__);
-
-    if (nbf == 0) {
-        return;
-    }
-
-    /* setup linear algebra parameters */
-    memory_t mem{memory_t::none};
-    linalg_t la{linalg_t::none};
-
-    switch (pu_) {
-        case device_t::CPU: {
-            mem = memory_t::host;
-            la  = linalg_t::blas;
-            break;
-        }
-        case device_t::GPU: {
-            mem = memory_t::device;
-            la  = linalg_t::gpublas;
-            break;
-        }
-    }
-
-    auto work = mdarray<double_complex, 1>(nbf * n__, ctx_.mem_pool(mem));
-
-    linalg(la).gemm('N', 'N', nbf, n__, nbf, &linalg_const<double_complex>::one(),
-                     reinterpret_cast<double_complex*>(op_.at(mem, 0, packed_mtrx_offset_(ia), ispn_block__)), nbf,
-                     beta_phi__.at(mem, offs, 0), beta_phi__.ld(), &linalg_const<double_complex>::zero(), work.at(mem),
-                     nbf);
-
-    int jspn = ispn_block__ & 1;
-
-    linalg(ctx_.blas_linalg_t())
-        .gemm('N', 'N', num_gkvec_loc, n__, nbf, &linalg_const<double_complex>::one(), beta_gk.at(mem, 0, offs),
-              num_gkvec_loc, work.at(mem), nbf, &linalg_const<double_complex>::one(),
-              op_phi__.pw_coeffs(jspn).prime().at(op_phi__.preferred_memory_t(), 0, idx0__),
-              op_phi__.pw_coeffs(jspn).prime().ld());
-    switch (pu_) {
-        case device_t::CPU: {
-            break;
-        }
-        case device_t::GPU: {
-#ifdef SIRIUS_GPU
-            acc::sync_stream(stream_id(-1));
-#endif
-            break;
-        }
-    }
-}
-
-template <>
-void Non_local_operator::apply<double>(int chunk__, int ispn_block__, Wave_functions<double>& op_phi__, int idx0__, int n__,
-                                       Beta_projectors_base& beta__, matrix<double>& beta_phi__)
-{
-    PROFILE("sirius::Non_local_operator::apply");
-
-    if (is_null_) {
-        return;
-    }
-
-    auto& beta_gk     = beta__.pw_coeffs_a();
-    int num_gkvec_loc = beta__.num_gkvec_loc();
-    int nbeta         = beta__.chunk(chunk__).num_beta_;
-
-    /* setup linear algebra parameters */
-    memory_t mem{memory_t::none};
-    linalg_t la{linalg_t::none};
-    switch (pu_) {
-        case device_t::CPU: {
-            mem = memory_t::host;
-            la  = linalg_t::blas;
-            break;
-        }
-        case device_t::GPU: {
-            mem = memory_t::device;
-            la  = linalg_t::gpublas;
-            break;
-        }
-    }
-
-    auto work = mdarray<double, 1>(nbeta * n__, ctx_.mem_pool(mem));
-
-    /* compute O * <beta|phi> for atoms in a chunk */
-    #pragma omp parallel for
-    for (int i = 0; i < beta__.chunk(chunk__).num_atoms_; i++) {
-        /* number of beta functions for a given atom */
-        int nbf  = beta__.chunk(chunk__).desc_(static_cast<int>(beta_desc_idx::nbf), i);
-        int offs = beta__.chunk(chunk__).desc_(static_cast<int>(beta_desc_idx::offset), i);
-        int ia   = beta__.chunk(chunk__).desc_(static_cast<int>(beta_desc_idx::ia), i);
-
-        if (nbf == 0) {
-            continue;
-        }
-        linalg(la).gemm('N', 'N', nbf, n__, nbf, &linalg_const<double>::one(),
-                         op_.at(mem, 0, packed_mtrx_offset_(ia), ispn_block__), nbf, beta_phi__.at(mem, offs, 0),
-                         beta_phi__.ld(), &linalg_const<double>::zero(), work.at(mem, offs), nbeta,
-                         stream_id(omp_get_thread_num()));
-    }
-    switch (pu_) {
-        case device_t::GPU: {
-            /* wait for previous zgemms */
-            #pragma omp parallel
-            acc::sync_stream(stream_id(omp_get_thread_num()));
-            break;
-        }
-        case device_t::CPU: {
-            break;
-        }
-    }
-
-    int jspn = ispn_block__ & 1;
-
-    /* compute <G+k|beta> * O * <beta|phi> and add to op_phi */
-    linalg(ctx_.blas_linalg_t())
-        .gemm('N', 'N', 2 * num_gkvec_loc, n__, nbeta, &linalg_const<double>::one(),
-              reinterpret_cast<double*>(beta_gk.at(mem)), 2 * num_gkvec_loc, work.at(mem), nbeta,
-              &linalg_const<double>::one(),
-              reinterpret_cast<double*>(op_phi__.pw_coeffs(jspn).prime().at(op_phi__.preferred_memory_t(), 0, idx0__)),
-              2 * op_phi__.pw_coeffs(jspn).prime().ld());
-
-    switch (pu_) {
-        case device_t::GPU: {
-            acc::sync_stream(stream_id(-1));
-            break;
-        }
-        case device_t::CPU: {
-            break;
-        }
-    }
-}
-
-D_operator::D_operator(Simulation_context const& ctx_)
-    : Non_local_operator(ctx_)
+template <typename T>
+D_operator<T>::D_operator(Simulation_context const& ctx_)
+    : Non_local_operator<T>(ctx_)
 {
     if (ctx_.gamma_point()) {
-        this->op_ = mdarray<double, 3>(1, this->packed_mtrx_size_, ctx_.num_mag_dims() + 1);
+        this->op_ = mdarray<T, 3>(1, this->packed_mtrx_size_, ctx_.num_mag_dims() + 1);
     } else {
-        this->op_ = mdarray<double, 3>(2, this->packed_mtrx_size_, ctx_.num_mag_dims() + 1);
+        this->op_ = mdarray<T, 3>(2, this->packed_mtrx_size_, ctx_.num_mag_dims() + 1);
     }
     this->op_.zero();
     initialize();
 }
 
-void D_operator::initialize()
+template <typename T>
+void D_operator<T>::initialize()
 {
     PROFILE("sirius::D_operator::initialize");
 
@@ -326,7 +81,7 @@ void D_operator::initialize()
 
         /* in case of spin orbit coupling */
         if (uc.atom(ia).type().spin_orbit_coupling()) {
-            mdarray<double_complex, 3> d_mtrx_so(nbf, nbf, 4);
+            mdarray<std::complex<T>, 3> d_mtrx_so(nbf, nbf, 4);
             d_mtrx_so.zero();
 
             /* transform the d_mtrx */
@@ -338,7 +93,7 @@ void D_operator::initialize()
                     /* note that the `I` integrals are already calculated and stored in atom.d_mtrx */
                     for (int sigma = 0; sigma < 2; sigma++) {
                         for (int sigmap = 0; sigmap < 2; sigmap++) {
-                            double_complex result(0, 0);
+                            std::complex<T> result(0, 0);
                             for (auto xi2p = 0; xi2p < nbf; xi2p++) {
                                 if (atom.type().compare_index_beta_functions(xi2, xi2p)) {
                                     /* just sum over m2, all other indices are the same */
@@ -423,8 +178,8 @@ void D_operator::initialize()
                     int idx = xi2 * nbf + xi1;
                     switch (this->ctx_.num_mag_dims()) {
                         case 3: {
-                            double bx = uc.atom(ia).d_mtrx(xi1, xi2, 2);
-                            double by = uc.atom(ia).d_mtrx(xi1, xi2, 3);
+                            T bx = uc.atom(ia).d_mtrx(xi1, xi2, 2);
+                            T by = uc.atom(ia).d_mtrx(xi1, xi2, 3);
 
                             this->op_(0, this->packed_mtrx_offset_(ia) + idx, 2) = bx;
                             this->op_(1, this->packed_mtrx_offset_(ia) + idx, 2) = -by;
@@ -433,8 +188,8 @@ void D_operator::initialize()
                             this->op_(1, this->packed_mtrx_offset_(ia) + idx, 3) = by;
                         }
                         case 1: {
-                            double v  = uc.atom(ia).d_mtrx(xi1, xi2, 0);
-                            double bz = uc.atom(ia).d_mtrx(xi1, xi2, 1);
+                            T v  = uc.atom(ia).d_mtrx(xi1, xi2, 0);
+                            T bz = uc.atom(ia).d_mtrx(xi1, xi2, 1);
 
                             /* add ionic part */
                             if (lm1 == lm2) {
@@ -473,26 +228,28 @@ void D_operator::initialize()
 
     /* D-operator is not diagonal in spin in case of non-collinear magnetism
        (spin-orbit coupling falls into this case) */
-    if (ctx_.num_mag_dims() == 3) {
+    if (this->ctx_.num_mag_dims() == 3) {
         this->is_diag_ = false;
     }
 }
 
-Q_operator::Q_operator(Simulation_context const& ctx__)
-    : Non_local_operator(ctx__)
+template <typename T>
+Q_operator<T>::Q_operator(Simulation_context const& ctx__)
+    : Non_local_operator<T>(ctx__)
 {
     /* Q-operator is independent of spin if there is no spin-orbit; however, it simplifies the apply()
      * method if the Q-operator has a spin index */
-    if (ctx_.gamma_point()) {
-        this->op_ = mdarray<double, 3>(1, this->packed_mtrx_size_, ctx_.num_mag_dims() + 1);
+    if (this->ctx_.gamma_point()) {
+        this->op_ = mdarray<T, 3>(1, this->packed_mtrx_size_, this->ctx_.num_mag_dims() + 1);
     } else {
-        this->op_ = mdarray<double, 3>(2, this->packed_mtrx_size_, ctx_.num_mag_dims() + 1);
+        this->op_ = mdarray<T, 3>(2, this->packed_mtrx_size_, this->ctx_.num_mag_dims() + 1);
     }
     this->op_.zero();
     initialize();
 }
 
-void Q_operator::initialize()
+template <typename T>
+void Q_operator<T>::initialize()
 {
     PROFILE("sirius::Q_operator::initialize");
 
@@ -540,7 +297,7 @@ void Q_operator::initialize()
                     for (auto si = 0; si < 2; si++) {
                         for (auto sj = 0; sj < 2; sj++) {
 
-                            double_complex result(0, 0);
+                            std::complex<T> result(0, 0);
 
                             for (int xi2p = 0; xi2p < nbf; xi2p++) {
                                 if (uc.atom(ia).type().compare_index_beta_functions(xi2, xi2p)) {
@@ -602,7 +359,8 @@ void Q_operator::initialize()
 template <typename T>
 void
 apply_non_local_d_q(spin_range spins__, int N__, int n__, Beta_projectors& beta__, Wave_functions<real_type<T>>& phi__,
-                    D_operator* d_op__, Wave_functions<real_type<T>>* hphi__, Q_operator* q_op__, Wave_functions<real_type<T>>* sphi__)
+                    D_operator<real_type<T>>* d_op__, Wave_functions<real_type<T>>* hphi__, Q_operator<real_type<T>>* q_op__,
+                    Wave_functions<real_type<T>>* sphi__)
 {
 
     for (int i = 0; i < beta__.num_chunks(); i++) {
@@ -637,7 +395,7 @@ apply_non_local_d_q(spin_range spins__, int N__, int n__, Beta_projectors& beta_
 template <typename T>
 void
 apply_S_operator(device_t pu__, spin_range spins__, int N__, int n__, Beta_projectors& beta__,
-                 Wave_functions<real_type<T>>& phi__, Q_operator* q_op__, Wave_functions<real_type<T>>& sphi__)
+                 Wave_functions<real_type<T>>& phi__, Q_operator<real_type<T>>* q_op__, Wave_functions<real_type<T>>& sphi__)
 {
     for (auto s: spins__) {
         sphi__.copy_from(pu__, n__, phi__, s, N__, s, N__);
@@ -648,26 +406,64 @@ apply_S_operator(device_t pu__, spin_range spins__, int N__, int n__, Beta_proje
     }
 }
 
+template class Non_local_operator<double>;
+
+template class D_operator<double>;
+
+template class Q_operator<double>;
+
 template
 void
 apply_non_local_d_q<double>(spin_range spins__, int N__, int n__, Beta_projectors& beta__,
-                            Wave_functions<double>& phi__, D_operator* d_op__, Wave_functions<double>* hphi__,
-                            Q_operator* q_op__, Wave_functions<double>* sphi__);
+                            Wave_functions<double>& phi__, D_operator<double>* d_op__, Wave_functions<double>* hphi__,
+                            Q_operator<double>* q_op__, Wave_functions<double>* sphi__);
 
 template
 void
 apply_non_local_d_q<double_complex>(spin_range spins__, int N__, int n__, Beta_projectors& beta__,
-                                    Wave_functions<double>& phi__, D_operator* d_op__, Wave_functions<double>* hphi__,
-                                    Q_operator* q_op__, Wave_functions<double>* sphi__);
+                                    Wave_functions<double>& phi__, D_operator<double>* d_op__, Wave_functions<double>* hphi__,
+                                    Q_operator<double>* q_op__, Wave_functions<double>* sphi__);
 
 template
 void
 apply_S_operator<double>(device_t pu__, spin_range spins__, int N__, int n__, Beta_projectors& beta__,
-                         Wave_functions<double>& phi__, Q_operator* q_op__, Wave_functions<double>& sphi__);
+                         Wave_functions<double>& phi__, Q_operator<double>* q_op__, Wave_functions<double>& sphi__);
 
 template
 void
 apply_S_operator<double_complex>(device_t pu__, spin_range spins__, int N__, int n__, Beta_projectors& beta__,
-                                 Wave_functions<double>& phi__, Q_operator* q_op__, Wave_functions<double>& sphi__);
+                                 Wave_functions<double>& phi__, Q_operator<double>* q_op__, Wave_functions<double>& sphi__);
 
+#ifdef USE_FP32
+// TODO: not yet have enough support to initialize it
+/*
+template class Non_local_operator<float>;
+
+template class D_operator<float>;
+
+template class Q_operator<float>
+
+template
+void
+apply_non_local_d_q<float>(spin_range spins__, int N__, int n__, Beta_projectors& beta__,
+                            Wave_functions<float>& phi__, D_operator<float>* d_op__, Wave_functions<float>* hphi__,
+                            Q_operator<float>* q_op__, Wave_functions<float>* sphi__);
+
+template
+void
+apply_non_local_d_q<std::complex<float>>(spin_range spins__, int N__, int n__, Beta_projectors& beta__,
+                                         Wave_functions<float>& phi__, D_operator<float>* d_op__, Wave_functions<float>* hphi__,
+                                         Q_operator<float>* q_op__, Wave_functions<float>* sphi__);
+
+template
+void
+apply_S_operator<float>(device_t pu__, spin_range spins__, int N__, int n__, Beta_projectors& beta__,
+                         Wave_functions<float>& phi__, Q_operator<float>* q_op__, Wave_functions<float>& sphi__);
+
+template
+void
+apply_S_operator<std::complex<float>>(device_t pu__, spin_range spins__, int N__, int n__, Beta_projectors& beta__,
+                                      Wave_functions<float>& phi__, Q_operator<float>* q_op__, Wave_functions<float>& sphi__);
+*/
+#endif
 } // namespace sirius
