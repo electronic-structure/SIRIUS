54d83bfd2d439face6e837e10ade58162e6a95a9
toxa81 <toxa81@gmail.com>
2021-08-13 15:46:45 +0200

    test fp32 for wave-functions orthogonalization

--- apps/tests/test_wf_ortho_6.cpp
+++ apps/tests/test_wf_ortho_6.cpp
@@ -2,6 +2,7 @@
 
 using namespace sirius;
 
+template <typename T>
 void test_wf_ortho(BLACS_grid const& blacs_grid__,
                    double cutoff__,
                    int num_bands__,
@@ -32,15 +33,15 @@ void test_wf_ortho(BLACS_grid const& blacs_grid__,
         return 123;
     };
 
-    Wave_functions<double> phi(gvp, num_atoms, nmt, 2 * num_bands__, mem__, nsp);
-    Wave_functions<double> tmp(gvp, num_atoms, nmt, 2 * num_bands__, mem__, nsp);
+    Wave_functions<T> phi(gvp, num_atoms, nmt, 2 * num_bands__, mem__, nsp);
+    Wave_functions<T> tmp(gvp, num_atoms, nmt, 2 * num_bands__, mem__, nsp);
 
     for (int is = 0; is < nsp; is++) {
-        phi.pw_coeffs(is).prime() = [](int64_t i0, int64_t i1){return utils::random<double_complex>();};
-        phi.mt_coeffs(is).prime() = [](int64_t i0, int64_t i1){return utils::random<double_complex>();};
+        phi.pw_coeffs(is).prime() = [](int64_t i0, int64_t i1){return utils::random<std::complex<T>>();};
+        phi.mt_coeffs(is).prime() = [](int64_t i0, int64_t i1){return utils::random<std::complex<T>>();};
     }
 
-    dmatrix<double_complex> ovlp(2 * num_bands__, 2 * num_bands__, blacs_grid__, bs__, bs__);
+    dmatrix<std::complex<T>> ovlp(2 * num_bands__, 2 * num_bands__, blacs_grid__, bs__, bs__);
 
     if (is_device_memory(mem__)) {
         ovlp.allocate(mem__);
@@ -52,15 +53,22 @@ void test_wf_ortho(BLACS_grid const& blacs_grid__,
     }
 
     for (int iss = 0; iss < num_spin_steps; iss++) {
-        orthogonalize<double_complex, 0, 0>(spla_ctx, mem__, la__, num_mag_dims__ == 3 ? 2 : iss, {&phi}, 0,           num_bands__, ovlp, tmp);
-        orthogonalize<double_complex, 0, 0>(spla_ctx, mem__, la__, num_mag_dims__ == 3 ? 2 : iss, {&phi}, num_bands__, num_bands__, ovlp, tmp);
+        orthogonalize<std::complex<T>, 0, 0>(spla_ctx, mem__, la__, num_mag_dims__ == 3 ? 2 : iss, {&phi}, 0,           num_bands__, ovlp, tmp);
+
+        inner(spla_ctx, spin_range(num_mag_dims__ == 3 ? 2 : iss), phi, 0, num_bands__, phi, 0, num_bands__, ovlp, 0, 0);
+        auto max_diff = check_identity(ovlp, num_bands__);
+        if (Communicator::world().rank() == 0) {
+            printf("maximum difference (first num_bands) : %18.12e\n", max_diff);
+        }
+
+        orthogonalize<std::complex<T>, 0, 0>(spla_ctx, mem__, la__, num_mag_dims__ == 3 ? 2 : iss, {&phi}, num_bands__, num_bands__, ovlp, tmp);
     }
 
     for (int iss = 0; iss < num_spin_steps; iss++) {
         inner(spla_ctx, spin_range(num_mag_dims__ == 3 ? 2 : iss), phi, 0, 2 * num_bands__, phi, 0, 2 * num_bands__, ovlp, 0, 0);
         auto max_diff = check_identity(ovlp, 2 * num_bands__);
         if (Communicator::world().rank() == 0) {
-            printf("maximum difference: %18.12f\n", max_diff);
+            printf("maximum difference: %18.12e\n", max_diff);
             if (max_diff > 1e-12) {
                 printf("\x1b[31m" "Fail\n" "\x1b[0m" "\n");
             } else {
@@ -70,6 +78,7 @@ void test_wf_ortho(BLACS_grid const& blacs_grid__,
     }
 }
 
+template <typename T>
 void call_test(std::vector<int> mpi_grid_dims__,
                double cutoff__,
                int num_bands__,
@@ -86,7 +95,7 @@ void call_test(std::vector<int> mpi_grid_dims__,
         blacs_grid = std::unique_ptr<BLACS_grid>(new BLACS_grid(Communicator::world(), mpi_grid_dims__[0], mpi_grid_dims__[1]));
     }
     for (int i = 0; i < repeat__; i++) {
-        test_wf_ortho(*blacs_grid, cutoff__, num_bands__, bs__, num_mag_dims__, mem__, la__);
+        test_wf_ortho<T>(*blacs_grid, cutoff__, num_bands__, bs__, num_mag_dims__, mem__, la__);
     }
 }
 
@@ -100,6 +109,7 @@ int main(int argn, char** argv)
     args.register_key("--num_mag_dims=", "{int} number of magnetic dimensions");
     args.register_key("--linalg_t=", "{string} type of the linear algebra driver");
     args.register_key("--memory_t=", "{string} type of memory");
+    args.register_key("--fp32", "use FP32 arithmetics");
 
     args.parse_args(argn, argv);
     if (args.exist("help")) {
@@ -116,7 +126,16 @@ int main(int argn, char** argv)
     auto mem = get_memory_t(args.value<std::string>("memory_t", "host"));
 
     sirius::initialize(1);
-    call_test(mpi_grid_dims, cutoff, num_bands, bs, num_mag_dims, mem, la, 1);
+    if (args.exist("fp32")) {
+#if defined(USE_FP32)
+        call_test<float>(mpi_grid_dims, cutoff, num_bands, bs, num_mag_dims, mem, la, 1);
+#else
+        RTE_THROW("Not compiled with FP32 support");
+#endif
+    } else {
+        call_test<double>(mpi_grid_dims, cutoff, num_bands, bs, num_mag_dims, mem, la, 1);
+    }
+
     int my_rank = Communicator::world().rank();
 
     sirius::finalize(1);
