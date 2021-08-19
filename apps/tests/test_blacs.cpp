e59dbf207ea38f813270befda6ca72eb03b821a8
toxa81 <toxa81@gmail.com>
2021-08-19 10:36:08 +0200

    test only with scalapack enabled

--- apps/tests/test_blacs.cpp
+++ apps/tests/test_blacs.cpp
@@ -6,6 +6,7 @@ int main(int argn, char** argv)
 {
     sirius::initialize(true);
 
+#if defined(SIRIUS_SCALAPACK)
     std::cout << Communicator::self().size() << " " << Communicator::self().rank() << std::endl;
     std::cout << Communicator::world().size() << " " << Communicator::world().rank() << std::endl;
 
@@ -13,6 +14,7 @@ int main(int argn, char** argv)
     blacs_handler = linalg_base::create_blacs_handler(Communicator::world().mpi_comm());
 
     sirius::finalize(true);
+#endif
 
     return 0;
 }
