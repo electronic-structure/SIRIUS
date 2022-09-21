8966c80b3fe7674d3e727ae9cf37f35addf56ee2
Anton Kozhevnikov <toxa81@gmail.com>
2022-09-21 20:59:37 +0000

    update test

--- apps/tests/test_wf_trans.cpp
+++ apps/tests/test_wf_trans.cpp
@@ -5,45 +5,7 @@ using namespace sirius;
 
 template <typename T, typename F>
 void test_wf_trans(sddk::BLACS_grid const& blacs_grid__, double cutoff__, int num_bands__, int bs__, int num_mag_dims__,
-                   sddk::memory_t mem__, sddk::linalg_t la__)
-{
-    //spla::Context spla_ctx(
-    //    la__ == sddk::linalg_t::blas || la__ == sddk::linalg_t::lapack || la__ == sddk::linalg_t::scalapack ? SPLA_PU_HOST : SPLA_PU_GPU);
-
-    //int nsp = (num_mag_dims__ == 0) ? 1 : 2;
-
-    //matrix3d<double> M = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
-    //sddk::Gvec gvec(M, cutoff__, sddk::Communicator::world(), false);
-    //sddk::Gvec_partition gvp(gvec, sddk::Communicator::world(), sddk::Communicator::self());
-    //if (sddk::Communicator::world().rank() == 0) {
-    //    printf("number of bands          : %i\n", num_bands__);
-    //    printf("number of spins          : %i\n", nsp);
-    //    printf("full spinors             : %i\n", num_mag_dims__ == 3);
-    //    printf("total number of G-vectors: %i\n", gvec.num_gvec());
-    //    printf("local number of G-vectors: %i\n", gvec.count());
-    //}
-
-    //int num_atoms = 31;
-    //auto nmt = [](int i) {
-    //    return 123;
-    //};
-
-    //sddk::Wave_functions<T> phi(gvp, num_atoms, nmt, 2 * num_bands__, mem__, nsp);
-    //sddk::Wave_functions<T> tmp(gvp, num_atoms, nmt, 2 * num_bands__, mem__, nsp);
-
-    //for (int is = 0; is < nsp; is++) {
-    //    phi.pw_coeffs(is).prime() = [](int64_t i0, int64_t i1){return utils::random<std::complex<T>>();};
-    //    phi.mt_coeffs(is).prime() = [](int64_t i0, int64_t i1){return utils::random<std::complex<T>>();};
-    //}
-
-    //sddk::dmatrix<F> tmtrx(2 * num_bands__, 2 * num_bands__, blacs_grid__, bs__, bs__);
-
-    //sddk::transform<std::complex<T>, F>(spla_ctx, 0, phi, 0, num_bands__, tmtrx, 0, 0, tmp, 0, num_bands__);
-}
-
-template <typename T, typename F>
-void test_wf_trans_new(sddk::BLACS_grid const& blacs_grid__, double cutoff__, int num_bands__, int bs__, int num_mag_dims__,
-                   sddk::memory_t mem__, sddk::linalg_t la__)
+                   sddk::memory_t mem__)
 {
     spla::Context spla_ctx(is_host_memory(mem__) ? SPLA_PU_HOST : SPLA_PU_GPU);
 
@@ -69,7 +31,7 @@ void test_wf_trans_new(sddk::BLACS_grid const& blacs_grid__, double cutoff__, in
     wf::Wave_functions<T> psi(gvec, num_mt_coeffs, wf::num_mag_dims(num_mag_dims__), wf::num_bands(num_bands__),
             sddk::memory_t::host);
 
-    auto sr = wf::spin_range(0, 2);
+    auto sr = num_mag_dims__ == 3 ? wf::spin_range(0, 2) : wf::spin_range(0);
 
     for (auto s = sr.begin(); s != sr.end(); s++) {
         for (int i = 0; i < num_bands__; i++) {
@@ -140,7 +102,7 @@ void test_wf_trans_new(sddk::BLACS_grid const& blacs_grid__, double cutoff__, in
 
 template <typename T>
 void call_test(std::vector<int> mpi_grid_dims__, double cutoff__, int num_bands__, int bs__, int num_mag_dims__,
-               sddk::memory_t mem__, sddk::linalg_t la__, int repeat__)
+               sddk::memory_t mem__, int repeat__)
 {
     std::unique_ptr<sddk::BLACS_grid> blacs_grid;
     if (mpi_grid_dims__[0] * mpi_grid_dims__[1] == 1) {
@@ -149,8 +111,7 @@ void call_test(std::vector<int> mpi_grid_dims__, double cutoff__, int num_bands_
         blacs_grid = std::make_unique<sddk::BLACS_grid>(sddk::Communicator::world(), mpi_grid_dims__[0], mpi_grid_dims__[1]);
     }
     for (int i = 0; i < repeat__; i++) {
-        test_wf_trans<T, double>(*blacs_grid, cutoff__, num_bands__, bs__, num_mag_dims__, mem__, la__);
-        test_wf_trans_new<T, std::complex<double>>(*blacs_grid, cutoff__, num_bands__, bs__, 3, mem__, la__);
+        test_wf_trans<T, std::complex<double>>(*blacs_grid, cutoff__, num_bands__, bs__, num_mag_dims__, mem__);
     }
 }
 
@@ -162,7 +123,6 @@ int main(int argn, char** argv)
     args.register_key("--bs=", "{int} block size");
     args.register_key("--num_bands=", "{int} number of bands");
     args.register_key("--num_mag_dims=", "{int} number of magnetic dimensions");
-    args.register_key("--linalg_t=", "{string} type of the linear algebra driver");
     args.register_key("--memory_t=", "{string} type of memory");
     args.register_key("--fp32", "use FP32 arithmetics");
 
@@ -177,18 +137,17 @@ int main(int argn, char** argv)
     auto bs = args.value<int>("bs", 32);
     auto num_bands = args.value<int>("num_bands", 100);
     auto num_mag_dims = args.value<int>("num_mag_dims", 0);
-    auto la = sddk::get_linalg_t(args.value<std::string>("linalg_t", "blas"));
     auto mem = sddk::get_memory_t(args.value<std::string>("memory_t", "host"));
 
     sirius::initialize(1);
     if (args.exist("fp32")) {
 #if defined(USE_FP32)
-        call_test<float>(mpi_grid_dims, cutoff, num_bands, bs, num_mag_dims, mem, la, 1);
+        call_test<float>(mpi_grid_dims, cutoff, num_bands, bs, num_mag_dims, mem, 1);
 #else
         RTE_THROW("Not compiled with FP32 support");
 #endif
     } else {
-        call_test<double>(mpi_grid_dims, cutoff, num_bands, bs, num_mag_dims, mem, la, 1);
+        call_test<double>(mpi_grid_dims, cutoff, num_bands, bs, num_mag_dims, mem, 1);
     }
 
     int my_rank = sddk::Communicator::world().rank();
