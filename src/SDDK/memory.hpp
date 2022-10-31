bede097d48febb82d8bba10af49f25e5199cc796
Simon Pintarelli <simon.pintarelli@cscs.ch>
2022-10-31 09:21:58 +0100

    make sure memory pool cannot be duplicated in compilation units

--- src/SDDK/memory.hpp
+++ src/SDDK/memory.hpp
@@ -663,14 +663,7 @@ void memory_pool_deleter::memory_pool_deleter_impl::free(void* ptr__)
 
 /// Return a memory pool.
 /** A memory pool is created when this function called for the first time. */
-inline sddk::memory_pool& get_memory_pool(sddk::memory_t M__)
-{
-    static std::map<sddk::memory_t, sddk::memory_pool> memory_pool_;
-    if (memory_pool_.count(M__) == 0) {
-        memory_pool_.emplace(M__, sddk::memory_pool(M__));
-    }
-    return memory_pool_.at(M__);
-}
+sddk::memory_pool& get_memory_pool(sddk::memory_t M__);
 
 #ifdef NDEBUG
 #define mdarray_assert(condition__)
