8114ab55c6c64f495a023087e04bf25bbfe389e9
Anton Kozhevnikov <toxa81@gmail.com>
2023-02-09 16:23:49 +0000

    introduce the base class

--- src/density/paw_density.hpp
+++ src/density/paw_density.hpp
@@ -28,66 +28,100 @@
 namespace sirius {
 
 template <typename T>
-class paw_density
+class PAW_field4D
 {
   private:
-    Unit_cell const* uc_{nullptr};
-    std::array<Spheric_function_set<T>, 4> ae_density_;
-    std::array<Spheric_function_set<T>, 4> ps_density_;
-
+    /// Unit cell.
+    Unit_cell const& uc_;
+    /// All-electron part.
+    std::array<Spheric_function_set<T>, 4> ae_components_;
+    /// Pseudo-potential part.
+    std::array<Spheric_function_set<T>, 4> ps_components_;
     /* copy constructor is forbidden */
-    paw_density(paw_density const& src__) = delete;
+    PAW_field4D(PAW_field4D const& src__) = delete;
     /* copy assignment operator is forbidden */
-    paw_density& operator=(paw_density const& src__) = delete;
-
+    PAW_field4D& operator=(PAW_field4D const& src__) = delete;
   public:
-
-    paw_density(Unit_cell const& uc__)
-        : uc_{&uc__}
+    PAW_field4D(Unit_cell const& uc__, bool is_global__)
+        : uc_{uc__}
     {
         if (!uc__.num_paw_atoms()) {
             return;
         }
 
+        auto ptr = (is_global__) ? nullptr : &uc__.spl_num_paw_atoms();
+
         for (int j = 0; j < uc__.parameters().num_mag_dims() + 1; j++) {
-            ae_density_[j] = Spheric_function_set<T>(uc__, uc__.paw_atoms(),
-                    [&uc__](int ia){return 2 * uc__.atom(ia).type().indexr().lmax();}, &uc__.spl_num_paw_atoms());
-            ps_density_[j] = Spheric_function_set<T>(uc__, uc__.paw_atoms(),
-                    [&uc__](int ia){return 2 * uc__.atom(ia).type().indexr().lmax();}, &uc__.spl_num_paw_atoms());
+            ae_components_[j] = Spheric_function_set<T>(uc__, uc__.paw_atoms(),
+                    [&uc__](int ia){return 2 * uc__.atom(ia).type().indexr().lmax();}, ptr);
+            ps_components_[j] = Spheric_function_set<T>(uc__, uc__.paw_atoms(),
+                    [&uc__](int ia){return 2 * uc__.atom(ia).type().indexr().lmax();}, ptr);
         }
+
     }
 
     void zero(int ia__)
     {
-        for (int j = 0; j < uc_->parameters().num_mag_dims() + 1; j++) {
-            ae_density_[j][ia__].zero(sddk::memory_t::host);
-            ps_density_[j][ia__].zero(sddk::memory_t::host);
+        for (int j = 0; j < uc_.parameters().num_mag_dims() + 1; j++) {
+            ae_components_[j][ia__].zero(sddk::memory_t::host);
+            ps_components_[j][ia__].zero(sddk::memory_t::host);
         }
     }
 
-    auto& ae_density(int i__, int ja__)
+    auto& ae_component(int i__)
     {
-        return ae_density_[i__][ja__];
+        return ae_components_[i__];
     }
 
-    auto const& ae_density(int i__, int ja__) const
+    auto const& ae_component(int i__) const
     {
-        return ae_density_[i__][ja__];
+        return ae_components_[i__];
     }
 
-    auto& ps_density(int i__, int ja__)
+    auto& ps_component(int i__)
     {
-        return ps_density_[i__][ja__];
+        return ps_components_[i__];
     }
 
-    auto const& ps_density(int i__, int ja__) const
+    auto const& ps_component(int i__) const
     {
-        return ps_density_[i__][ja__];
+        return ps_components_[i__];
     }
 
     auto const& unit_cell() const
     {
-        return *uc_;
+        return uc_;
+    }
+};
+
+template <typename T>
+class paw_density : public PAW_field4D<T>
+{
+  public:
+
+    paw_density(Unit_cell const& uc__)
+        : PAW_field4D<T>(uc__, false)
+    {
+    }
+
+    auto& ae_density(int i__, int ja__)
+    {
+        return this->ae_component(i__)[ja__];
+    }
+
+    auto const& ae_density(int i__, int ja__) const
+    {
+        return this->ae_component(i__)[ja__];
+    }
+
+    auto& ps_density(int i__, int ja__)
+    {
+        return this->ps_component(i__)[ja__];
+    }
+
+    auto const& ps_density(int i__, int ja__) const
+    {
+        return this->ps_component(i__)[ja__];
     }
 };
 
