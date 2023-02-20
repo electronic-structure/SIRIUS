8e71bb801849ddd0de89a504ad2ddbd8dd9598d5
Anton Kozhevnikov <toxa81@gmail.com>
2023-02-20 22:10:43 +0000

    fixes

--- src/symmetry/symmetrize.hpp
+++ src/symmetry/symmetrize.hpp
@@ -495,7 +495,8 @@ symmetrize(Crystal_symmetry const& sym__, mpi::Communicator const& comm__, int n
         sht::rotation_matrix(lmax, sym__[i].spg_op.euler_angles, sym__[i].spg_op.proper, rotm);
 
         for (int ialoc = 0; ialoc < spl_atoms.local_size(); ialoc++) {
-            int ia = spl_atoms[ialoc];
+            /* get global index of the atom */
+            int ia = frlm.atoms()[spl_atoms[ialoc]];
             int lmmax_ia = frlm[ia].angular_domain_size();
             int nrmax_ia = frlm.unit_cell().atom(ia).num_mt_points();
             int ja = sym__[i].spg_op.inv_sym_atom[ia];
@@ -506,11 +507,10 @@ symmetrize(Crystal_symmetry const& sym__, mpi::Communicator const& comm__, int n
                     (*frlm__[j])[ja].ld(), &la::constant<double>::zero(),
                     ftmp.at(sddk::memory_t::host, 0, 0, j), ftmp.ld());
             }
-            if (num_mag_dims__ == 0) {
-                for (int ir = 0; ir < nrmax_ia; ir++) {
-                    for (int lm = 0; lm < lmmax_ia; lm++) {
-                        fsym_loc(lm, ir, 0, ialoc) += ftmp(lm, ir, 0);
-                    }
+            /* always symmetrize the scalar component */
+            for (int ir = 0; ir < nrmax_ia; ir++) {
+                for (int lm = 0; lm < lmmax_ia; lm++) {
+                    fsym_loc(lm, ir, 0, ialoc) += ftmp(lm, ir, 0);
                 }
             }
             /* apply S part to [0, 0, z] collinear vector */
