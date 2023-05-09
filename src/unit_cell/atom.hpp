df424f23b28ed6d75d5b249272f8db650e3cfb2d
Anton Kozhevnikov <toxa81@gmail.com>
2023-05-09 21:25:11 +0000

    use mt_basis_size

--- src/unit_cell/atom.hpp
+++ src/unit_cell/atom.hpp
@@ -122,7 +122,7 @@ class Atom
         }
 
         if (!type().parameters().full_potential()) {
-            int nbf = type().mt_lo_basis_size();
+            int nbf = type().mt_basis_size();
             d_mtrx_ = sddk::mdarray<double, 3>(nbf, nbf, type().parameters().num_mag_dims() + 1, sddk::memory_t::host,
                                                "Atom::d_mtrx_");
             d_mtrx_.zero();
