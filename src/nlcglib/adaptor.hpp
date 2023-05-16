f5ba1a612f9469981f4e834dff8e42da022da388
Simon Pintarelli <simon.pintarelli@cscs.ch>
2023-05-16 10:02:01 +0200

    header

--- src/nlcglib/adaptor.hpp
+++ src/nlcglib/adaptor.hpp
@@ -1,5 +1,24 @@
-#ifndef NLCGLIB_ADAPTOR_H
-#define NLCGLIB_ADAPTOR_H
+// Copyright (c) 2023 Simon Pintarelli, Anton Kozhevnikov, Thomas Schulthess
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+// the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
+//    following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+//    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef __NLCGLIB_ADAPTOR_HPP__
+#define __NLCGLIB_ADAPTOR_HPP__
 
 #include <memory>
 #include <nlcglib/interface.hpp>
@@ -231,4 +250,4 @@ make_matrix_view(nlcglib::buffer_protocol<numeric_t, 2>& buf)
 
 } // namespace sirius
 
-#endif /* NLCGLIB_ADAPTOR_H */
+#endif /* __NLCGLIB_ADAPTOR_HPP__ */
