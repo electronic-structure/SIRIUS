python: fix band energy computation for gamma point

python_module/sirius/ot/ot_basic.py
@@ -90,7 +90,11 @@ class Energy:
         for key, val in yn.items():
             k, ispn = key
             benergies = np.zeros(self.ctx.num_bands(), dtype=np.complex128)
-            benergies[: val.shape[1]] = np.einsum("ij,ij->j", val, np.conj(X[key]))
+            if self.ctx.gamma_point:
+                tmp = np.einsum("ij,ij->j", val[1:,:], np.conj(X[key])[1:, :])
+                benergies[: val.shape[1]] = (np.array(val[0,:]) * np.array(X[key][0,:]) + 2 * np.real(tmp)).flatten()
+            else:
+                benergies[: val.shape[1]] = np.einsum("ij,ij->j", val, np.conj(X[key]))
 
             for j, ek in enumerate(benergies):
                 self.kpointset[k].set_band_energy(j, ispn, np.real(ek))