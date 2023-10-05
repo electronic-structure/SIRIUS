cbb585a5713a936562b2ece3be0e18df1523aaa2
Anton Kozhevnikov <toxa81@gmail.com>
2023-10-05 13:47:31 +0000

    add missing file

---
+++ src/utils/packed_index.hpp
@@ -0,0 +1,30 @@
+#ifndef __PACKED_INDEX_HPP__
+#define __PACKED_INDEX_HPP__
+
+namespace sirius {
+
+/// Pack two indices into one for symmetric matrices.
+inline int packed_index(int i__, int j__)
+{
+    /* suppose we have a symmetric matrix: M_{ij} = M_{ji}
+           j
+       +-------+
+       | + + + |
+      i|   + + |   -> idx = j * (j + 1) / 2 + i  for  i <= j
+       |     + |
+       +-------+
+
+       i, j are row and column indices 
+    */
+
+    if (i__ > j__) {
+        std::swap(i__, j__);
+    }
+    return j__ * (j__ + 1) / 2 + i__;
+}
+
+}
+
+#endif
+
+
