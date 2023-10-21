54866ab9c1b6e7a3f37df16891136ad4f390e4f4
Anton Kozhevnikov <toxa81@gmail.com>
2023-10-21 20:02:04 +0000

    fix

--- src/core/acc/acc.hpp
+++ src/core/acc/acc.hpp
@@ -514,10 +514,12 @@ inline bool check_device_ptr(void const* ptr__)
 
 } // namespace acc
 
+#if defined(SIRIUS_CUDA) || defined(SIRIUS_ROCM)
 extern "C" {
 void scale_matrix_rows_gpu(int nrow, int ncol, acc_complex_double_t* mtrx, double const* v);
 void scale_matrix_elements_gpu(acc_complex_double_t* ptr__, int ld__, int nrow__, int ncol__, double beta__);
 }
+#endif
 
 } // namespace sirius
 
