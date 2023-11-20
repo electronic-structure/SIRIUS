4d95dc9956da2a872384d0cf56cb5df0b91e24eb
Anton Kozhevnikov <toxa81@gmail.com>
2023-11-20 17:32:01 +0000

    apply format

--- src/k_point/generate_w90_coeffs.hpp
+++ src/k_point/generate_w90_coeffs.hpp
@@ -2,7 +2,7 @@
  *
  *  \brief Interface to W90 library.
  */
-#if defined(SIRIUS_WANNIER90)
+#ifdef SIRIUS_WANNIER90
 
 #ifndef __GENERATE_W90_COEFFS_HPP__
 #define __GENERATE_W90_COEFFS_HPP__
@@ -11,27 +11,69 @@
 
 namespace sirius {
 
-using fortran_bool = int16_t;
+using fortran_bool = int32_t; // int16_t;
 
 extern "C" {
 // TODO: add names of arguments
-void wannier_setup_(const char*, int32_t*, int32_t*, const double*, const double*, double*,
-                    int32_t*, // care! arg (4,5) changed with const
-                    int32_t*, char (*)[3], double*, fortran_bool*, fortran_bool*, int32_t*, int32_t*, int32_t*, int32_t*, int32_t*,
-                    double*, int32_t*, int32_t*, int32_t*, double*, double*, double*, int32_t*, int32_t*, double*,
-                    size_t, size_t);
+void
+wannier_setup_(const char*, int32_t*, int32_t*, const double*, const double*, double*,
+               int32_t*, // care! arg (4,5) changed with const
+               int32_t*, char (*)[3], double*, fortran_bool*, fortran_bool*, int32_t*, int32_t*, int32_t*, int32_t*,
+               int32_t*, double*, int32_t*, int32_t*, int32_t*, double*, double*, double*, int32_t*, int32_t*, double*,
+               size_t, size_t);
 
-void wannier_run_(const char*, int32_t*, int32_t*, double*, double*, double*, int32_t*, int32_t*, int32_t*, int32_t*,
-                  char (*)[3], double*, fortran_bool*, std::complex<double>*, std::complex<double>*, double*,
-                  std::complex<double>*, std::complex<double>*, fortran_bool*, double*, double*, double*, size_t, size_t);
+void
+wannier_run_(const char*, int32_t*, int32_t*, double*, double*, double*, int32_t*, int32_t*, int32_t*, int32_t*,
+             char (*)[3], double*, fortran_bool*, std::complex<double>*, std::complex<double>*, double*,
+             std::complex<double>*, std::complex<double>*, fortran_bool*, double*, double*, double*, size_t, size_t);
 }
 
-void write_Amn(mdarray<std::complex<double>, 3>& Amn);
+struct k_info
+{
+    r3::vector<double> ibz;
+    int ik_ibz;
+    r3::vector<double> fbz;
+    r3::vector<double> G;
+    r3::matrix<int> R;
+    r3::matrix<int> invR;
+    r3::vector<double> t;
+};
 
-void write_Mmn(mdarray<std::complex<double>, 4>& M, mdarray<int, 2>& nnlist,
-               mdarray<int32_t, 3>& nncell);
+void
+read_nnkp(const int& num_kpts, int& num_wann, int& nntot, mdarray<int, 2>& nnlist, mdarray<int32_t, 3>& nncell,
+          mdarray<int32_t, 1>& exclude_bands);
 
-void write_eig(mdarray<double, 2>& eigval);
+void
+write_Amn(mdarray<std::complex<double>, 3> const& Amn, int const& num_kpts, int const& num_bands, int const& num_wann);
+
+void
+write_Mmn(mdarray<std::complex<double>, 4> const& M, mdarray<int, 2> const& nnlist, mdarray<int32_t, 3> const& nncell,
+          int const& num_kpts, int const& num_neighbors, int const& num_bands);
+
+void
+write_eig(mdarray<double, 2> const& eigval, int const& num_bands, int const& num_kpts);
+
+void
+from_irreduciblewedge_to_fullbrillouinzone(K_point_set& kset_ibz, K_point_set& kset_fbz, std::vector<k_info>& k_temp);
+
+void
+rotate_wavefunctions(K_point_set& kset_ibz, K_point_set& kset_fbz, std::vector<k_info> const& k_temp,
+                     int const& num_bands, std::vector<int> const& band_index_tot);
+
+void
+calculate_Amn(K_point_set& kset_fbz, int const& num_bands, int const& num_wann, mdarray<std::complex<double>, 3>& A);
+
+void
+send_receive_kpb(std::vector<std::shared_ptr<fft::Gvec>>& gvec_kpb,
+                 std::vector<mdarray<std::complex<double>, 2>>& wf_kpb, K_point_set& kset_fbz,
+                 std::vector<int>& ikpb_index, int const& nntot, mdarray<int, 2> const& nnlist, int const& num_bands);
+
+void
+calculate_Mmn(mdarray<std::complex<double>, 4>& M, K_point_set& kset_fbz, int const& num_bands,
+              std::vector<std::shared_ptr<fft::Gvec>> const& gvec_kpb,
+              std::vector<mdarray<std::complex<double>, 2>> const& wf_kpb, std::vector<int> const& ikpb_index,
+              int const& nntot, mdarray<int, 2> const& nnlist, mdarray<int, 3> const& nncell);
+
 } // namespace sirius
 
 #endif
