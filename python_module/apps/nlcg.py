c07353ad5e2f921164e02ea0edc0b80823c914dd
Simon Pintarelli <simon.pintarelli@cscs.ch>
2024-03-16 22:52:09 +0100

    python: nlcg disable hdf5 dump

--- python_module/apps/nlcg.py
+++ python_module/apps/nlcg.py
@@ -24,11 +24,14 @@ if __name__ == '__main__':
 
         def _callback(fn, it, **kwargs):
             if it % interval == 0:
+                pass
                 # kset.ctx().create_storage_file()
                 # store_density_potential(E.density, E.potential)
-                mag_mom = E.density.compute_atomic_mag_mom()
-                save_state({'f': fn, 'ek': kset.e, 'mag_mom': mag_mom},
-                           kset=kset, prefix='fn_%05d_' % it)
+                # mag_mom = E.density.compute_atomic_mag_mom()
+                # save_state({'f': fn, 'ek': kset.e, 'mag_mom': mag_mom},
+                #            kset=kset, prefix='fn_%05d_' % it)
+                # save_state({'f': fn, 'ek': kset.e},
+                #            kset=kset, prefix='fn_%05d_' % it)
         return _callback
 
     def error_callback(kset, **kwargs):
@@ -36,12 +39,13 @@ if __name__ == '__main__':
 
         if args.dump_on_error:
             def _callback(prefix='nlcg_dump', **kwargs):
+                pass
                 # kset.ctx().create_storage_file()
                 # store_density_potential(E.density, E.potential)
-                mag_mom = E.density.compute_atomic_mag_mom()
-                dump_dict = {**kwargs, 'mag_mom': mag_mom}
-                save_state(dump_dict,
-                           kset=kset, prefix=prefix)
+                # mag_mom = E.density.compute_atomic_mag_mom()
+                # dump_dict = {**kwargs, 'mag_mom': mag_mom}
+                # save_state(dump_dict,
+                #            kset=kset, prefix=prefix)
         else:
             def _callback(*args, **kwargs):
                 return None
