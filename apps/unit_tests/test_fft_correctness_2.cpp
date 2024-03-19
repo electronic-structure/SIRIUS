1ec31fab64fcc120fe8e899dba5f3d6476885f93
toxa81 <toxa81@gmail.com>
2024-03-19 17:46:40 +0100

    update licence boilerplate

--- apps/unit_tests/test_fft_correctness_2.cpp
+++ apps/unit_tests/test_fft_correctness_2.cpp
@@ -1,3 +1,11 @@
+/* This file is part of SIRIUS electronic structure library.
+ *
+ * Copyright (c), ETH Zurich.  All rights reserved.
+ *
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 #include <sirius.hpp>
 
 /* test FFT: tranfrom random function to real space, transform back and compare with the original function */
