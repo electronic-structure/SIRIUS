4115fd34f48c7b297602c093335f5f457dbc91b7
toxa81 <toxa81@gmail.com>
2024-03-20 09:54:51 +0100

    fix rt_grapth

--- src/core/rt_graph.cpp
+++ src/core/rt_graph.cpp
@@ -1,11 +1,3 @@
-/* This file is part of SIRIUS electronic structure library.
- *
- * Copyright (c), ETH Zurich.  All rights reserved.
- *
- * Please, refer to the LICENSE file in the root directory.
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 /*
  * Copyright (c) 2019 Simon Frasch
  *
