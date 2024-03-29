bd9bb4581a9ee6939f89001b1336e29859ced84c
Anton Kozhevnikov <toxa81@gmail.com>
2024-03-29 21:21:52 +0100

    [update] Licence boilerplate (#978)
    * update main licence to BSD 3-clause
    
    * update licence boilerplate
    
    * add AUTHORS

--- apps/tests/test_examples.cpp
+++ apps/tests/test_examples.cpp
@@ -1,3 +1,11 @@
+/* This file is part of SIRIUS electronic structure library.
+ *
+ * Copyright (c), ETH Zurich.  All rights reserved.
+ *
+ * Please, refer to the LICENSE file in the root directory.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 #include <sirius.hpp>
 #include <spla/spla.hpp>
 #include "core/wf/wave_functions.hpp"
