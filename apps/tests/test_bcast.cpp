104f25a325a400ffcea8a68782fcb57ba4f919cd
Anton Kozhevnikov <toxa81@gmail.com>
2024-06-18 15:30:22 +0000

    init cmd_args in test_bcast.cpp

--- apps/tests/test_bcast.cpp
+++ apps/tests/test_bcast.cpp
@@ -6,38 +6,44 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <sirius.h>
+#include <sirius.hpp>
+#include "testing.hpp"
 
 using namespace sirius;
 
-void
-test()
+int
+test1(cmd_args const& args__)
 {
-    int size = (1 << 22);
-    int nt   = 1;
-    mdarray<int, 2> buff(size, nt);
+    auto size = args__.value("size", (1 << 20));
 
-    Communicator comm(MPI_COMM_WORLD);
+    double const mb{1<<20};
 
-    Timer t("bcast");
-    #pragma omp parallel num_threads(nt)
-    {
-        int thread_id = Platform::thread_id();
-        for (int i = 0; i < comm.size(); i++) {
-            comm.bcast(&buff(0, thread_id), size, i);
-        }
+    double t = -wtime();
+    mdarray<char, 1> buf({size});
+    buf.zero();
+
+    /* N of MPI ranks broadcast one by one the buffer of `size` bytes. Each of N-1 ranks recieves a copy
+     * of a buffer. Number of bytes moved across the network: N * (N-1) * size */
+
+    int N =  mpi::Communicator::world().size();
+    for (int r = 0; r < N; r++) {
+        mpi::Communicator::world().bcast(buf.at(memory_t::host), size, r);
     }
-    double tval = t.stop();
-
-    if (comm.rank() == 0) {
-        printf("bcast sepeed : %f [MB / sec]\n", double(size * sizeof(int) * comm.size()) / tval / (1 << 20));
+    t += wtime();
+    if (mpi::Communicator::world().rank() == 0) {
+        printf("time  : %f sec.\n", t);
+        printf("speed : %f Mb/s,\n", static_cast<uint64_t>(N) * (N - 1) * size / mb / t);
     }
+    return 0;
 }
 
 int
 main(int argn, char** argv)
 {
-    Platform::initialize(1);
-    test();
-    Platform::finalize();
+    cmd_args args(argn, argv, {
+        {"size=", "buffer size in bytes"}});
+
+    sirius::initialize(1);
+    call_test("test_bcast_v2", test1, args);
+    sirius::finalize();
 }
