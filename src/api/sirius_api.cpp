53a38cd0cd71058d622993a534a28ce7ab88cb49
Anton Kozhevnikov <toxa81@gmail.com>
2024-07-21 15:58:01 +0000

    apply format

--- src/api/sirius_api.cpp
+++ src/api/sirius_api.cpp
@@ -5833,9 +5833,8 @@ sirius_add_hubbard_atom_pair(void* const* handler__, int* const atom_pair__, int
                     for (int idx = 0; idx < conf_dict.nonlocal().size(); idx++) {
                         auto v = conf_dict.nonlocal(idx);
                         s << "index : " << idx << ", atoms : " << v.atom_pair()[0] << " " << v.atom_pair()[1]
-                          << ", T : " << v.T()[0] << " " << v.T()[1] << " " << v.T()[2]
-                          << ", n : " << v.n()[0] << " " << v.n()[1]
-                          << ", l : " << v.l()[0] << " " << v.l()[1] << std::endl;
+                          << ", T : " << v.T()[0] << " " << v.T()[1] << " " << v.T()[2] << ", n : " << v.n()[0] << " "
+                          << v.n()[1] << ", l : " << v.l()[0] << " " << v.l()[1] << std::endl;
                     }
                     RTE_THROW(s);
                 }
