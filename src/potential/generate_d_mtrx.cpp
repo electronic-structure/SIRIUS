1b45970b59a2f2b15e8548f71efb54a68be23215
toxa81 <toxa81@gmail.com>
2025-01-29 00:28:23 +0100

    minor fixes

--- src/potential/generate_d_mtrx.cpp
+++ src/potential/generate_d_mtrx.cpp
@@ -65,9 +65,7 @@ Potential::generate_d_mtrx()
         if (!atom_type.augment() || atom_type.num_atoms() == 0) {
             for (int iv = 0; iv < ctx_.num_mag_dims() + 1; iv++) {
                 for (int i = 0; i < atom_type.num_atoms(); i++) {
-                    int ia     = atom_type.atom_id(i);
-                    auto& atom = unit_cell_.atom(ia);
-
+                    int ia = atom_type.atom_id(i);
                     for (int xi2 = 0; xi2 < nbf; xi2++) {
                         for (int xi1 = 0; xi1 < nbf; xi1++) {
                             d_mtrx_[ia](xi1, xi2, iv) = 0;
