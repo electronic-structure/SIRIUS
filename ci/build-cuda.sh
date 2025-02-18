0dbe57d95a6fbe6f5998a1f0864a102632361c8b
Anton Kozhevnikov <toxa81@gmail.com>
2025-02-19 00:05:52 +0100

    try a fix

--- ci/build-cuda.sh
+++ ci/build-cuda.sh
@@ -4,8 +4,11 @@ set -xeuo pipefail
 
 export SPACK_SYSTEM_CONFIG_PATH=/user-environment/config
 
+spack --version
+
 # make sure we keep the stage direcorty
 spack config --scope=user add config:build_stage:/dev/shm/spack-stage
+spack config --scope=user update config
 
 spack env create -d ./spack-env
 # add local repository with current sirius recipe
