92aaf599bd9c02d10214ccc186bc48f804d80301
Anton Kozhevnikov <toxa81@gmail.com>
2025-03-09 23:07:18 +0100

    remove debug hacks

--- ci/build-cuda.sh
+++ ci/build-cuda.sh
@@ -4,8 +4,6 @@ set -xeuo pipefail
 
 export SPACK_SYSTEM_CONFIG_PATH=/user-environment/config
 
-spack --version
-
 # make sure we keep the stage direcorty
 spack config --scope=user add config:build_stage:/dev/shm/spack-stage
 
@@ -14,13 +12,6 @@ spack env create -d ./spack-env
 spack -e ./spack-env repo add $REPO
 
 spack -e ./spack-env config add "packages:all:variants:[cuda_arch=${CUDA_ARCH},+cuda]"
-
-# workaround, first command fails asking to update config format, doesn't make any sense, cannot reproduce on cli
-#spack -e ./spack-env config add config:install_tree:$SPACK_INSTALL_TREE
-yq w -i ./spack-env/spack.yaml 'spack.config.install_tree.root' $SPACK_INSTALL_TREE
-#spack -e ./spack-env config add 'config:install_tree:projections:all:"{name}-{version}"'
-yq w -i ./spack-env/spack.yaml 'spack.config.install_tree.projections.all' '{name}-{version}'
-# debug
 
 spack -e ./spack-env add $SPEC
 
