78b10b4866e55dccdc94a6eaa5fd2256e85924f2
Simon Pintarelli <simon.pintarelli@cscs.ch>
2025-04-24 21:43:24 +0200

    add missing .spec

--- spack/packages/nlcglib/package.py
+++ spack/packages/nlcglib/package.py
@@ -113,7 +113,7 @@ class Nlcglib(CMakePackage, CudaPackage, ROCmPackage):
             print(self.spec["kokkos"])
             options += [
                 self.define(
-                    "CMAKE_CXX_COMPILER", "{0}".format(self["kokkos-nvcc-wrapper"].kokkos_cxx)
+                    "CMAKE_CXX_COMPILER", "{0}".format(self.spec["kokkos-nvcc-wrapper"].kokkos_cxx)
                 )
             ]
 
