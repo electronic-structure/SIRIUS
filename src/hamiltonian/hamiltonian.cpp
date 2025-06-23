baacdea1ebc3d8e92660436004616901c14599e4
Simon Pintarelli <simon.pintarelli@cscs.ch>
2025-06-23 17:04:36 +0200

    format

--- src/hamiltonian/hamiltonian.cpp
+++ src/hamiltonian/hamiltonian.cpp
@@ -157,7 +157,7 @@ Hamiltonian0<T>::apply_bmt(wf::Wave_functions<T>& psi__, std::vector<wf::Wave_fu
 
         zm.zero();
 
-/* only upper triangular part of zm is computed because it is a hermitian matrix */
+        /* only upper triangular part of zm is computed because it is a hermitian matrix */
         #pragma omp parallel for default(shared)
         for (int xi2 = 0; xi2 < mt_basis_size; xi2++) {
             int lm2    = atom.type().indexb(xi2).lm;
