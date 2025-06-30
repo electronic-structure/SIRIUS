eb8a6ade3fb98b21d16bf377f9f0caaa8f3d4ed7
Mathieu Taillefumier <mathieu.taillefumier@free.fr>
2025-06-30 08:28:45 -0400

    Use the old package conventions

--- spack/packages/toml_f/package.py
+++ spack/packages/toml_f/package.py
@@ -2,10 +2,10 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack_repo.builtin.build_systems.meson import MesonPackage
+#from spack_repo.builtin.build_systems.meson import MesonPackage
 
 from spack.package import *
-
+from spack.build_systems import cmake, meson
 
 class TomlF(MesonPackage):
     """
