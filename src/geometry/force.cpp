20fc22551b03eb06a19cbe030f90feaf69b76262
Anton Kozhevnikov <toxa81@gmail.com>
2025-11-28 14:20:35 +0100

    fix expression for ewald force

--- src/geometry/force.cpp
+++ src/geometry/force.cpp
@@ -394,7 +394,10 @@ Force::calc_forces_ewald()
             double d  = unit_cell.nearest_neighbour(i, ia).distance;
             double d2 = d * d;
 
-            auto t = dot(unit_cell.lattice_vectors(), r3::vector<int>(unit_cell.nearest_neighbour(i, ia).translation));
+            //auto tl = unit_cell.atom(ia).position() - unit_cell.atom(ja).position() - r3::vector<int>(unit_cell.nearest_neighbour(i, ia).translation);
+
+            //auto t = dot(unit_cell.lattice_vectors(), tl);
+	    auto& t = unit_cell.nearest_neighbour(i, ia).rc;
 
             double scalar_part =
                     static_cast<double>(unit_cell.atom(ia).zn() * unit_cell.atom(ja).zn()) / d2 *
