49ad65c45d9c2c79cd3a157d4d7241455c154db9
Simon Pintarelli <simon.pintarelli@cscs.ch>
2025-12-01 15:24:51 +0100

    fix collaps.py

--- apps/timers/collapse.py
+++ apps/timers/collapse.py
@@ -5,16 +5,25 @@ import numpy as np
 import copy
 import sys
 
-
 data = json.load(open(sys.argv[1], "r"))
 
 
 def getentry(path, data):
+    total_time = np.sum(data["timings"])
     if len(data["sub-timings"]) == 0:
-        yield path, np.sum(data["timings"])
+        return [(path, total_time)]
+
+    sub_timings = []
     for label in data["sub-timings"]:
-        yield from getentry(copy.deepcopy(path) + [label], data["sub-timings"][label])
+        sub_timings += getentry(
+            copy.deepcopy(path) + [label], data["sub-timings"][label]
+        )
+    total_sub_timings = np.sum([x[1] for x in sub_timings])
+    own_time = total_time - total_sub_timings
+    if own_time < 0:
+        raise Exception('something went wrong')
+    return [(path, own_time)] + sub_timings
 
 
-for path, t in getentry(["sirius"], data["sirius"]):
+for path, t in getentry(["SCF Loop"], data):
     print(";".join(path) + " " + f"{t:10.10f}")
