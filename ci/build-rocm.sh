f7aa8ae4933ef327f85289ca14146fade5e9b1d4
Simon Pintarelli <simon.pintarelli@cscs.ch>
2026-04-19 12:54:57 +0200

    update ci

--- ci/build-rocm.sh
+++ ci/build-rocm.sh
@@ -2,8 +2,6 @@
 
 set -xeuo pipefail
 
-export SPACK_SYSTEM_CONFIG_PATH=/user-environment/config
-
 # make sure we keep the stage direcorty
 spack config --scope=spack add config:build_stage:/dev/shm/spack-stage
 # we might need to install dependencies too, e.g. nlcglib in case of API changes
