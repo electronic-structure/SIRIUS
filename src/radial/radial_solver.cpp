12d07135d283dfa778986785fdc729ea4c3df821
toxa81 <toxa81@gmail.com>
2026-07-20 17:27:52 +0200

    more diagnostic output

--- src/radial/radial_solver.cpp
+++ src/radial/radial_solver.cpp
@@ -36,6 +36,15 @@ find_enu(relativity_t rel__, int zn__, int n__, int l__, Radial_grid<double> con
     double constexpr enu_tol{1e-9};
 
     std::stringstream sinfo;
+    sinfo << "find_enu()" << std::endl
+          << "  rel : " << get_relativity(rel__) << std::endl
+          << "  zn  : " << zn__ << std::endl
+          << "  n   : " << n__ << std::endl
+          << "  l   : " << l__ << std::endl
+          << "  auto_enu        : " << enu__.auto_enu << std::endl
+          << "  etop            : " << enu__.etop << std::endl
+          << "  etop_first_pass : " << enu__.etop_first_pass << std::endl
+          << "  ebot            : " << enu__.ebot << std::endl;
 
     auto compute_etop = [&]() -> int {
         /* We want to find enu such that the wave-function at the muffin-tin boundary is zero
@@ -45,9 +54,7 @@ find_enu(relativity_t rel__, int zn__, int n__, int l__, Radial_grid<double> con
         int sp;
         double denu{1e-6};
         double e1;
-        sinfo << "find_enu(): find top enery" << std::endl
-              << "  n         : " << n__ << ", l : " << l__ << std::endl
-              << "  enu_start : " << enu__.etop << std::endl;
+        sinfo << "  step : estimate top of the energy" << std::endl;
         try {
             /* 1st pass: estimate upper and lower boundaries of the etop */
             e1 = solver.integrate_forward_until(rel__, enu__.etop, l__, 0, chi_p, chi_q, p, dpdr, q, dqdr, false,
@@ -62,11 +69,14 @@ find_enu(relativity_t rel__, int zn__, int n__, int l__, Radial_grid<double> con
                                              return false;
                                          });
         } catch (std::exception const& e) {
-            sinfo << e.what() << std::endl << "denu : " << denu;
+            sinfo << "  denu  : " << denu << std::endl;
+            sinfo << "  error : " << e.what() << std::endl;
             return 1;
         }
 
         if (std::abs(e1 - enu__.etop_first_pass) > enu_tol) {
+            sinfo << "  step : refine top of the energy" << std::endl;
+
             enu_out.etop_first_pass = e1;
 
             double e2 = e1 - sp * denu;
@@ -76,9 +86,8 @@ find_enu(relativity_t rel__, int zn__, int n__, int l__, Radial_grid<double> con
                 std::swap(e1, e2);
             }
 
-            sinfo << "find_enu(): refine top energy" << std::endl
-                  << "  e1 : " << e1 << ", e2 : " << e2 << std::endl
-                  << "  enu_start : " << (e1 + e2) / 2 << std::endl;
+            sinfo << "  e1 : " << e1 << std::endl
+                  << "  e2 : " << e2 << std::endl;
 
             try {
                 /* 2nd pass: refine by bisection */
@@ -93,7 +102,9 @@ find_enu(relativity_t rel__, int zn__, int n__, int l__, Radial_grid<double> con
                                                     return std::abs(e1 - e2) < enu_tol;
                                                 });
             } catch (std::exception const& e) {
-                sinfo << e.what() << std::endl;
+                sinfo << " last e1 : " << e1 << std::endl
+                      << " last e2 : " << e2 << std::endl
+                      << " error   : " << e.what() << std::endl;
                 return 1;
             }
         } else {
@@ -105,7 +116,7 @@ find_enu(relativity_t rel__, int zn__, int n__, int l__, Radial_grid<double> con
 
     /* start by computing top of linearization energy */
     if (compute_etop() != 0) {
-        sinfo << "find_enu(): top of the linearization energy interval is not found";
+        sinfo << "  error : top of the linearization energy interval is not found";
         RTE_THROW(sinfo);
     }
 
@@ -146,7 +157,7 @@ find_enu(relativity_t rel__, int zn__, int n__, int l__, Radial_grid<double> con
      * at the muffin-tin boundary. This will be the bottom of the band. Here we look at a sign change
      * of the derivative. */
     try {
-        sinfo << "find_enu(): find bottom energy" << std::endl << "  enu_start : " << enu__.etop << std::endl;
+        sinfo << "  step : estimate bottom of the energy" << std::endl;
         auto sd = surface_deriv();
         auto e1 = solver.integrate_forward_until(rel__, enu_out.etop, l__, 0, chi_p, chi_q, p, dpdr, q, dqdr, false,
                                             [&denu, sd, &surface_deriv, &p](int iter, int nn, double& enu) {
@@ -167,7 +178,9 @@ find_enu(relativity_t rel__, int zn__, int n__, int l__, Radial_grid<double> con
             enu_out.ebot = (e1 + e2) * 0.5;
             enu_out.enu = (enu_out.ebot + enu_out.etop) / 2.0;
         } else {
-            sinfo << "find_enu(): refine bottom energy" << std::endl << "  enu_start : " << (e1 + e2) / 2 << std::endl;
+            sinfo << "  step : refine bottom of the energy";
+            sinfo << "  e1 : " << e1 << std::endl
+                  << "  e2 : " << e2 << std::endl;
             enu_out.ebot = solver.integrate_forward_until(rel__, (e1 + e2) / 2, l__, 0, chi_p, chi_q, p, dpdr, q, dqdr, false,
                                             [&e1, &e2, sd, &surface_deriv](int iter, int nn, double& enu) {
                                                 if (surface_deriv() * sd > 0) {
@@ -193,7 +206,10 @@ find_enu(relativity_t rel__, int zn__, int n__, int l__, Radial_grid<double> con
             }
         }
     } catch (std::exception const& e) {
-        sinfo << e.what() << std::endl << "denu : " << denu << std::endl;
+        sinfo << "  last p(R)  : " << p.back() << std::endl
+              << "  last p'(R) : " << dpdr.back() << std::endl
+              << "  error      : " << e.what() << std::endl;
+        RTE_THROW(sinfo);
     }
 
     return enu_out;
