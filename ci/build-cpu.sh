1845ad644f1dc8a5115eb6c5e3eaa666ce1bcff5
Simon Pintarelli <simon.pintarelli@cscs.ch>
2026-09-01 16:31:03 +0200

    pin cray-mpich version

--- ci/build-cpu.sh
+++ ci/build-cpu.sh
@@ -11,7 +11,7 @@ spack config --scope=spack add config:install_tree:root:/dev/shm/spack-stage
 
 spack env create -d ./spack-env
 # make sure cray-mpich is used
-spack -e ./spack-env add cray-mpich
+spack -e ./spack-env add cray-mpich@9.1.0
 
 # add local repository with current sirius recipe
 spack -e ./spack-env repo add $REPO
